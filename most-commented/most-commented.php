e97f66806e3f611a0a9d13938c7ab6862df5e479
mtdewvirus <mtdewvirus@dd790b09-5c1c-0410-8744-875116bcd8a6>
2008-06-17 13:05:09 +0000

    most-commented vip plugin
    git-svn-id: https://vip-svn.wordpress.com/plugins@7846 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ most-commented/most-commented.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Most Commented
+Plugin URI: http://mtdewvirus.com/code/wordpress-plugins/
+Description: Retrieves a list of the posts with the most comments. Modified for Last X days -- by DJ Chuang www.djchuang.com 
+Version: 1.4.1
+Author: Nick Momrik
+Author URI: http://mtdewvirus.com/
+*/
+
+function mdv_most_commented($no_posts = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $duration='') {
+    global $wpdb;
+	
+	$mdv_most_commented = wp_cache_get('mdv_most_commented');
+	if ($mdv_most_commented === false) {
+		$request = "SELECT ID, post_title, COUNT($wpdb->comments.comment_post_ID) AS 'comment_count' FROM $wpdb->posts, $wpdb->comments";
+		$request .= " WHERE comment_approved = '1' AND $wpdb->posts.ID=$wpdb->comments.comment_post_ID AND post_status = 'publish'";
+		if (!$show_pass_post) $request .= " AND post_password =''";
+	
+		if ($duration !="") $request .= " AND DATE_SUB(CURDATE(),INTERVAL ".$duration." DAY) < post_date ";
+	
+		$request .= " GROUP BY $wpdb->comments.comment_post_ID ORDER BY comment_count DESC LIMIT $no_posts";
+		$posts = $wpdb->get_results($request);
+
+		if ($posts) {
+			foreach ($posts as $post) {
+				$post_title = stripslashes($post->post_title);
+				$comment_count = $post->comment_count;
+				$permalink = get_permalink($post->ID);
+				$mdv_most_commented .= $before . '<a href="' . $permalink . '" title="' . $post_title.'">' . $post_title . '</a> (' . $comment_count.')' . $after;
+			}
+		} else {
+			$mdv_most_commented .= $before . "None found" . $after;
+		}
+	
+		wp_cache_set('mdv_most_commented', $mdv_most_commented);
+	} 
+
+    echo $mdv_most_commented;
+}
+?>
