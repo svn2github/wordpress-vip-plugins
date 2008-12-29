c0fcb0a7fceb20e8c644817d7f95130ea0bbab61
tott <tott@dd790b09-5c1c-0410-8744-875116bcd8a6>
2008-12-29 22:13:17 +0000

    added intensedebate plugin to vip plugins.
    git-svn-id: https://vip-svn.wordpress.com/plugins@13766 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ intensedebate/intensedebate-comment-template.php
@@ -0,0 +1,195 @@
+<?php
+
+//check for mobile and output WP comments if so
+if(get_option('id_revertMobile')==0 && id_is_mobile())
+{
+	include(get_option("id_comment_template_file"));
+}
+else //output new ID comments
+{
+	global $id_cur_post;
+	$id_cur_post = $post;
+?>
+
+	<div id='idc-container'></div>
+	<script type='text/javascript' src='http://www.intensedebate.com/js/wordpressTemplateCommentWrapper2.php?acct=<?php echo get_option("id_blogAcct");?>&amp;postid=<?php echo $id;?>&amp;title=<?php echo urlencode($post->post_title);?>&amp;url=<?php echo urlencode(the_permalink($id));?>&amp;posttime=<?php echo urlencode($post->post_date_gmt);?>&amp;postauthor=<?php echo urlencode(get_author_name($post->post_author));?>&amp;guid=<?php echo urlencode($post->guid);?>'></script>
+	<noscript>
+
+<?php
+	$old_template = file_get_contents(get_option("id_comment_template_file"));
+	if(!preg_match("/<noscript>(.|\n|\r)*?<\/noscript>/i", $old_template, $matches)) //check if the file contains any <noscript> tags...if it does we can't use it
+	{
+		include(get_option("id_comment_template_file"));
+	}
+	else //if we can't use theirs, use the default theme to output comments
+	{
+		// Do not delete these lines
+	if (!empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php' == basename($_SERVER['SCRIPT_FILENAME']))
+		die ('Please do not load this page directly. Thanks!');
+
+	if (!empty($post->post_password)) { // if there's a password
+		if ($_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {  // and it doesn't match the cookie
+			?>
+
+			<p class="nocomments">This post is password protected. Enter the password to view comments.</p>
+
+			<?php
+			return;
+		}
+	}
+
+	/* This variable is for alternating comment background */
+	$oddcomment = 'class="alt" ';
+?>
+
+<!-- You can start editing here. -->
+
+<?php if ($comments) : ?>
+	<h3 id="comments"><?php comments_number('No Responses', 'One Response', '% Responses' );?> to &#8220;<?php the_title(); ?>&#8221;</h3>
+
+	<ol class="commentlist">
+
+	<?php foreach ($comments as $comment) : ?>
+
+		<li <?php echo $oddcomment; ?>id="comment-<?php comment_ID() ?>">
+			<?php echo get_avatar( $comment, 32 ); ?>
+			<cite><?php comment_author_link() ?></cite> Says:
+			<?php if ($comment->comment_approved == '0') : ?>
+			<em>Your comment is awaiting moderation.</em>
+			<?php endif; ?>
+			<br />
+
+			<small class="commentmetadata"><a href="#comment-<?php comment_ID() ?>" title=""><?php comment_date('F jS, Y') ?> at <?php comment_time() ?></a> <?php edit_comment_link('edit','&nbsp;&nbsp;',''); ?></small>
+
+			<?php comment_text() ?>
+
+		</li>
+
+	<?php
+		/* Changes every other comment to a different class */
+		$oddcomment = ( empty( $oddcomment ) ) ? 'class="alt" ' : '';
+	?>
+
+	<?php endforeach; /* end for each comment */ ?>
+
+	</ol>
+
+ <?php else : // this is displayed if there are no comments so far ?>
+
+	<?php if ('open' == $post->comment_status) : ?>
+		<!-- If comments are open, but there are no comments. -->
+
+	 <?php else : // comments are closed ?>
+		<!-- If comments are closed. -->
+		<p class="nocomments">Comments are closed.</p>
+
+	<?php endif; ?>
+<?php endif; ?>
+
+
+<?php if ('open' == $post->comment_status) : ?>
+
+<h3 id="respond">Leave a Reply</h3>
+
+<?php if ( get_option('comment_registration') && !$user_ID ) : ?>
+<p>You must be <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?redirect_to=<?php echo urlencode(get_permalink()); ?>">logged in</a> to post a comment.</p>
+<?php else : ?>
+
+<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+
+<?php if ( $user_ID ) : ?>
+
+<p>Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. <a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout" title="Log out of this account">Log out &raquo;</a></p>
+
+<?php else : ?>
+
+<p><input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" size="22" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+<label for="author"><small>Name <?php if ($req) echo "(required)"; ?></small></label></p>
+
+<p><input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+<label for="email"><small>Mail (will not be published) <?php if ($req) echo "(required)"; ?></small></label></p>
+
+<p><input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" size="22" tabindex="3" />
+<label for="url"><small>Website</small></label></p>
+
+<?php endif; ?>
+
+<!--<p><small><strong>XHTML:</strong> You can use these tags: <code><?php echo allowed_tags(); ?></code></small></p>-->
+
+<p><textarea name="comment" id="comment" cols="100%" rows="10" tabindex="4"></textarea></p>
+
+<p><input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
+<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+</p>
+<?php do_action('comment_form', $post->ID); ?>
+
+</form>
+
+<?php endif; // If registration required and not logged in ?>
+
+<?php endif; // if you delete this the sky will fall on your head
+	}
+		
+?>
+	</noscript>
+
+<?php
+
+}
+
+function id_is_mobile()
+{
+	$op = strtolower($_SERVER['HTTP_X_OPERAMINI_PHONE']);
+	$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
+	$ac = strtolower($_SERVER['HTTP_ACCEPT']);
+	$ip = $_SERVER['REMOTE_ADDR'];
+
+	 $isMobile = strpos($ac, 'application/vnd.wap.xhtml+xml') !== false
+        || $op != ''
+        || strpos($ua, 'sony') !== false 
+        || strpos($ua, 'symbian') !== false 
+        || strpos($ua, 'nokia') !== false 
+        || strpos($ua, 'samsung') !== false 
+        || strpos($ua, 'mobile') !== false
+        || strpos($ua, 'windows ce') !== false
+        || strpos($ua, 'epoc') !== false
+        || strpos($ua, 'opera mini') !== false
+        || strpos($ua, 'nitro') !== false
+        || strpos($ua, 'j2me') !== false
+        || strpos($ua, 'midp-') !== false
+        || strpos($ua, 'cldc-') !== false
+        || strpos($ua, 'netfront') !== false
+        || strpos($ua, 'mot') !== false
+        || strpos($ua, 'up.browser') !== false
+        || strpos($ua, 'up.link') !== false
+        || strpos($ua, 'audiovox') !== false
+        || strpos($ua, 'blackberry') !== false
+        || strpos($ua, 'ericsson,') !== false
+        || strpos($ua, 'panasonic') !== false
+        || strpos($ua, 'philips') !== false
+        || strpos($ua, 'sanyo') !== false
+        || strpos($ua, 'sharp') !== false
+        || strpos($ua, 'sie-') !== false
+        || strpos($ua, 'portalmmm') !== false
+        || strpos($ua, 'blazer') !== false
+        || strpos($ua, 'avantgo') !== false
+        || strpos($ua, 'danger') !== false
+        || strpos($ua, 'palm') !== false
+        || strpos($ua, 'series60') !== false
+        || strpos($ua, 'palmsource') !== false
+        || strpos($ua, 'pocketpc') !== false
+        || strpos($ua, 'smartphone') !== false
+        || strpos($ua, 'rover') !== false
+        || strpos($ua, 'ipaq') !== false
+        || strpos($ua, 'au-mic,') !== false
+        || strpos($ua, 'alcatel') !== false
+        || strpos($ua, 'ericy') !== false
+        || strpos($ua, 'up.link') !== false
+        || strpos($ua, 'vodafone/') !== false
+        || strpos($ua, 'wap1.') !== false
+        || strpos($ua, 'wap2.') !== false;
+
+	return $isMobile;
+}
+?>
+
---
+++ intensedebate/readme.txt
@@ -0,0 +1,40 @@
+=== IntenseDebate Comments ===
+Contributors: Jon Fox, Crowd Favorite
+Tags: widget, profile, community, avatars, spam, notification, email, threaded, comments
+Requires at least: 2.0
+Tested up to: 2.7
+Stable tag: trunk
+
+IntenseDebate comments enhance and encourage conversation on your blog.  Build your reader community, increase your comments, & boost pageviews.
+
+== Description ==
+
+IntenseDebate Comments enhance and encourage conversation on your blog or website. Full comment and account data sync between IntenseDebate and WordPress ensures that you will always have your comments. Custom integration with your WordPress admin panel makes moderation a piece of cake. Comment threading, reply-by-email, user accounts and reputations, comment voting, along with Twitter and friendfeed integrations enrich your readers' experience and make more of the internet aware of your blog and comments which drives traffic to you! 
+
+== Installation ==
+
+*Note: As is the case when installing any new plugin, it's always a good idea to backup your blog data before installing.*
+
+1. After downloading the IntenseDebate plugin, unpack and upload the file to the wp-content/plugins folder on your blog. Make sure to leave the directory structure of the archive intact so that all of the IntenseDebate files are located in 'wp-content/plugins/intensedebate/'2. You will need to activate the IntenseDebate plugin in order to see your new comment system. Go to the Plugins tab and find IntenseDebate Comments in the list and click **Activate**.3. After activating proceed to your plugin settings page to configure your plugin. Don't forget to visit your account at [IntenseDebate.com](http://intensedebate.com) for *additional customization options*.
+
+== Frequently Asked Questions ==
+
+= What is data synchronization? =
+
+Comments made in IntenseDebate are automatically backed-up to your WordPress comment system, while your existing WordPress comments are automatically imported into IntenseDebate. You always have all of your comments so uninstalling is a one-click process if you change your mind. Syncing your accounts enables auto-login so logging into WordPress automatically logs you into IntenseDebate. Profile sync also means that comments you make are synchronized to both profiles so you maintain ownership of your profile.
+
+= What about SEO? =
+
+IntenseDebate outputs the standard WordPress comments enabling your comments to still be indexed by search engines that ignore JavaScript, while ensuring that visitors surfing with JavaScript disabled will be able to interact with comments made in IntenseDebate. Readers with JS disabled can comment in the original WP system and those comments will be imported into IntenseDebate.
+
+= How do I use my OpenID? = 
+
+While you can use your OpenID to login and signup with IntenseDebate, using your OpenID to synchronize IntenseDebate and WordPress accounts is not currently supported.
+
+If you created your account using your OpenID you will need to access your IntenseDebate username and userkey. Your username and user key will allow you to sync your accounts. You will need to be logged into IntenseDebate in order to access your username and [user key](http://intensedebate.com/userkey).
+
+= How do I sync my accounts? = 
+After installing and activating the IntenseDebate plugin you will need to update your account settings in your Plugin Settings page and either create an IntenseDebate account or login to your IntenseDebate account. Your data synchronization and comment import will start as soon as you have successfully logged in.
+
+**Please Note:**
+Your comments may take several hours to import. The time of your import is dependent on how many other import requests we are receiving in addition to how many comments are currently being imported. 
