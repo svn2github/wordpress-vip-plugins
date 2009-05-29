a746b3a257e675cceb5468a4da4153c59218108a
mtdewvirus <mtdewvirus@dd790b09-5c1c-0410-8744-875116bcd8a6>
2009-05-29 16:21:32 +0000

    git-svn-id: https://vip-svn.wordpress.com/plugins@19145 dd790b09-5c1c-0410-8744-875116bcd8a6

--- intensedebate/intensedebate-comment-template.php
+++ intensedebate/intensedebate-comment-template.php
@@ -1,7 +1,7 @@
 <?php
 
 //check for mobile and output WP comments if so
-if(get_option('id_revertMobile')==0 && id_is_mobile())
+if(get_option('id_revertMobile')==0 && is_mobile())
 {
 	include(get_option("id_comment_template_file"));
 }
@@ -90,7 +90,6 @@ else //output new ID comments
 	<?php endif; ?>
 <?php endif; ?>
 
-
 <?php if ('open' == $post->comment_status) : ?>
 
 <h3 id="respond">Leave a Reply</h3>
@@ -132,68 +131,9 @@ else //output new ID comments
 <?php endif; // If registration required and not logged in ?>
 
 <?php endif; // if you delete this the sky will fall on your head
-	}
-		
+	}		
 ?>
 	</noscript>
 
 <?php
-
 }
-
-function id_is_mobile()
-{
-	$op = strtolower($_SERVER['HTTP_X_OPERAMINI_PHONE']);
-	$ua = strtolower($_SERVER['HTTP_USER_AGENT']);
-	$ac = strtolower($_SERVER['HTTP_ACCEPT']);
-	$ip = $_SERVER['REMOTE_ADDR'];
-
-	 $isMobile = strpos($ac, 'application/vnd.wap.xhtml+xml') !== false
-        || $op != ''
-        || strpos($ua, 'sony') !== false 
-        || strpos($ua, 'symbian') !== false 
-        || strpos($ua, 'nokia') !== false 
-        || strpos($ua, 'samsung') !== false 
-        || strpos($ua, 'mobile') !== false
-        || strpos($ua, 'windows ce') !== false
-        || strpos($ua, 'epoc') !== false
-        || strpos($ua, 'opera mini') !== false
-        || strpos($ua, 'nitro') !== false
-        || strpos($ua, 'j2me') !== false
-        || strpos($ua, 'midp-') !== false
-        || strpos($ua, 'cldc-') !== false
-        || strpos($ua, 'netfront') !== false
-        || strpos($ua, 'mot') !== false
-        || strpos($ua, 'up.browser') !== false
-        || strpos($ua, 'up.link') !== false
-        || strpos($ua, 'audiovox') !== false
-        || strpos($ua, 'blackberry') !== false
-        || strpos($ua, 'ericsson,') !== false
-        || strpos($ua, 'panasonic') !== false
-        || strpos($ua, 'philips') !== false
-        || strpos($ua, 'sanyo') !== false
-        || strpos($ua, 'sharp') !== false
-        || strpos($ua, 'sie-') !== false
-        || strpos($ua, 'portalmmm') !== false
-        || strpos($ua, 'blazer') !== false
-        || strpos($ua, 'avantgo') !== false
-        || strpos($ua, 'danger') !== false
-        || strpos($ua, 'palm') !== false
-        || strpos($ua, 'series60') !== false
-        || strpos($ua, 'palmsource') !== false
-        || strpos($ua, 'pocketpc') !== false
-        || strpos($ua, 'smartphone') !== false
-        || strpos($ua, 'rover') !== false
-        || strpos($ua, 'ipaq') !== false
-        || strpos($ua, 'au-mic,') !== false
-        || strpos($ua, 'alcatel') !== false
-        || strpos($ua, 'ericy') !== false
-        || strpos($ua, 'up.link') !== false
-        || strpos($ua, 'vodafone/') !== false
-        || strpos($ua, 'wap1.') !== false
-        || strpos($ua, 'wap2.') !== false;
-
-	return $isMobile;
-}
-?>
-
