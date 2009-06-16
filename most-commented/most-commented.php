84788b2d44029bf4e6050a9f90ebe9ccf79084d8
mtdewvirus <mtdewvirus@dd790b09-5c1c-0410-8744-875116bcd8a6>
2009-06-16 15:03:44 +0000

    add an option to switch between echo/return. Validate some data. Set cache expiration.
    git-svn-id: https://vip-svn.wordpress.com/plugins@19579 dd790b09-5c1c-0410-8744-875116bcd8a6

--- most-commented/most-commented.php
+++ most-commented/most-commented.php
@@ -3,28 +3,30 @@
 Plugin Name: Most Commented
 Plugin URI: http://mtdewvirus.com/code/wordpress-plugins/
 Description: Retrieves a list of the posts with the most comments. Modified for Last X days -- by DJ Chuang www.djchuang.com 
-Version: 1.5
+Version: 1.6
 Author: Nick Momrik
 Author URI: http://mtdewvirus.com/
 */
 
-function mdv_most_commented($no_posts = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $duration='') {
-    global $wpdb;
-	
-	$mdv_most_commented = wp_cache_get('mdv_most_commented');
-	if ($mdv_most_commented === false) {
-		$request = "SELECT ID, post_title, comment_count FROM $wpdb->posts";
-		$request .= " WHERE post_status = 'publish'";
-		if (!$show_pass_post) $request .= " AND post_password =''";
-	
-		if ($duration !="") $request .= " AND DATE_SUB(CURDATE(),INTERVAL ".$duration." DAY) < post_date ";
-	
+function mdv_most_commented( $no_posts = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $duration = '', $echo = true ) {
+	global $wpdb;
+
+	if ( !($posts = wp_cache_get('mdv_most_commented')) ) {
+		$request = "SELECT ID, post_title, comment_count FROM $wpdb->posts WHERE post_status = 'publish'";
+		if ( !$show_pass_post ) $request .= " AND post_password =''";
+		if ( is_int($duration) ) $request .= " AND DATE_SUB(CURDATE(),INTERVAL ".$duration." DAY) < post_date ";
+		if ( !is_int($no_posts) ) $no_posts = 5;
 		$request .= " ORDER BY comment_count DESC LIMIT $no_posts";
+
 		$posts = $wpdb->get_results($request);
 
-		if ($posts) {
+		wp_cache_set('mdv_most_commented', $posts, '', 1800);
+	}
+
+	if ( $echo ) {
+		if ( !empty($posts) ) {
 			foreach ($posts as $post) {
-				$post_title = stripslashes($post->post_title);
+				$post_title = apply_filters('the_title', $post->post_title);
 				$comment_count = $post->comment_count;
 				$permalink = get_permalink($post->ID);
 				$mdv_most_commented .= $before . '<a href="' . $permalink . '" title="' . $post_title.'">' . $post_title . '</a> (' . $comment_count.')' . $after;
@@ -32,10 +34,9 @@ function mdv_most_commented($no_posts = 5, $before = '<li>', $after = '</li>', $
 		} else {
 			$mdv_most_commented .= $before . "None found" . $after;
 		}
-	
-		wp_cache_set('mdv_most_commented', $mdv_most_commented);
-	} 
 
-    echo $mdv_most_commented;
+		echo $mdv_most_commented;
+	} else {
+		return $posts;
+	}
 }
-?>
