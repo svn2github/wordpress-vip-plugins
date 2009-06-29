af27c6ece37ad8466c53d39fba92291fcdaf8dcd
mtdewvirus <mtdewvirus@dd790b09-5c1c-0410-8744-875116bcd8a6>
2009-06-29 14:51:22 +0000

    new version with better code formatting and an option to hide trackbacks and pingbacks
    git-svn-id: https://vip-svn.wordpress.com/plugins@20058 dd790b09-5c1c-0410-8744-875116bcd8a6

--- recent-comments/recent-comments.php
+++ recent-comments/recent-comments.php
@@ -3,22 +3,26 @@
 Plugin Name: Recent Comments
 Plugin URI: http://mtdewvirus.com/code/wordpress-plugins/
 Description: Retrieves a list of the most recent comments.
-Version: 1.18
+Version: 1.2
 Author: Nick Momrik
 Author URI: http://mtdewvirus.com/
 */
 
-function most_recent_comments($no_comments = 5, $comment_lenth = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $comment_style = 0) {
-    global $wpdb;
+function most_recent_comments($no_comments = 5, $comment_lenth = 5, $before = '<li>', $after = '</li>', $show_pass_post = false, $comment_style = 0, $hide_pingbacks_trackbacks = false) {
+	global $wpdb;
 	$most_recent_comments = wp_cache_get('most_recent_comments');
 	
-	if($most_recent_comments === false) {
-    	$request = "SELECT ID, comment_ID, comment_content, comment_author, comment_author_url, post_title FROM $wpdb->comments LEFT JOIN $wpdb->posts ON $wpdb->posts.ID=$wpdb->comments.comment_post_ID WHERE post_status IN ('publish','static') ";
-		if(!$show_pass_post) $request .= "AND post_password ='' ";
+	if ( false === $most_recent_comments ) {
+		$request = "SELECT ID, comment_ID, comment_content, comment_author, comment_author_url, post_title FROM $wpdb->comments LEFT JOIN $wpdb->posts ON $wpdb->posts.ID=$wpdb->comments.comment_post_ID WHERE post_status IN ('publish','static') ";
+		if( !$show_pass_post ) $request .= "AND post_password ='' ";
+		if ( $hide_pingbacks_trackbacks ) $request .= "AND comment_type='' ";
 		$request .= "AND comment_approved = '1' ORDER BY comment_ID DESC LIMIT $no_comments";
+
 		$comments = $wpdb->get_results($request);
-    	$output = '';
-		if ($comments) {
+	
+		$output = '';
+
+		if ( $comments ) {
 			$idx = 0;
 			foreach ($comments as $comment) {
 				$comment_author = stripslashes($comment->comment_author);
@@ -26,40 +30,41 @@ function most_recent_comments($no_comments = 5, $comment_lenth = 5, $before = '<
 					$comment_author = "anonymous"; 
 				$comment_content = strip_tags($comment->comment_content);
 				$comment_content = stripslashes($comment_content);
-				$words=split(" ",$comment_content); 
-				$comment_excerpt = join(" ",array_slice($words,0,$comment_lenth));
-				$permalink = get_permalink($comment->ID)."#comment-".$comment->comment_ID;
-    	
-				if ($comment_style == 1) {
+				$words = split(" ", $comment_content); 
+				$comment_excerpt = join(" ", array_slice($words, 0, $comment_lenth));
+				$permalink = get_permalink($comment->ID) . "#comment-" . $comment->comment_ID;
+				
+				if ( 1 == $comment_style ) {
 					$post_title = stripslashes($comment->post_title);
 					$post_id= stripslashes($comment->post_id);
 					$url = $comment->comment_author_url;
 					$idx++;
-					if ($idx % 2 == 1) {
+					if ( 1 == $idx % 2 )
 						$before = "<li class='statsclass1'>";
-					} else {
+					else
 						$before = "<li class='statsclass2'>";
-					}
 					$output .= $before . "<a href='$permalink'>$comment_author</a>" . ' on <a href="' . get_permalink($comment->ID) . '">' . $post_title . '</a>' . $after;
-				}
-				else {
+				} else {
 					$idx++;
-					if ($idx % 2 == 1) {
+					if ( 1 == $idx % 2 )
 						$before = "<li class='statsclass1'>";
-					} else {
+					else
 						$before = "<li class='statsclass2'>";
-					}
+		
 					$output .= $before . '<strong>' . $comment_author . ':</strong> <a href="' . $permalink;
 					$output .= '" title="View the entire comment by ' . $comment_author.'">' . $comment_excerpt.'</a>' . $after;
 				}
 			}
+
 			$output = convert_smilies($output);
 		} else {
 			$output .= $before . "None found" . $after;
 		}
+
 		$most_recent_comments = $output;
-    	wp_cache_set('most_recent_comments', $most_recent_comments);
+		wp_cache_set('most_recent_comments', $most_recent_comments);
 	}
+
 	echo $most_recent_comments;
 }
 ?>
