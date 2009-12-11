2b232525f08fb639a12f9f17ac5e4e1c53ace953
nickmomrik <nickmomrik@dd790b09-5c1c-0410-8744-875116bcd8a6>
2009-12-11 15:29:00 +0000

    git-svn-id: https://vip-svn.wordpress.com/plugins@25329 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-helper-wpcom.php
+++ vip-helper-wpcom.php
@@ -6,7 +6,7 @@
  * filters, and actions that are enabled across all of WordPress.com.
  *
  * To add these functions to your theme add
-include(ABSPATH . 'wp-content/themes/vip/plugins/vip-helper-wpcom.php');
+require_once( ABSPATH . 'wp-content/themes/vip/plugins/vip-helper-wpcom.php' );
  * in the theme's 'functions.php'. This should be wrapped in a 
 if ( function_exists('wpcom_is_vip') ) { // WPCOM specific
  * so you don't load it in your local environment. This will help alert you if
