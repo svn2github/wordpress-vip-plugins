5b3debc9bf6336984b9605c57d5d849321b273c0
nickmomrik <nickmomrik@dd790b09-5c1c-0410-8744-875116bcd8a6>
2010-06-01 21:54:12 +0000

    sync vip plugin is-mobile.php from WPCOM version. Looking at getting similar functionality in core so we do not need this file
    git-svn-id: https://vip-svn.wordpress.com/plugins@31501 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/is-mobile.php
+++ vip-do-not-include-on-wpcom/is-mobile.php
@@ -9,39 +9,28 @@
  * VIP sites can use their own mobile theme by contacting VIP Support.
  *
  * Many of the user agents came from http://www.russellbeattie.com/blog/mobile-browser-detection-in-php
+ *
+ * VIP version of this is manually synced from WPCOM code.
  */
 
 function is_mobile( $kind = 'any', $return_matched_agent = false ) {
-	static $kinds = array('smart' => false, 'dumb' => false, 'any' => false);
+	static $kinds = array( 'smart' => false, 'dumb' => false, 'any' => false );
 	static $first_run = true;
 	static $matched_agent = '';
 
 	if ( $return_matched_agent )
 		return $matched_agent;
 
-	if ( empty( $_SERVER['HTTP_USER_AGENT'] ) )
+	if ( empty( $_SERVER['HTTP_USER_AGENT'] ) || strpos( strtolower( $_SERVER['HTTP_USER_AGENT'] ), 'ipad' ) )
 		return false;
 
 	if ( $first_run ) {
 		$first_run = false;
 
-		$agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+		$agent = strtolower( $_SERVER['HTTP_USER_AGENT'] );
 
-		$bot_agents = array(
-			'attentio', 'baiduspider', 'chtml generic', 'fast crawler', 'fastmobilecrawl', 'googlebot', 
-			'heritrix', 'irlbot', 'jumpbot', 'mediapartners', 'mediobot', 'motionbot', 'msnbot', 
-			'nokia6230i/. fast crawler', 'slurp', 'spider', 'teoma', 'twiceler', 
-			'yahooseeker', 'yahooysmcm');
-
-		// if UA is a bot, do not count as mobile
-		foreach ( $bot_agents as $bot_agent ) {
-			if ( false !== strpos( $agent, $bot_agent ) ) {
-				return false;
-			}
-		}
-
-		$smart_agents = array('android', 'iphone', 'ipod', 'maemo');
-		$smart_webkit_agents = array('webos');
+		$smart_agents = array( 'android', 'iphone', 'ipod', 'maemo' );
+		$smart_webkit_agents = array( 'webos' );
 		if ( false !== strpos( $agent, 'applewebkit' ) )
 			$smart_agents = array_merge( $smart_agents, $smart_webkit_agents );
 
@@ -61,14 +50,14 @@ function is_mobile( $kind = 'any', $return_matched_agent = false ) {
 				$kinds['dumb'] = true;
 				$matched_agent = 'vnd.wap.xhtml+xml';
 			} else {
-				$dumb_agents = array('alcatel', 'au-mic,', 'audiovox', 'avantgo', 'blackberry', 'blazer',
+				$dumb_agents = array( 'alcatel', 'au-mic,', 'audiovox', 'avantgo', 'blackberry', 'blazer',
 					'cldc-', 'danger', 'docomo', 'epoc',
 					'ericsson,', 'ericy', 'i-mode', 'ipaq', 'j2me', 'midp-', 'mobile',
 					'mot-', 'netfront', 'nitro', 'nokia', 'opera mini', 'palm',
 					'palmsource', 'panasonic', 'philips', 'pocketpc', 'portalmmm',
 					'rover', 'samsung', 'sanyo', 'series60', 'sie-',
 					'smartphone', 'sony', 'symbian', 'up.browser', 'up.link',
-					'up.link', 'vodafone/', 'wap1.', 'wap2.', 'webos', 'windows ce');
+					'up.link', 'vodafone/', 'wap1.', 'wap2.', 'webos', 'windows ce' );
 
 				foreach ( $dumb_agents as $dumb_agent ) {
 					if ( false !== strpos( $agent, $dumb_agent ) ) {
@@ -85,4 +74,56 @@ function is_mobile( $kind = 'any', $return_matched_agent = false ) {
 	}
 
 	return $kinds[$kind];
+}
+
+function is_bot() {
+	static $is_bot = false;
+	static $first_run = true;
+
+	if ( $first_run ) {
+		$first_run = false;
+
+	/*
+		$bot_ips = array( );
+
+		foreach ( $bot_ips as $bot_ip ) {
+			if ( $_SERVER['REMOTE_ADDR'] == $bot_ip )
+				$is_bot = true;
+		}
+	*/
+
+		$agent = strtolower( $_SERVER['HTTP_USER_AGENT'] );
+
+		$bot_agents = array(
+			'alexa', 'altavista', 'ask jeeves', 'attentio', 'baiduspider', 'chtml generic', 'crawler', 'fastmobilecrawl',
+			'feedfetcher-google', 'firefly', 'froogle', 'gigabot', 'googlebot', 'heritrix', 'ia_archiver', 'irlbot',
+			'infoseek', 'jumpbot', 'lycos', 'mediapartners', 'mediobot', 'motionbot', 'msnbot', 'mshots', 'openbot',
+			'pythumbnail', 'scooter', 'slurp', 'snapbot', 'spider', 'surphace scout', 'taptubot', 'technoratisnoop',
+			'teoma', 'twiceler', 'yahooseeker', 'yahooysmcm', 'yammybot' );
+
+		foreach ( $bot_agents as $bot_agent ) {
+			if ( false !== strpos( $agent, $bot_agent ) )
+				$is_bot = true;
+		}
+	}
+
+	return $is_bot;
+}
+
+/*
+  is_ipad() can be used to check the User Agent for an iPad device
+
+  They type can check for any iPad, an iPad using Safari, or an iPad using something other than Safari
+*/
+
+function is_ipad( $type = 'ipad-any' ) {
+	$is_ipad = ( false !== strpos( strtolower( $_SERVER['HTTP_USER_AGENT'] ), 'ipad') );
+	$is_safari = ( false !== strpos( strtolower( $_SERVER['HTTP_USER_AGENT'] ), 'safari' ) );
+
+	if ( 'ipad-safari' == $type )
+		return $is_ipad && $is_safari;
+	elseif ( 'ipad-not-safari' == $type )
+		return $is_ipad && !$is_safari;
+	else
+		return $is_ipad;
 }
