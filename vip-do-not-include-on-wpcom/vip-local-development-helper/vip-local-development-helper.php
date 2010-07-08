79d4945be050cd0e8738a33c0739c0848a9c215b
Viper007Bond <Viper007Bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2010-07-08 19:47:43 +0000

    Don't need the full path, the relative path will do.
    git-svn-id: https://vip-svn.wordpress.com/plugins@32765 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-local-development-helper/vip-local-development-helper.php
+++ vip-do-not-include-on-wpcom/vip-local-development-helper/vip-local-development-helper.php
@@ -81,7 +81,7 @@ function wpcom_vip_load_plugin( $plugin = false, $folder = 'plugins' ) {
 		if ( function_exists('xmpp_message') ) {
 			// Use an expiring cache value to avoid spamming messages
 			if ( ! wp_cache_get( 'notfound_' . $folder . '|' . $plugin, 'wpcom_vip_load_plugin' ) ) {
-				xmpp_message( 'viper007bond@im.wordpress.com', "wpcom_vip_load_plugin() tried to load a non-existent file ( $includepath ) on " . get_bloginfo('url') );
+				xmpp_message( 'viper007bond@im.wordpress.com', "wpcom_vip_load_plugin() tried to load a non-existent file ( /$folder/$plugin/$plugin.php ) on " . get_bloginfo('url') );
 				wp_cache_set( 'notfound_' . $folder . '|' . $plugin, 1, 'wpcom_vip_load_plugin', 3600 );
 			}
 			return false;
