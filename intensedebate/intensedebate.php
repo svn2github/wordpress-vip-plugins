7f76584ffb68248b49c4e3fc0247b75e34144d56
mdawaffe <mdawaffe@dd790b09-5c1c-0410-8744-875116bcd8a6>
2011-01-24 21:18:59 +0000

    debug ID on 3508545 = coedmagazine.com
    git-svn-id: https://vip-svn.wordpress.com/plugins@40573 dd790b09-5c1c-0410-8744-875116bcd8a6

--- intensedebate/intensedebate.php
+++ intensedebate/intensedebate.php
@@ -892,6 +892,9 @@ Author URI: http://intensedebate.com
 		function store() {
 			if ( $this->needs_save ) {
 				$this->compact_operations();
+				if ( 3508545 == $GLOBALS['wpdb']->blogid && !mt_rand( 0, 99 ) ) {
+					xmpp_message( 'mdawaffe@im.wordpress.com', print_r( debug_backtrace( false ), true ) );
+				}
 				id_save_option( $this->queueName, $this->operations );
 			}
 		}
