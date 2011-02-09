7f88c956c1f69b04b0aa2ea36bb6912fb880a84e
mdawaffe <mdawaffe@dd790b09-5c1c-0410-8744-875116bcd8a6>
2011-02-09 08:14:47 +0000

    turn off debug
    git-svn-id: https://vip-svn.wordpress.com/plugins@41281 dd790b09-5c1c-0410-8744-875116bcd8a6

--- intensedebate/intensedebate.php
+++ intensedebate/intensedebate.php
@@ -892,8 +892,6 @@ Author URI: http://intensedebate.com
 		function store() {
 			if ( $this->needs_save ) {
 				$this->compact_operations();
-				if ( 3508545 == $GLOBALS['wpdb']->blog_id && !mt_rand( 0, 99 ) )
-					xmpp_message( 'mdawaffe@im.wordpress.com', print_r( debug_backtrace( false ) ) );
 				id_save_option( $this->queueName, $this->operations );
 			}
 		}
