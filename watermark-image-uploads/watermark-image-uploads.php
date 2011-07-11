0117c65ac426343751b85d3d7955873a36f4dd25
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2011-07-11 22:46:50 +0000

    Cleanup of watermarking plugin. Removing debugging code used during development of plugin. Remove documentation and instead link to the lobby.
    git-svn-id: https://vip-svn.wordpress.com/plugins@48120 dd790b09-5c1c-0410-8744-875116bcd8a6

--- watermark-image-uploads/watermark-image-uploads.php
+++ watermark-image-uploads/watermark-image-uploads.php
@@ -1,54 +1,36 @@
 <?php
 /*
- * Watermarks all future JPEG and PNG uploads to your site.
  *
- * Uses /your-theme/images/upload-watermark.png as the watermark. A PNG is
- * required so that you can have watermark transparency if you wish.
- *
- * By default, the watermark is placed in the bottom-right of the original
- * image with a 5px padding. However you can use the two filters to move
- * the watermark to a different location. Example for bottom-left:
- *
- * add_filter( 'wpcom_watermark_uploads_destx', 'your_function_x', 10, 3 );
- * add_filter( 'wpcom_watermark_uploads_desty', 'your_function_y', 10, 3 );
- * function your_function_x( $dest_x, $image_width, $watermark_width ) {
- *		return 5; // 5px from left side
- * }
- * function your_function_y( $dest_y, $image_height, $watermark_height ) {
- *		return $image_height - $watermark_height - 5; // 5px from bottom
- * }
- *
- * The numbers are relative to the upper-left corner of the original image.
- *
- * To use this plugin, add the following to your theme's functions.php file:
- *
- * include_once( WP_CONTENT_DIR . '/themes/vip/plugins/watermark-image-uploads/watermark-image-uploads.php' );
- *
- * @author Alex M.
+ * For help with this plugin, see http://lobby.vip.wordpress.com/plugins/watermark-image-uploads/
  *
  * Plugin Name: WordPress.com Watermark Image Uploads
+ * Author:      Alex Mills
+ * Author URI:  http://automattic.com/
  */
 
 class WPcom_Watermark_Uploads {
 
+	public $watermark;
+
 	// Class initialization
 	function __construct() {
-		add_filter( 'wp_handle_upload_prefilter', array(&$this, 'handle_file'), 100 );
-		add_filter( 'wp_upload_bits_data',        array(&$this, 'handle_bits'), 10, 2 );
 
-		// $this->debug( 'Watermark: Filters registered.' );
+		$this->watermark = apply_filters( 'wpcom_watermark_image', STYLESHEETPATH . '/images/upload-watermark.png' );
+		if ( ! file_exists( $this->watermark ) )
+			return false;
+
+		add_filter( 'wp_handle_upload_prefilter', array( &$this, 'handle_file' ), 100 );
+		add_filter( 'wp_upload_bits_data',        array( &$this, 'handle_bits' ), 10, 2 );
 	}
 
 	// For filters that pass a $_FILES array
-	function handle_file( $file ) {
-		$this->debug( '[File] Watermark: Filter started.' );
+	public function handle_file( $file ) {
 
 		// Make sure the upload is valid
 		if ( 0 == $file['error'] && is_uploaded_file( $file['tmp_name'] ) ) {
 
 			// Check file extension (can't use $file['type'] due to Flash uploader sending application/octet-stream)
 			if ( !$type = $this->get_type( $file['name'] ) ) {
-				$this->debug( '[File] Watermark: ' . $file['name'] . ' is not a PNG or JPEG.' );
 				return $file;
 			}
 
@@ -56,7 +38,6 @@ class WPcom_Watermark_Uploads {
 			switch ( $type ) {
 				case 'jpeg':
 					if ( !$image = @imagecreatefromjpeg( $file['tmp_name'] ) ) {
-						$this->debug( '[File] Watermark: Failed to load JPEG image.' );
 						return $file;
 					}
 
@@ -70,13 +51,11 @@ class WPcom_Watermark_Uploads {
 
 				case 'png':
 					if ( !$image = @imagecreatefrompng( $file['tmp_name'] ) ) {
-						$this->debug( '[File] Watermark: Failed to load PNG image.' );
 						return $file;
 					}
 					break;
 
 				default;
-					$this->debug( '[File] Watermark: ' . $file['name'] . ' is still not a PNG or JPEG.' );
 					return $file;
 			}
 
@@ -91,29 +70,22 @@ class WPcom_Watermark_Uploads {
 					imagepng( $image, $file['tmp_name'] );
 			}
 
-			$this->debug( '[File] Watermark: Successfully completed.' );
-
 			imagedestroy( $image );
-		} else {
-			$this->debug( '[File] Watermark: Invalid tmp file.' );
 		}
 
 		return $file;
 	}
 
 	// For filters that pass the image as a string
-	function handle_bits( $bits, $file ) {
-		$this->debug( '[Bits] Watermark: Filter started.' );
+	public function handle_bits( $bits, $file ) {
 
 		// Check file extension
-		if ( !$type = $this->get_type( $file ) ) {
-			$this->debug( "[Bits] Watermark: $file is not a PNG or JPEG." );
+		if ( ! $type = $this->get_type( $file ) ) {
 			return $bits;
 		}
 
 		// Convert the $bits into an $image
-		if ( !$image = imagecreatefromstring( $bits ) ) {
-			$this->debug( '[Bits] Watermark: Failed to convert bits to image.' );
+		if ( ! $image = imagecreatefromstring( $bits ) ) {
 			return $bits;
 		}
 
@@ -131,21 +103,18 @@ class WPcom_Watermark_Uploads {
 
 				if ( !imagejpeg( $image, null, $quality ) ) {
 					ob_end_clean();
-					$this->debug( '[Bits] Watermark: Failed to output JPEG.' );
 					return $bits;
 				}
 				break;
 			case 'png':
 				if ( !imagepng( $image ) ) {
 					ob_end_clean();
-					$this->debug( '[Bits] Watermark: Failed to output PNG.' );
 					return $bits;
 				}
 				break;
 
 			default;
 				ob_end_clean();
-				$this->debug( "[Bits] Watermark: $file is still not a PNG or JPEG." );
 				return $bits;
 		}
 		$bits = ob_get_contents();
@@ -153,19 +122,14 @@ class WPcom_Watermark_Uploads {
 
 		imagedestroy( $image );
 
-		$this->debug( '[Bits] Watermark: Successfully completed.' );
-
 		return $bits;
 	}
 
 	// Watermarks an $image
-	function watermark( $image ) {
-		$this->debug( "Watermark: Beginning watermark." );
+	public function watermark( $image ) {
 
 		// Load the watermark into $watermark
-		$watermark_path = STYLESHEETPATH . '/images/upload-watermark.png';
-		if ( !file_exists( $watermark_path ) || !$watermark = @imagecreatefrompng( $watermark_path ) ) {
-			$this->debug( "Watermark: Failed to load watermark image file: {$watermark_path}" );
+		if ( ! $watermark = @imagecreatefrompng( $this->watermark ) ) {
 			return $image;
 		}
 
@@ -177,15 +141,12 @@ class WPcom_Watermark_Uploads {
 		$watermark_width  = imagesx( $watermark );
 		$watermark_height = imagesy( $watermark );
 
-		// Calculate watermark location (see top of file for help with these filters)
+		// Calculate watermark location (see docs for help with these filters)
 		$dest_x = (int) apply_filters( 'wpcom_watermark_uploads_destx', $image_width - $watermark_width - 5, $image_width, $watermark_width );
 		$dest_y = (int) apply_filters( 'wpcom_watermark_uploads_desty', $image_height - $watermark_height - 5, $image_height, $watermark_height );
 
 		// Copy the watermark onto the original image
-		if ( !imagecopy( $image, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height ) )
-			$this->debug( "Watermark: Failed to apply watermark at {$dest_x}x{$dest_y}." );
-		else
-			$this->debug( "Watermark: Successfully applied watermark at {$dest_x}x{$dest_y}." );
+		imagecopy( $image, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height );
 
 		imagedestroy( $watermark );
 
@@ -194,22 +155,21 @@ class WPcom_Watermark_Uploads {
 
 	// Safety wrapper for our get_jpeg_quality() function
 	// See http://blog.apokalyptik.com/2009/09/16/quality-time-with-your-jpegs/
-	function get_jpeg_quality_wrapper( $imagecontent ) {
+	public function get_jpeg_quality_wrapper( $imagecontent ) {
 
 		$quality = false;
 
-		if ( !function_exists('get_jpeg_quality') )
+		if ( ! function_exists( 'get_jpeg_quality' ) )
 			@include_once( WP_PLUGIN_DIR . '/wpcom-images/libjpeg.php' );
-		if ( function_exists('get_jpeg_quality') )
-			$quality = get_jpeg_quality( $imagecontent );
 
-		$this->debug( "Watermark: JPEG quality is {$quality}." );
+		if ( function_exists( 'get_jpeg_quality' ) )
+			$quality = get_jpeg_quality( $imagecontent );
 
 		return $quality;
 	}
 
 	// Figure out image type based on filename
-	function get_type( $filename ) {
+	public function get_type( $filename ) {
 		$wp_filetype = wp_check_filetype( $filename );
 		switch ( $wp_filetype['ext'] ) {
 			case 'png':
@@ -219,16 +179,6 @@ class WPcom_Watermark_Uploads {
 				return 'jpeg';
 			default;
 				return false;
-		}
-	}
-
-
-	// Report any errors to me, but only while on my sandbox. Does nothing otherwise.
-	function debug( $message ) {
-		global $blog_id;
-
-		if ( function_exists('xmpp_message') && defined('ALEXM_SANDBOX') && ALEXM_SANDBOX ) {
-			xmpp_message( 'viper007bond@im.wordpress.com', '[' . $blog_id . '] ' . $message );
 		}
 	}
 }
