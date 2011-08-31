ea4c4859d3c7613a20789f4cdb0cbdb3ab18cd4b
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2011-08-31 17:34:09 +0000

    External Permalinks: Add option to select the redirect status code. Default remains at 307 but can be filtered.
    git-svn-id: https://vip-svn.wordpress.com/plugins@50131 dd790b09-5c1c-0410-8744-875116bcd8a6

--- external-permalinks/external-permalinks.php
+++ external-permalinks/external-permalinks.php
@@ -33,8 +33,14 @@ add_filter( 'page_link', 'external_permalinks_filter_page_permalink', 1, 2 );
  * @return null
  */
 function external_permalinks_redirect_postpage_request( $query ) {
-	if( is_single() && $link = get_post_meta( get_the_ID(), '_external_permalink', true ) ) {
-		wp_redirect( $link, 307 );
+	$post_id = get_the_ID();
+	if( is_single() && $link = get_post_meta( $post_id, '_external_permalink', true ) ) {
+	
+		$status_code = (int) get_post_meta( $post_id, '_external_permalink_status_code', true );
+		if( ! in_array( $status_code, external_permalinks_get_status_codes() ) )
+			$status_code = external_permalinks_default_status_code();
+		
+		wp_redirect( $link, $status_code );
 		exit;
 	}
 }
@@ -58,13 +64,27 @@ add_action( 'admin_init', 'external_permalinks_add_meta_box' );
  * @return html
  */
 function external_permalinks_meta_box( $post ) {
-	$link = esc_url( get_post_meta( $post->ID, '_external_permalink', true ) );
+	$link = get_post_meta( $post->ID, '_external_permalink', true );
+	$status_code = (int) get_post_meta( $post->ID, '_external_permalink_status_code', true );
+	$all_status_codes = external_permalinks_get_status_codes();
+	
 ?>
 	<div class="inside">
 		<label for="external-permalink-url"><?php _e( 'Destination Address:' ); ?></label><br />
-		<input name="external-permalink-url" id="external-permalink-url" type="text" value="<?php echo $link; ?>" style="width:99%;" />
+		<input name="external-permalink-url" id="external-permalink-url" type="text" value="<?php echo esc_url( $link ); ?>" style="width:99%;" />
 		<p><strong><?php printf( __( 'Be sure to include %s before the destination address.' ), '<em>http://</em>' );?></strong></p>
 		<p><?php _e( 'To restore the original permalink, remove the link entered above.' ); ?></p>
+		
+		<p>
+			<label for="external-permalink-status-code"><?php _e( 'Status Code:' ); ?></label><br />
+			<select name="external-permalink-status-code" id="external-permalink-status-code" style="width:20%;">
+				<option value=""><?php _e( '-- Select --' ) ?></option>
+				<?php foreach( $all_status_codes as $code ) : ?>
+					<option <?php selected( $status_code, $code ); ?>><?php echo esc_html( $code ); ?></option>
+				<?php endforeach; ?>
+			</select>
+		</p>
+		
 		<input type="hidden" name="external-permalink-url-old" value="<?php echo $link; ?>" />
 		<input type="hidden" name="external-permalink-nonce" value="<?php echo wp_create_nonce( 'external-permalinks' ); ?>" />
 	</div>
@@ -87,6 +107,21 @@ function external_permalinks_save_link() {
 		elseif( empty( $_POST[ 'external-permalink-url' ] ) && !empty( $_POST[ 'external-permalink-url-old' ] ) ) {
 			delete_post_meta( $post->ID, '_external_permalink' );
 		}
+		
+		if( ! empty( $_POST[ 'external-permalink-status-code' ] ) && in_array( $_POST[ 'external-permalink-status-code' ], external_permalinks_get_status_codes() ) ) {
+			$status_code = intval( $_POST[ 'external-permalink-status-code' ] );
+			update_post_meta( $post->ID, '_external_permalink_status_code', $status_code );
+		} else {
+			delete_post_meta( $post->ID, '_external_permalink_status_code' );
+		}
 	}
 }
 add_action( 'save_post', 'external_permalinks_save_link' );
+
+function external_permalinks_default_status_code() {
+	return apply_filters( 'external_permalinks_default_status_code', 307 );
+}
+
+function external_permalinks_get_status_codes() {
+	return array( 301, 302, 307 );
+}
