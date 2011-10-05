faf7cac70b5297f108328345401ca4509b89968c
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2011-10-05 01:07:44 +0000

    Tweaks and improvements for New Device Notifications.
    * Add a week grace period after plugin activation during which no e-mails will be sent. This allows all regular users to receive the cookie without spamming everyone.
    * Move the plugin to only run in the admin area. Logging on the front end would be best, but this is v1.0 or whatever. Keeps the spam down for now.
    * Set a memcached key unique to the user + IP address + user agent that serves as a backup to the cookie. The cookie is better as it's (mostly) persistent and will stay valid across dynamic IP changes, but this cache key will prevent e-mail spam when cookies are disabled.
    * E-mail content improvements thanks to Paul.
    * Tweak how the e-mail is sent: send it to all moderators and the blog owner and CC the user in question. Also send a single e-mail to everyone instead of an e-mail per person.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@51787 dd790b09-5c1c-0410-8744-875116bcd8a6

--- new-device-notification/new-device-notification.php
+++ new-device-notification/new-device-notification.php
@@ -11,16 +11,23 @@ Author URI:   http://vip.wordpress.com/
 
 class New_Device_Notification {
 
+	// Notifications won't be sent for a certain period of time after the plugin is enabled.
+	// This is to get all of the normal users into the logs and avoid spamming inboxes.
+	public $grace_period = 604800; // 1 week
+
 	public $cookie_name = 'deviceseenbefore';
 
 	public $cookie_hash;
 
 	function __construct() {
-		add_action( 'init', array( &$this, 'init' ), 99 );
+		// Log when this plugin was first activated
+		add_option( 'newdevicenotification_installedtime', time() );
 
+		// Wait until "admin_init" to do anything else
+		add_action( 'admin_init', array( &$this, 'admin_init' ), 99 );
 	}
 
-	public function init() {
+	public function admin_init() {
 		global $current_user;
 
 		// IP whitelist
@@ -36,19 +43,32 @@ class New_Device_Notification {
 		if ( is_super_admin() || ! current_user_can( 'edit_posts' ) || sms_user_has_two_step_auth( $current_user->ID ) )
 			return;
 
+		// Set up the per-blog salt
 		$salt = get_option( 'newdevicenotification_salt' );
 		if ( ! $salt ) {
 			$salt = wp_generate_password( 64, true, true );
 			add_option( 'newdevicenotification_salt', $salt );
 		}
 
-		$this->cookie_hash = hash_hmac( 'md5', $current_user->user_login , $salt );
+		$this->cookie_hash = hash_hmac( 'md5', $current_user->ID, $salt );
 
+		// Seen this device before?
 		if ( $this->verify_cookie() )
 			return;
 
+		// Attempt to mark this device as seen via a cookie
 		$this->set_cookie();
+
+		// Maybe we've seen this user+IP+agent before but they don't accept cookies?
+		$memcached_key = 'lastseen_' . $current_user->ID . '_' . md5( $_SERVER['REMOTE_ADDR'] . '|' . $_SERVER['HTTP_USER_AGENT'] );
+		if ( wp_cache_get( $memcached_key, 'newdevicenotification' ) )
+			return;
+
+		// As a backup to the cookie, record this IP address (only in memcached for now, proper logging will come later)
+		wp_cache_set( $memcached_key, time(), 'newdevicenotification' );
+
 		$this->notify_of_new_device();
+
 	}
 
 	public function verify_cookie() {
@@ -83,56 +103,71 @@ class New_Device_Notification {
 		$location = $this->ip_to_city( $_SERVER['REMOTE_ADDR'] );
 		$blogname = html_entity_decode( get_bloginfo( 'name' ), ENT_QUOTES );
 
-		$subject = sprintf( apply_filters( 'ndn_subject', '[%1$s] %2$s has logged in from an unknown computer' ), $blogname, $current_user->display_name );
+		send_vip_team_debug_message( "[NDN] New device detected for {$current_user->user_login} on " . parse_url( home_url(), PHP_URL_HOST ) . ': ' . $_SERVER['REMOTE_ADDR'] . " ({$location->human}) using " . $_SERVER['HTTP_USER_AGENT'] );
+
+		// If we're still in the grace period, don't send an e-mail
+		$installed_time = get_option( 'newdevicenotification_installedtime' );
+		if ( time() - $installed_time < $this->grace_period )
+			return false;
+
+		$subject = sprintf( apply_filters( 'ndn_subject', '[%1$s] %2$s has logged in from an unknown device' ), $blogname, $current_user->display_name );
 		$message = sprintf( apply_filters( 'ndn_message',
 'Hello,
 
-This automated e-mail to all site moderators is to inform you that %1$s has logged into %2$s ( %3$s ) from a computer that the site has never seen them log in from before.
+This is an automated email to all site moderators to inform you that %1$s has logged into %2$s ( %3$s ) from a device that we don\'t recognize or that had last been used before %9$s when this monitoring was first enabled.
 
-Chances are that they are just logging in from a new browser or computer and that this e-mail can safely be ignored.
+While they are likely simply logging in from a new web browser or computer (in which case this email can be safely ignored), there is also a chance that their account has been compromised and someone else has logged into their account.
 
-However there is also a chance that their account has been compromised and someone else has logged into their account.
+Here are some details about the login to help verify if it was legitimate:
 
-Here are some details about the login to help verify if it was legitimate or not:
-
+WP.com Username: %8$s
 IP Address: %4$s
 Hostname: %5$s
-Location Based On IP Address: %6$s  (this is an educated guess)
+Guessed Location: %6$s
 Browser User Agent: %7$s
 
-If you think this log in is potentially not legitmate, please immediately reply to this e-mail so that the WordPress.com VIP staff can assist you in revoking the user\'s access.
+If you believe that this log in was unauthorized, please immediately reply to this e-mail and our VIP team will work with you to remove this user\'s access.
+
+You should also immediately have the user change their password:
+
+http://support.wordpress.com/passwords/
 
 Feel free to also reply to this e-mail if you have any questions whatsoever.
 
-- Your WordPress.com VIP Support Team' ),
-
-			$current_user->display_name,              // 1
-			$blogname,                                // 2
-			trailingslashit( home_url() ),            // 3
-			$_SERVER['REMOTE_ADDR'],                  // 4
-			gethostbyaddr( $_SERVER['REMOTE_ADDR'] ), // 5
-			$location->human,                         // 6
-			strip_tags( $_SERVER['HTTP_USER_AGENT'] ) // 7, strip_tags() is better than nothing
+- WordPress.com VIP' ),
+			$current_user->display_name,               // 1
+			$blogname,                                 // 2
+			trailingslashit( home_url() ),             // 3
+			$_SERVER['REMOTE_ADDR'],                   // 4
+			gethostbyaddr( $_SERVER['REMOTE_ADDR'] ),  // 5
+			$location->human,                          // 6
+			strip_tags( $_SERVER['HTTP_USER_AGENT'] ), // 7, strip_tags() is better than nothing
+			$current_user->user_login,                 // 8
+			date( 'F jS, Y', $installed_time )         // 9, Not adjusted for timezone but close enough
 		);
 
 		// "admin_email" plus any e-mails passed to the vip_multiple_moderators() function
-		$emails = array_unique( apply_filters( 'wpcom_vip_multiple_moderators', array( get_option( 'admin_email' ), $current_user->user_email ) ) );
+		$emails = array_unique( apply_filters( 'wpcom_vip_multiple_moderators', array( get_option( 'admin_email' ) ) ) );
 
-		$headers = 'From: WordPress.com VIP Support <vip-support@wordpress.com>' . "\r\n";
+		$headers  = 'From: "WordPress.com VIP Support" <vip-support@wordpress.com>' . "\r\n";
+		$headers .= 'CC: ' . $current_user->user_email . "\r\n";
 
-		foreach ( $emails as $email ) {
-			wp_mail( $email, $subject, $message, $headers );
-		}
+		wp_mail( $emails, $subject, $message, $headers );
+
+		return true;
 	}
 
 	public function ip_to_city( $ip ) {
 		$location = ip2location( $ip );
 
 		$human = array();
+
 		if ( ! empty( $location->city ) && '-' != $location->city )
 			$human[] = $location->city;
+
 		if ( ! empty( $location->region ) && '-' != $location->region && ( empty( $location->city ) || $location->region != $location->city ) )
 			$human[] = $location->region;
+
 		if ( ! empty( $location->country_long ) && '-' != $location->country_long )
 			$human[] = $location->country_long;
 
