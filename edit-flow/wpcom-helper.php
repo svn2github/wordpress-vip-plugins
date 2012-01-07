8cbddf9b4769c5420ddbab30259c281324e8fb97
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-01-07 00:15:28 +0000

    Load the Edit Flow caps in the proper way so users can actually use it
    git-svn-id: https://vip-svn.wordpress.com/plugins@56230 dd790b09-5c1c-0410-8744-875116bcd8a6

--- edit-flow/wpcom-helper.php
+++ edit-flow/wpcom-helper.php
@@ -4,6 +4,49 @@
  * them with the WP.com apprach.
  */
 add_filter( 'ef_kill_add_caps_to_role', '__return_true' );
+$custom_roles = array(
+	'administrator' => array(
+		'capabilities' => array (
+			'ef_view_calendar' => true,
+			'edit_post_subscriptions' => true,
+			'edit_usergroups' => true,
+			'ef_view_story_budget' => true,
+		),
+	),
+	'editor' => array(
+		'capabilities' => array (
+			'ef_view_calendar' => true,
+			'edit_post_subscriptions' => true,
+			'ef_view_story_budget' => true,
+		),
+	),
+	'author' => array(
+		'capabilities' => array (
+			'ef_view_calendar' => true,
+			'edit_post_subscriptions' => true,
+			'ef_view_story_budget' => true,
+		),
+	),
+	'contributor' => array(
+		'capabilities' => array (
+			'ef_view_calendar' => true,
+			'ef_view_story_budget' => true,
+		),
+	),
+);
+global $wp_user_roles;
+foreach ( $custom_roles as $role_name => $role_args ) {
+	if ( isset( $wp_user_roles[ $role_name ] ) ) {
+		foreach ( $role_args as $arg_name => $arg_values ) {
+			if ( is_array( $wp_user_roles[ $role_name ][ $arg_name ] ) )
+				$wp_user_roles[ $role_name ][ $arg_name ] = array_merge( $wp_user_roles[ $role_name ][ $arg_name ], $arg_values ); // Change this if the caps should be completely overwritten
+			else
+				$wp_user_roles[ $role_name ][ $arg_name ] = $arg_values;
+		}
+	} else {
+		$wp_user_roles[ $role_name ] = $role_args;
+	}
+}
 
 /**
  * Edit Flow loads modules after plugins_loaded, which has already been fired on WP.com
