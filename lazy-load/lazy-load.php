ea12b2bd898c4a697c77fa0100c906795a1ca779
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-01-23 15:09:11 +0000

    plugins: lazy-load - sync changes from wp.org v0.2
    git-svn-id: https://vip-svn.wordpress.com/plugins@57068 dd790b09-5c1c-0410-8744-875116bcd8a6

--- lazy-load/lazy-load.php
+++ lazy-load/lazy-load.php
@@ -2,6 +2,7 @@
 /**
  * Plugin Name: Lazy Load
  * Description: Lazy load images to improve page load times. Uses jQuery.sonar to only load an image when it's visible in the viewport.
+ * Version: 0.2
  *
  * Code by the WordPress.com VIP team, TechCrunch 2011 Redesign team, and Jake Goldman (10up LLC).
  * Uses jQuery.sonar by Dave Artz (AOL): http://www.artzstudio.com/files/jquery-boston-2010/jquery.sonar/ 
@@ -11,13 +12,12 @@
 
 class LazyLoad_Images {
 
-	const version = '0.1';
-
-	var $base_url = '';
+	const version = '0.2';
 
 	function __construct() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'add_scripts' ) );
 		add_filter( 'the_content', array( $this, 'add_image_placeholders' ) );
+		add_filter( 'post_thumbnail_html', array( $this, 'add_image_placeholders' ) );
 	}
 
 	function add_scripts() {
@@ -31,10 +31,10 @@ class LazyLoad_Images {
 			return $content;
 
 		// In case you want to change the placeholder image
-		$image = apply_filters( 'lazyload_images_placeholder_image', $this->get_url( 'images/1x1.trans.gif' ) );
+		$placeholder_image = apply_filters( 'lazyload_images_placeholder_image', $this->get_url( 'images/1x1.trans.gif' ) );
 
 		// This is a pretty simple regex, but it works
-		$content = preg_replace( '#<img([^>]+?)src=#', sprintf( '<img${1}src="%s" data-lazy-src=', $image ), $content );
+		$content = preg_replace( '#<img([^>]+?)src=[\'"]([^\'">]*)[\'"]([^>]*)>#', sprintf( '<img${1}src="%s" data-lazy-src="${2}"${3}><noscript><img${1}src="${2}"${3}></noscript>', $placeholder_image ), $content );
 
 		return $content;
 	}
---
+++ lazy-load/readme.txt
@@ -0,0 +1,37 @@
+=== Lazy Load ===
+Contributors: batmoo, automattic, jakemgold, get10up
+Tags: lazy load, images, front-end optimization
+Requires at least: 3.2
+Tested up to: 3.3
+Stable tag: 0.2
+
+Lazy load images to improve page load times and server bandwidth. Images are loaded only when visible to the user.
+
+== Description ==
+
+Lazy load images to improve page load times. Uses jQuery.sonar to only load an image when it's visible in the viewport.
+
+This plugin is an amalgamation of code written by the WordPress.com VIP team at Automattic, the TechCrunch 2011 Redesign team, and Jake Goldman (10up LLC).
+
+Uses <a href="http://www.artzstudio.com/files/jquery-boston-2010/jquery.sonar/ ">jQuery.sonar</a> by Dave Artz (AOL).
+
+== Installation ==
+
+1. Upload the plugin to your plugins directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Enjoy!
+
+== Screenshots ==
+
+No applicable screenshots
+
+== Changelog ==
+
+= 0.2 =
+
+* Adds noscript tags to allow the image to show up in no-js contexts (including crawlers), props smub
+* Lazy Load post thumbnails, props ivancamilov
+
+= 0.1 =
+
+* Initial working version
