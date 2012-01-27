49df9defb63434dfde822f360a3bfe19dfc86b82
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-01-27 23:50:19 +0000

    Pass additional arguments to the filter to allow for finer grain control
    git-svn-id: https://vip-svn.wordpress.com/plugins@57371 dd790b09-5c1c-0410-8744-875116bcd8a6

--- watermark-image-uploads/watermark-image-uploads.php
+++ watermark-image-uploads/watermark-image-uploads.php
@@ -26,7 +26,7 @@ class WPcom_Watermark_Uploads {
 	// For filters that pass a $_FILES array
 	public function handle_file( $file ) {
 
-		if ( false === apply_filters( 'wpcom_watermark_enabled', true ) )
+		if ( false === apply_filters( 'wpcom_watermark_enabled', true, 'file', $file ) )
 			return $file;
 
 		// Make sure the upload is valid
@@ -82,7 +82,7 @@ class WPcom_Watermark_Uploads {
 	// For filters that pass the image as a string
 	public function handle_bits( $bits, $file ) {
 
-		if ( false === apply_filters( 'wpcom_watermark_enabled', true ) )
+		if ( false === apply_filters( 'wpcom_watermark_enabled', true, 'bits', $file ) )
 			return $bits;
 
 		// Check file extension
