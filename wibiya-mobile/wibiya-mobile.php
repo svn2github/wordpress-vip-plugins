b086b297cfb4020e38a9a4cf2e398f69a629c6b4
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-03-28 18:41:15 +0000

    Sync over new changes from Wibiya.
    1040: removed admin panel from mobile redirect
    1041: Fixed ipad issue
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@61316 dd790b09-5c1c-0410-8744-875116bcd8a6

--- wibiya-mobile/wibiya-mobile.php
+++ wibiya-mobile/wibiya-mobile.php
@@ -101,7 +101,7 @@ function ap_wibiya_mobile_options_page()
             ap_wibiya_mobile_check_site();
             break;
     }
-	if(($GLOBALS['ap_wibiya_mobile_policy']>=__POLICY_DISABLE_USER__) && ($GLOBALS['ap_wibiya_mobile_register']!=1))
+	if((($GLOBALS['ap_wibiya_mobile_policy']>=__POLICY_DISABLE_USER__) && ($GLOBALS['ap_wibiya_mobile_register']!=1))||((get_option('ap_wibiya_mobile_url')=="")&&$GLOBALS['ap_wibiya_mobile_register']==1))
 	{ ap_wibiya_check_recovery(); }
     ap_wibiya_mobile_show_livepreview();
 }
@@ -152,7 +152,9 @@ function ap_wibiya_check_recovery()
 	{
 		$GLOBALS['ap_wibiya_mobile_register'] = 1;
 		update_option('ap_wibiya_mobile_register', 1);
-		update_option('ap_wibiya_mobile_url', base64_decode($_GET['url']));
+		$url=base64_decode($responseRec['url']);
+		if($url!="")
+		{ update_option('ap_wibiya_mobile_url',$url ); }
 	}
 }
 function ap_wibiya_mobile_update_policy($policy)
@@ -360,7 +362,7 @@ function ap_wibiya_mobile_theme_enable()
 function ap_wibiya_mobile_is_mobile()
 {
     $useragent = $_SERVER['HTTP_USER_AGENT'];
-    if (preg_match('/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4))) {
+    if (preg_match('/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|iPod|iPad|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4))) {
         return true;
     }
     return false;
@@ -380,21 +382,20 @@ function wibiya_mobile_run()
         add_action('admin_head', 'ap_wibiya_mobile_header');
     } else {
 
-	// We shouldn't be redirecting XML-RPC requests
-	if ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST )
-		return;
-
-        $is_mobile = false;
-        if ((function_exists('is_mobile') && is_mobile('any', false)) || (ap_wibiya_mobile_is_mobile() == true)) {
-            $is_mobile = true;
-        }
-        if ($is_mobile && isset($GLOBALS['ap_wibiya_mobile_install']) && $GLOBALS['ap_wibiya_mobile_install'] == 1 && $GLOBALS['ap_wibiya_mobile_policy'] >= __POLICY_ACTIVE_USER__ && get_option('ap_wibiya_mobile_url')&&(!isset($_GET['noredirect']))&&(!isset($_GET['feed']))) {
-			if(is_home())
+		// We shouldn't be redirecting XML-RPC requests
+		if ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ){  return; }						
+		$is_mobile = false;		
+        if ((!strstr($_SERVER['REQUEST_URI'],'noredirect'))&&(!strstr($_SERVER['REQUEST_URI'],'wp-admin'))&&(!strstr($_SERVER['REQUEST_URI'],'feed'))&&(!strstr($_SERVER['REQUEST_URI'],'wp-login'))&&((function_exists('is_mobile') && is_mobile('any', false)) || is_ipad() ||  (ap_wibiya_mobile_is_mobile() == true))) {
+			$is_mobile = true;
+		}
+        if ($is_mobile && isset($GLOBALS['ap_wibiya_mobile_install']) && $GLOBALS['ap_wibiya_mobile_install'] == 1 && $GLOBALS['ap_wibiya_mobile_policy'] >= __POLICY_ACTIVE_USER__ && get_option('ap_wibiya_mobile_url')) {
+			if($_SERVER['REQUEST_URI']=="/"||$_SERVER['REQUEST_URI']=="")
 			{  $mobile_url=get_option('ap_wibiya_mobile_url').'?url=&cms=wordpress&timestamp='.time();    }
-			else{ $mobile_url=get_option('ap_wibiya_mobile_url').'?url='.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'&cms=wordpress&timestamp='.time();  }	
+			else
+			{ $mobile_url=get_option('ap_wibiya_mobile_url').'?url='.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'&cms=wordpress&timestamp='.time();  }	
 			wp_redirect($mobile_url);
-            exit;
-        }
+			exit;
+		}
     }
 }
 if(function_exists('register_activation_hook'))
