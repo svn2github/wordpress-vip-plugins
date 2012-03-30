8b13ef0a59679cfe80f25defadb2f9aab542dc20
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-03-30 15:02:46 +0000

    Make Lazy Load play nicely with infinite scroll by rehooking events on post-load event
    git-svn-id: https://vip-svn.wordpress.com/plugins@61502 dd790b09-5c1c-0410-8744-875116bcd8a6

--- lazy-load/js/lazy-load.js
+++ lazy-load/js/lazy-load.js
@@ -1,13 +1,18 @@
 (function($) {
-	$( 'img[data-lazy-src]' ).bind( 'scrollin', { distance: 200 }, function() {
-		var img = this,
-			$img = jQuery(img),
-			src = $img.attr( 'data-lazy-src' );
-		$img.unbind( 'scrollin' ) // remove event binding
-			.hide()
-			.removeAttr( 'data-lazy-src' )
-			.attr( 'data-lazy-loaded', 'true' );;
-		img.src = src;
-		$img.fadeIn();
-	});
+	lazy_load_init();
+	$( 'body' ).bind( 'post-load', lazy_load_init ); // Work with WP.com infinite scroll
+
+	function lazy_load_init() {
+		jQuery( 'img[data-lazy-src]' ).bind( 'scrollin', { distance: 200 }, function() {
+			var img = this,
+				$img = jQuery(img),
+				src = $img.attr( 'data-lazy-src' );
+			$img.unbind( 'scrollin' ) // remove event binding
+				.hide()
+				.removeAttr( 'data-lazy-src' )
+				.attr( 'data-lazy-loaded', 'true' );;
+			img.src = src;
+			$img.fadeIn();
+		});
+	}
 })(jQuery);
