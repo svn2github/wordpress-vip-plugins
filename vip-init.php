d095dfdc5eb250cb6a72649908452cdfc2850b3e
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-04-05 02:13:35 +0000

    vip-init should load the powered by helper
    git-svn-id: https://vip-svn.wordpress.com/plugins@62159 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -3,7 +3,8 @@
 define( 'WPCOM_IS_VIP_ENV', function_exists( 'wpcom_is_vip' ) );
 
 // Load our development and environment helpers
-require_once( WP_CONTENT_DIR . '/themes/vip/plugins/vip-do-not-include-on-wpcom/vip-local-development-helper/vip-local-development-helper.php' );
+require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-local-development-helper/vip-local-development-helper.php' );
+require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powered-wpcom.php' );
 
 // Load the WordPress.com dependent helper files, only on WordPress.com (for now)
 if ( function_exists( 'wpcom_is_vip' ) ) {
