2df25429336d070705ee4ed573f48103348b9c59
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-04-09 15:13:52 +0000

    Load other local helpers in our init file
    git-svn-id: https://vip-svn.wordpress.com/plugins@62554 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -10,6 +10,11 @@ require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powe
 if ( function_exists( 'wpcom_is_vip' ) ) {
 	wpcom_vip_load_helper_wpcom(); // vip-helper-wpcom.php
 	wpcom_vip_load_helper_stats(); // vip-helper-wpcom.php
+} else {
+	// Local helpers that add WP.com functionality
+	require_once( __DIR__ . '/vip-do-not-include-on-wpcom/is-mobile.php' );
+	require_once( __DIR__ . '/vip-do-not-include-on-wpcom/wpcom-functions.php' );
+	require_once( __DIR__ . '/vip-do-not-include-on-wpcom/wpcom-shortcodes.php' );
 }
 
 // Load the "works everywhere" helper file
