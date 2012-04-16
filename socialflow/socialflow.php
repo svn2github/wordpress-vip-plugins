d593e8f773ad0f976a498ecd907fd294556d28e8
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-04-16 03:21:06 +0000

    Prevent SocialFlow js from being loaded for Pages until proper custom post type support is added, props rinatkhaziev
    git-svn-id: https://vip-svn.wordpress.com/plugins@63310 dd790b09-5c1c-0410-8744-875116bcd8a6

--- socialflow/socialflow.php
+++ socialflow/socialflow.php
@@ -19,6 +19,7 @@ class SocialFlow_Plugin {
 
 	public function __construct() {
 		global $pagenow;
+		global $typenow;
 		self::$instance = $this;
 
 		$this->l10n = array(
@@ -95,8 +96,8 @@ class SocialFlow_Plugin {
 		add_meta_box( 'socialflow', __( 'SocialFlow', 'socialflow' ), array( $this, 'display_compose_form' ), 'post', 'side', 'high', array( 'post_page' => true ) );
 	}
 
-	public function enqueue( $hook ) {		
-		if ( in_array( $hook, array( 'index.php', 'post-new.php', 'post.php' ) ) && ( !isset( $_REQUEST['post_type'] ) || in_array( $_REQUEST['post_type'], array( 'post' ) ) ) ) {
+	public function enqueue( $hook ) {
+		if ( in_array( $hook, array( 'index.php', 'post-new.php', 'post.php' ) ) && ( in_array( $_REQUEST['post_type'], array( 'post' ) ) || in_array( $typenow, array( 'post' ) ) ) ) {
 			$color = 'fresh' == get_user_meta( get_current_user_id(), 'admin_color', true ) ? '#F1F1F1' : '#F5FAFD';
 			?>
 <style type="text/css">
