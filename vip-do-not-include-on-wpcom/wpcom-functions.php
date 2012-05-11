4affa19f7d1f290024e66922c902a783c457cb67
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-05-11 04:18:10 +0000

    Add widnot and wido to local development functions so VIP devs can feel the frustration of widont everywhere!
    git-svn-id: https://vip-svn.wordpress.com/plugins@65658 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/wpcom-functions.php
+++ vip-do-not-include-on-wpcom/wpcom-functions.php
@@ -60,5 +60,20 @@ if ( ! function_exists( 'wpcom_is_vip' ) ) : // Do not load these on WP.com
 	function delete_user_attribute( $user_id, $meta_key, $meta_value = '' ) {
 		return delete_user_meta( $user_id, $meta_key, $meta_value );
 	}
-	
+
+	// These functions are defined on WordPress.com and can be a common source of frustration for VIP devs
+	// Now they can be frustrated in their local environments as well :)
+	if ( ! function_exists( 'widont' ) ) :
+		function widont( $str = '' ) {
+			return preg_replace( '|([^\s])\s+([^\s]+)\s*$|', '$1&nbsp;$2', $str );
+		}
+		add_filter( 'the_title', 'widont' );
+	endif;
+
+	if ( ! function_exists( 'widont' ) ) :
+		function wido( $str = '' ) {
+			return str_replace( '&#160;', ' ', $str );
+		}
+		add_filter( 'the_title_rss', 'wido' );
+	endif;
 endif;
