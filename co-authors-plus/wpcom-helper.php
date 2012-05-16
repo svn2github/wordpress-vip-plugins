977a0bac0c7575a7a8853fd957b9821b54e58887
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-05-16 22:46:56 +0000

    Co-Authors Plus: Make the parent page filterable to allow the user some flexibility. On WordPress.com, grofiles.php does some ugly stuff to profile.php we need to work around
    git-svn-id: https://vip-svn.wordpress.com/plugins@66072 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -8,3 +8,9 @@ $coauthors_plus_guest_authors_whitelist = array(
 	);
 if ( !in_array( get_option( 'stylesheet' ), $coauthors_plus_guest_authors_whitelist ) )
 	add_filter( 'coauthors_guest_authors_enabled', '__return_false' );
+
+/**
+ * There's some ugly code in mu-plugins/gprofiles.php that redirects anything on profile.php that's not 
+ * a whitelisted gprofile page. Let's put it under tools instead.
+ */
+add_filter( 'coauthors_guest_author_parent_page', function() { return 'tools.php'; } );
