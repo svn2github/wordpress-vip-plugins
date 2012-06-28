4ecd80a6601c85b49809e44a9681d70011230414
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-06-28 18:51:02 +0000

    JSON Feed: Remove potential XSS attack vector. See http://www.metaltoad.com/blog/using-jsonp-safely for details.
    git-svn-id: https://vip-svn.wordpress.com/plugins@69844 dd790b09-5c1c-0410-8744-875116bcd8a6

--- json-feed/json-feed.php
+++ json-feed/json-feed.php
@@ -58,7 +58,7 @@ function json_feed() {
 		echo json_encode($output);
 	} else {
 		header('Content-Type: application/javascript; charset=' . get_option('blog_charset'), true);
-		echo get_query_var('jsonp') . '(' . json_encode($output) . ')';
+		echo preg_replace( '/\W/', '', get_query_var('jsonp') ) . '(' . json_encode($output) . ')';
 	}
 }
 
