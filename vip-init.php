f2b5bb9d4d3d8a24118e6a37c7ea03d91f92cdc8
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-07-11 20:40:06 +0000

    Make use of the WPCOM_IS_VIP_ENV constant instead of checking to see if the function exists again
    git-svn-id: https://vip-svn.wordpress.com/plugins@71202 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -9,7 +9,7 @@ require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-roles.php' );
 require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-permastructs.php' );
 
 // Load the WordPress.com dependent helper files, only on WordPress.com (for now)
-if ( function_exists( 'wpcom_is_vip' ) ) {
+if ( WPCOM_IS_VIP_ENV ) {
 	wpcom_vip_load_helper_wpcom(); // vip-helper-wpcom.php
 	wpcom_vip_load_helper_stats(); // vip-helper-wpcom.php
 } else {
