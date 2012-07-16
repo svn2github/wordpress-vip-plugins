1bc3b1143bb14c4c5164d529900c1af953e580e5
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-07-16 06:18:40 +0000

    lazy-load: We need to force load gallery images in Carousel and give up lazy-loading otherwise images don't show up correctly
    Also adds new helper function lazy_load_image, which handles the actual loading of the image.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@71520 dd790b09-5c1c-0410-8744-875116bcd8a6

--- lazy-load/js/lazy-load.js
+++ lazy-load/js/lazy-load.js
@@ -3,16 +3,28 @@
 	$( 'body' ).bind( 'post-load', lazy_load_init ); // Work with WP.com infinite scroll
 
 	function lazy_load_init() {
-		jQuery( 'img[data-lazy-src]' ).bind( 'scrollin', { distance: 200 }, function() {
-			var img = this,
-				$img = jQuery(img),
-				src = $img.attr( 'data-lazy-src' );
-			$img.unbind( 'scrollin' ) // remove event binding
-				.hide()
-				.removeAttr( 'data-lazy-src' )
-				.attr( 'data-lazy-loaded', 'true' );;
-			img.src = src;
-			$img.fadeIn();
+		$( 'img[data-lazy-src]' ).bind( 'scrollin', { distance: 200 }, function() {
+			lazy_load_image( this );
 		});
+
+		// We need to force load gallery images in Carousel and give up lazy-loading otherwise images don't show up correctly
+		$( '[data-carousel-extra]' ).each( function() {
+			$( this ).find( 'img[data-lazy-src]' ).each( function() {
+				lazy_load_image( this );
+			} );		
+		} );
+	}
+
+	function lazy_load_image( img ) {
+		var $img = jQuery( img ),
+			src = $img.attr( 'data-lazy-src' );
+
+		$img.unbind( 'scrollin' ) // remove event binding
+			.hide()
+			.removeAttr( 'data-lazy-src' )
+			.attr( 'data-lazy-loaded', 'true' );
+
+		img.src = src;
+		$img.fadeIn();
 	}
 })(jQuery);
