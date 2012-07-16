e4e47bbd53ed26f3894088bf6b21a0dbe7ece897
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-07-16 18:30:10 +0000

    kapost: escape javascript variables and verify that the kapost URL from meta is a valid domain.
    git-svn-id: https://vip-svn.wordpress.com/plugins@71577 dd790b09-5c1c-0410-8744-875116bcd8a6

--- kapost-byline/kapost-byline.php
+++ kapost-byline/kapost-byline.php
@@ -169,6 +169,28 @@ function kapost_byline_update_post($id, $custom_fields, $uid=false, $blog_id=fal
 	return true;
 }
 
+function kapost_byline_verify_analytics_url($url)
+{
+	if(empty($url)) return false;
+
+	$hash	= md5($url);
+	$hashes = array('7dd0fdf1c9ddf706f2d7db3ac9394f47',
+					'6d24b20936cc0813222f0d811bbf3cae',
+					'8a0e9264f92e006f5655b4c19d4cab10',
+					'2e80e3bdbcfc7099625a183b058d6117',
+					'07231c6f8912f13f1fa43436b5854590',
+					'58619923a812c8175c245f6180ba9669',
+					'84573bbf41383a6a8fc402f9a550c434');
+
+	foreach($hashes as $h)
+	{
+		if($hash == $h)
+			return true;
+	}
+
+	return false;
+}
+
 function kapost_byline_the_content($content)
 {
 	global $post;
@@ -190,18 +212,22 @@ function kapost_inject_footer_script()
 	if(empty($kapost_analytics))
 		return;
 
-	if(!isset($kapost_analytics['site_id']))
-		$kapost_analytics['site_id'] = '';
+	if(kapost_byline_verify_analytics_url($kapost_analytics['url']) == false)
+		return;
 
-	extract($kapost_analytics, EXTR_SKIP);
+	$post_id	= esc_js($kapost_analytics['post_id']);
+	$author_id	= esc_js($kapost_analytics['author_id']);
+	$newsroom_id= esc_js($kapost_analytics['newsroom_id']);
+	$categories = esc_js($kapost_analytics['categories']);
+	$url		= esc_js($kapost_analytics['url']);
 
 echo "<!-- BEGIN KAPOST ANALYTICS CODE -->
 <script type=\"text/javascript\">
 <!--
-var _kaq = _kaq || [];
-_kaq.push([2, '" . esc_js( $post_id ) . "', '". esc_js( $site_id ) . "']);
+var _kapost_data = _kapost_data || [];
+_kapost_data.push([1, '$post_id', '$author_id', '$newsroom_id', '$categories']);
 (function(){
-var ka = document.createElement('script'); ka.async=true; ka.id='ka_tracker'; ka.src='". esc_js( $url ) ."/ka.js';
+var ka = document.createElement('script'); ka.async=true; ka.id='kp_tracker'; ka.src='$url/javascripts/tracker.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ka, s);
 })();
 //--> 
