465db98b75afdeaf623abb4595b8270086b56db1
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-07-24 16:57:19 +0000

    Co-Authors Plus; Enable Guest Authors on healthcommon /z 7678
    git-svn-id: https://vip-svn.wordpress.com/plugins@72367 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -10,6 +10,7 @@ $coauthors_plus_guest_authors_whitelist = array(
 		'vip/sc-stylecaster',
 		'vip/thoughtcatalog',
 		'vip/time-lifestyle',
+		'vip/healthcommon',
 	);
 if ( !in_array( get_option( 'stylesheet' ), $coauthors_plus_guest_authors_whitelist ) )
 	add_filter( 'coauthors_guest_authors_enabled', '__return_false' );
