6bdbc6fa4432e0118a57a7e338defb340bbc8f37
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-07-25 03:43:05 +0000

    Adding wp-pagenavi v2.82, with some mods to make it WP.com friendly
    git-svn-id: https://vip-svn.wordpress.com/plugins@72442 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ wp-pagenavi/wpcom-helper.php
@@ -0,0 +1,6 @@
+<?php
+/*
+Just some notes on changes from the .org version:
+
+* removed scb modules: cron, table, widget, hooks, boxespage
+* changed some hooks to make the plugin work when loaded late
