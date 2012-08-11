6059bb903793fb80bbcdffe74979d032354cc0b6
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-08-11 04:13:22 +0000

    New WP.com helper: wpcom_vip_allow_more_html_in_comments
    Allow more HTML in comments by non-author users
    
    /z 8251
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@74195 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-helper-wpcom.php
+++ vip-helper-wpcom.php
@@ -719,3 +719,11 @@ function wpcom_get_option_lock( $lock_name ) {
 
 	return $lock;
 }
+
+/**
+ * Allows non-author users to submit any tags allowed via $allowedposttags instead of just $allowedtags
+ */
+function wpcom_vip_allow_more_html_in_comments() {
+	remove_filter( 'pre_comment_content', 'wp_filter_kses' ); 
+	add_filter( 'pre_comment_content', 'wp_filter_post_kses' );
+}
