61b1023075a0d60fa8a56f9a2644c718cb9cc4b1
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-09-06 21:42:06 +0000

    vip-init: load wpcom helper functions in all enviroments.
    While most functions won't do anything outside WP.com, they're harmless and this way we can call functions without the need for function_exists and provide local fallbacks in some cases.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@76713 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -8,9 +8,12 @@ require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powe
 require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-roles.php' );
 require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-permastructs.php' );
 
+// These are helper functions specific to WP.com-related functionality
+wpcom_vip_load_helper_wpcom(); // vip-helper-wpcom.php
+
+
 // Load the WordPress.com dependent helper files, only on WordPress.com (for now)
 if ( WPCOM_IS_VIP_ENV ) {
-	wpcom_vip_load_helper_wpcom(); // vip-helper-wpcom.php
 	wpcom_vip_load_helper_stats(); // vip-helper-wpcom.php
 } else {
 	// Local helpers that add WP.com functionality
