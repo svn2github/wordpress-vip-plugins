8be35f00ffc0bdc59da7ecee970a3895e1b38bf0
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-09-20 20:05:15 +0000

    Safe Redirect Manager: Upgrade to v1.3
    git-svn-id: https://vip-svn.wordpress.com/plugins@78056 dd790b09-5c1c-0410-8744-875116bcd8a6

--- safe-redirect-manager/safe-redirect-manager.php
+++ safe-redirect-manager/safe-redirect-manager.php
@@ -4,7 +4,7 @@ Plugin Name: Safe Redirect Manager
 Plugin URI: http://www.10up.com
 Description: Easily and safely manage HTTP redirects.
 Author: Taylor Lovett (10up LLC), VentureBeat
-Version: 1.2
+Version: 1.4
 Author URI: http://www.10up.com
 
 GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
@@ -66,11 +66,104 @@ class SRM_Safe_Redirect_Manager {
 		add_action( 'admin_print_styles-edit.php', array( $this, 'action_print_logo_css' ), 10, 1 );
 		add_action( 'admin_print_styles-post.php', array( $this, 'action_print_logo_css' ), 10, 1 );
 		add_action( 'admin_print_styles-post-new.php', array( $this, 'action_print_logo_css' ), 10, 1 );
+		
+		// Search filters
+		add_filter( 'posts_join', array( $this, 'filter_search_join' ) );
+		add_filter( 'posts_where', array( $this, 'filter_search_where' ) );
+		add_filter( 'posts_distinct', array( $this, 'filter_search_distinct' ) );
+	}
+	
+	/**
+	 * Join posts table with postmeta table on search
+	 *
+	 * @since 1.2
+	 * @param string $join
+	 * @uses get_query_var
+	 * @return string
+	 */
+	public function filter_search_join( $join ) {
+		if ( $this->redirect_post_type != get_query_var( 'post_type' ) )
+			return $join;
+		
+		global $wpdb;
+		
+		$s = get_query_var( 's' );
+		if ( ! empty( $s ) ) {
+			$join .= " LEFT JOIN $wpdb->postmeta AS m ON ($wpdb->posts.ID = m.post_id) ";
+		}
+		return $join;
+	}
+	
+	/**
+	 * Return distinct search results
+	 *
+	 * @since 1.2
+	 * @param string $distinct
+	 * @uses get_query_var
+	 * @return string
+	 */
+	public function filter_search_distinct( $distinct ) {
+		if ( $this->redirect_post_type != get_query_var( 'post_type' ) )
+			return $distinct;
+		
+		return 'DISTINCT';
+	}
+	
+	/**
+	 * Join posts table with postmeta table on search
+	 *
+	 * @since 1.2
+	 * @param string $where
+	 * @uses is_search, get_query_var
+	 * @return string
+	 */
+	public function filter_search_where( $where ) {
+		if ( $this->redirect_post_type != get_query_var( 'post_type' ) || ! is_search() || empty( $where ) )
+			return $where;
+		
+		$exact = get_query_var( 'exact' );
+		$n = ( ! empty( $exact ) ) ? '' : '%';
+
+		$search = '';
+		$seperator = '';
+		$terms = $this->get_search_terms();
+		$search .= '(';
+		
+		// we check the meta values against each term in the search
+		foreach ( $terms as $term ) {
+			$search .= $seperator;	
+			$search .= sprintf( "( ( m.meta_value LIKE '%s%s%s' AND m.meta_key = '%s') OR ( m.meta_value LIKE '%s%s%s' AND m.meta_key = '%s') )", $n, $term, $n, $this->meta_key_redirect_from, $n, $term, $n, $this->meta_key_redirect_to );		
+			$seperator = ' OR ';
+		}
+		
+		$search .= ')';
+		
+		$where = preg_replace( '/\(\(\(.*?\)\)\)/is', '((' . $search . '))', $where );
+		
+		return $where;
+	}
+	
+	/**
+	 * Get an array of search terms
+	 *
+	 * @since 1.2
+	 * @uses get_query_var
+	 * @return array
+	 */
+	private function get_search_terms() {
+		$s = get_query_var( 's' );
+
+		if ( ! empty( $s ) ) {
+			preg_match_all( '/".*?("|$)|((?<=[\\s",+])|^)[^\\s",+]+/', stripslashes( $s ), $matches );
+			$search_terms = array_map( create_function( '$a', 'return trim( $a, "\\"\'\\n\\r " );' ), $matches[0] );
+		}
+		return $search_terms;
 	}
 	
 	/**
 	 * Swap tools logo for plugin logo
 	 *
+	 * @since 1.1
 	 * @uses plugins_url
 	 * @return void
 	 */
@@ -82,7 +175,7 @@ class SRM_Safe_Redirect_Manager {
 					background: url("<?php echo plugins_url( 'images/icon32x32.png', __FILE__ ); ?>") no-repeat top left !important;
 					margin-right: 0;
 				}
-				#visibility, .view-switch {
+				#visibility, .view-switch, .posts .inline-edit-col-left .inline-edit-group {
 					display: none;
 				}
 			</style>
@@ -93,10 +186,50 @@ class SRM_Safe_Redirect_Manager {
 	/**
 	 * Removes bulk actions from post manager
 	 *
+	 * @since 1.0
 	 * @return array
 	 */
 	public function filter_bulk_actions() {
 		return array();
+	}
+	
+	/**
+	 * Creates a redirect post, this function will be useful for import/exports scripts
+	 *
+	 * @param string $redirect_from
+	 * @param string $redirect_to
+	 * @param int $status_code
+	 * @since 1.3
+	 * @uses wp_insert_post, update_post_meta
+	 * @return int
+	 */
+	public function create_redirect( $redirect_from, $redirect_to, $status_code ) {
+		$sanitized_redirect_from = $this->sanitize_redirect_from( $redirect_from );
+		$sanitized_redirect_to = $this->sanitize_redirect_to( $redirect_to );
+		$sanitized_status_code = absint( $status_code );
+		
+		// check and make sure no parameters are empty or invalid after sanitation
+		if ( empty( $sanitized_redirect_from ) || empty( $sanitized_redirect_to ) || ! in_array( $sanitized_status_code, $this->valid_status_codes ) )
+			return 0;
+		
+		// create the post
+		$post_args = array(
+			'post_type' => $this->redirect_post_type,
+			'post_status' => 'publish',
+			'post_author' => 1
+		);
+		
+		$post_id = wp_insert_post(  $post_args );
+		
+		if ( 0 >= $post_id )
+			return 0;
+		
+		// update the posts meta info
+		update_post_meta( $post_id, $this->meta_key_redirect_from, $sanitized_redirect_from );
+		update_post_meta( $post_id, $this->meta_key_redirect_to, $sanitized_redirect_to );
+		update_post_meta( $post_id, $this->meta_key_redirect_status_code, $sanitized_status_code );
+		
+		return $post_id;
 	}
 	
 	/**
@@ -141,11 +274,14 @@ class SRM_Safe_Redirect_Manager {
 	 * Returns true if max redirects have been reached
 	 *
 	 * @since 1.0
-	 * @uses apply_filters
+	 * @uses apply_filters, get_transient
 	 * @return bool
 	 */
 	public function max_redirects_reached() {
-		$redirects = $this->update_redirect_cache();
+		if ( false === ( $redirects = get_transient( $this->cache_key_redirects ) ) ) {
+			$redirects = $this->update_redirect_cache();
+		}
+		
 		$max_redirects = apply_filters( 'srm_max_redirects', $this->default_max_redirects );
 		
 		return ( count( $redirects ) >= $max_redirects );
@@ -155,11 +291,13 @@ class SRM_Safe_Redirect_Manager {
 	 * Check for potential redirect loops or chains
 	 *
 	 * @since 1.0
-	 * @uses home_url
+	 * @uses home_url, get_transient
 	 * @return boolean
 	 */
 	public function check_for_possible_redirect_loops() {
-		$redirects = $this->update_redirect_cache();
+		if ( false === ( $redirects = get_transient( $this->cache_key_redirects ) ) ) {
+			$redirects = $this->update_redirect_cache();
+		}
 		
 		$current_url = parse_url( home_url() );
 		$this_host = ( is_array( $current_url ) && ! empty( $current_url['host'] ) ) ? $current_url['host'] : '';
@@ -199,10 +337,10 @@ class SRM_Safe_Redirect_Manager {
 	 * @since 1.0
 	 * @param string $title
 	 * @param int $post_id
-	 * @uses is_admin
+	 * @uses is_admin, get_post_meta
 	 * @return string
 	 */
-	public function filter_admin_title( $title, $post_id ) {
+	public function filter_admin_title( $title, $post_id = 0 ) {
 		if ( ! is_admin() || false === ( $redirect = get_post( $post_id ) ) || $redirect->post_type != $this->redirect_post_type )
 			return $title;
 		
@@ -475,8 +613,8 @@ class SRM_Safe_Redirect_Manager {
 			if ( ! in_array( $with_www, $content ) ) $content[] = $with_www;
 		}
 		
-        	return $content;
-    	}
+		return $content;
+	}
     
 	/**
 	 * Force update on the redirect cache and return cache
@@ -557,7 +695,7 @@ class SRM_Safe_Redirect_Manager {
 					$this->whitelist_hosts[] = $parsed_redirect['host'];
 					add_filter( 'allowed_redirect_hosts' , array( $this, 'filter_allowed_redirect_hosts' ) );
 				}
-					
+				
 				// if we have a valid status code, then redirect with it
 				if ( in_array( $status_code, $this->valid_status_codes ) )
 					wp_safe_redirect( esc_url_raw( $redirect_to ), $status_code );
@@ -580,16 +718,16 @@ class SRM_Safe_Redirect_Manager {
 	 * @return string
 	 */
 	public function sanitize_redirect_to( $path ) {
-        	$path = trim( $path );
+		$path = trim( $path );
         
-        	if (  preg_match( '/^www\./i', $path ) )
-            	$path = 'http://' . $path;
+		if (  preg_match( '/^www\./i', $path ) )
+			$path = 'http://' . $path;
         
-        	if ( ! preg_match( '/^https?:\/\//i', $path ) )
-            		if ( strpos( $path, '/' ) !== 0 )
-                		$path = '/' . $path;
+		if ( ! preg_match( '/^https?:\/\//i', $path ) )
+			if ( strpos( $path, '/' ) !== 0 )
+				$path = '/' . $path;
         
-        	return esc_url_raw( $path );
+		return esc_url_raw( $path );
 	}
     
 	/**
@@ -602,23 +740,24 @@ class SRM_Safe_Redirect_Manager {
 	 */
 	public function sanitize_redirect_from( $path ) {
         
-        	$path = trim( $path );
+		$path = trim( $path );
+		
+		if ( empty( $path ) )
+            	return '';
         
-        	if ( empty( $path ) )
-            		return '';
+		// dont accept paths starting with a .
+		if ( strpos( $path, '.' ) === 0 )
+			return '';
         
-        	// dont accept paths starting with a .
-        	if ( strpos( $path, '.' ) === 0 )
-            		return '';
+		// turn path in to absolute
+		if ( preg_match( '/https?:\/\//i', $path ) )
+			$path = preg_replace( '/^(http:\/\/|https:\/\/)(www\.)?[^\/?]+\/?(.*)/i', '/$3', $path );
+		elseif ( strpos( $path, '/' ) !== 0 )
+			$path = '/' . $path;
         
-        	// turn path in to absolute
-        	if ( preg_match( '/https?:\/\//i', $path ) )
-            		$path = preg_replace( '/^(http:\/\/|https:\/\/)(www\.)?[^\/?]+\/?(.*)/i', '/$3', $path );
-        	elseif ( strpos( $path, '/' ) !== 0 )
-            		$path = '/' . $path;
-        
-        	return esc_url_raw( $path );
-    	}
+		return esc_url_raw( $path );
+	}
 }
 
-new SRM_Safe_Redirect_Manager();
+global $safe_redirect_manager;
+$safe_redirect_manager = new SRM_Safe_Redirect_Manager();
