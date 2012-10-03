62a429a33250731d5ac426e5ef42bc8f561b7e94
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-03 21:42:22 +0000

    SEO Auto-Linker: Upgrade to v0.8.2
    git-svn-id: https://vip-svn.wordpress.com/plugins@79362 dd790b09-5c1c-0410-8744-875116bcd8a6

--- seo-auto-linker/wpcom-helper.php
+++ seo-auto-linker/wpcom-helper.php
@@ -7,3 +7,11 @@ add_filter( 'seoal_blacklist_max', 'wpcom_vip_seoal_blacklist_max' );
 function wpcom_vip_seoal_blacklist_max( $orig ) {
 	return 30;
 }
+
+/**
+ * Restrict the number of links that are possibly added to each post
+ */
+add_filter( 'seoal_number_links', 'wpcom_vip_seoal_number_links' );
+function wpcom_vip_seoal_number_links( $orig ) {
+	return 100;
+}
