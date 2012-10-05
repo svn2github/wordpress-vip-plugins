9be3511b7662a26e90f5a23d89f0230b7d22ead4
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-05 18:03:35 +0000

    Post Meta Inspector: Ignore Akismet keys as they're not relevant
    git-svn-id: https://vip-svn.wordpress.com/plugins@79659 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ post-meta-inspector/wpcom-helper.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * Ignore Akismet keys as they're not relevant
+ */
+add_filter( 'pmi_ignore_post_meta_key', 'wpcom_filter_pmi_ignore_post_meta_key', 10, 2 );
+function wpcom_filter_pmi_ignore_post_meta_key( $ret, $key ) {
+	$ignored = array(
+			'_akismet_post_spam',
+			'_akismet_post_guid',
+			'_akismet_post_content',
+			'_feedback_akismet_values',
+		);
+	return (bool) in_array( $key, array( '_edit_last' ) );
+}
