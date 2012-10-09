ed465c93d8a913d83c07477bae3229ea3429aedb
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-09 17:05:39 +0000

    NBCS Moderation Queue Alerts: Sync from vip-improvements branch
    * Use wp_count_comments() instead of get_comment_count(), as the latter provides moderation and trashed as the same number and is deprecated
    https://github.com/Automattic/nbcs-wp-moderation-queue-alerts/commit/ed7a9fc7840d405053aa47a36366a44a3c400d27
    
    * admin_url() is a better function to use in this context
    https://github.com/Automattic/nbcs-wp-moderation-queue-alerts/commit/f8e7f112e7393c3ea4b9beedcb9277a649b90505
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@80011 dd790b09-5c1c-0410-8744-875116bcd8a6

--- nbcs-moderation-queue-alerts/nbcs-moderation-queue-alerts.php
+++ nbcs-moderation-queue-alerts/nbcs-moderation-queue-alerts.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Moderation Queue Alerts
+Plugin Name: NBCS Moderation Queue Alerts
 Description: Plugin to send emails when the comment moderation queue grows too large
 Version: 1.0
 Author: James Dowd
@@ -15,24 +15,24 @@ function nbcs_moderation_queue_alerts_check_queue() {
 		return; // Don't do anything if the settings have not been set
 	}
 
-	$comment_count = get_comment_count();
-	if ( $comment_count['awaiting_moderation'] >= intval( $options['minimum'] ) ) {
+	$comment_count = wp_count_comments();
+	if ( $comment_count->moderated >= intval( $options['minimum'] ) ) {
 		if ( intval( $options['frequency'] ) > 0 ) {
 			set_transient( 'nbcs-moderation-queue-delay', true, 60 * intval( $options['frequency'] ) );
 		}
 
 		$blog_name = get_bloginfo( 'name' );
 		$subject = sprintf( __( '%s Moderation Queue Alert', 'nbcs-moderation-queue' ), $blog_name );
-		$message = sprintf( __( 'There are currently %d comments in the %s moderation queue.', 'nbcs-moderation-queue' ), $comment_count['awaiting_moderation'], $blog_name );
+		$message = sprintf( __( 'There are currently %d comments in the %s moderation queue.', 'nbcs-moderation-queue' ), $comment_count->moderated, $blog_name );
 		if ( $options['frequency'] > 0 ) {
 			$message .= sprintf( __( ' You will not receive another alert for %d minutes.', 'nbcs-moderation-queue' ), $options['frequency'] );
 		}
-		$message .= '</p><p><a href="' . site_url( '/wp-admin/edit-comments.php' ) . '">' . __( 'Go to comments page', 'nbcs-moderation-queue' ) . '</a></p>';
+		$message .= '</p><p><a href="' . admin_url( 'edit-comments.php' ) . '">' . __( 'Go to comments page', 'nbcs-moderation-queue' ) . '</a></p>';
 		
 		$headers = array( 'Content-Type: text/html' );
 		
-		$subject = apply_filters( 'nbcs-moderation-queue-subject', $subject, $comment_count['awaiting_moderation'] );
-		$message = apply_filters( 'nbcs-moderation-queue-message', $message, $comment_count['awaiting_moderation'] );
+		$subject = apply_filters( 'nbcs-moderation-queue-subject', $subject, $comment_count );
+		$message = apply_filters( 'nbcs-moderation-queue-message', $message, $comment_count );
 		
 		wp_mail( $options['email'], $subject, $message, $headers );
 	}
