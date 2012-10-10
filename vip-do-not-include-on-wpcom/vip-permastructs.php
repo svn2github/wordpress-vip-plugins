5ff75c48129b8aa97024e98ab94bc75815e98cdd
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-10 15:05:19 +0000

    VIP Permastructs: introduce new helper function, wpcom_vip_load_custom_cdn.
    Allows VIPs to use a custom CDN host for displaying theme images and media library content. Only to used under supervision to avoid site breakage.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@80144 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-permastructs.php
+++ vip-do-not-include-on-wpcom/vip-permastructs.php
@@ -75,3 +75,50 @@ function _wpcom_vip_filter_tag_base( $tag_base ) {
 	return $wpcom_vip_tag_base;
 }
 endif;
+
+if ( ! function_exists( 'wpcom_vip_set_cdn_url' ) ):
+/**
+ * Use a custom CDN host for displaying theme images and media library content.
+ * 
+ * Please get in touch before using this as it can break your site.
+ *
+ * @param string Hostname of the CDN for media library assets.
+ * @param string Hostname of the CDN for static assets.
+ * @param bool Whether the custom CDN host should be used in the admin context as well.
+ */
+function wpcom_vip_load_custom_cdn( $cdn_host_media, $cdn_host_static = '', $include_admin = false ) {
+	if ( ! WPCOM_IS_VIP_ENV )
+		return;
+
+	if ( ! $include_admin && is_admin() )
+		return;
+
+	if ( ! empty( $cdn_host_static ) ) {
+		$cdn_host_static = parse_url( esc_url_raw( $cdn_host_static ), PHP_URL_HOST );
+
+		add_filter( 'wpcom_staticize_subdomain_host', function( $host ) use ( $cdn_host_static ) {
+			return $cdn_host_static;
+		}, 999 );
+	}
+
+	if ( ! empty( $cdn_host_media ) ) {
+		$cdn_host_media = parse_url( esc_url_raw( $cdn_host_media ), PHP_URL_HOST );
+
+		add_filter( 'wp_get_attachment_url', function( $url, $attachment_id ) use ( $cdn_host_media ) {
+			return _wpcom_vip_custom_cdn_replace( $url, $cdn_host_media );
+		}, 999, 2 );
+
+		add_filter( 'the_content', function( $content ) use ( $cdn_host_media ) {
+			if ( false !== strpos( $content, 'files.wordpress.com' ) ) {
+				$content = preg_replace_callback( '#(https?://[\w]+.files.wordpress.com[^\'">]+)#', function( $matches ) use ( $cdn_host_media ) {
+					return _wpcom_vip_custom_cdn_replace( $matches[1], $cdn_host_media );
+				}, $content );
+			}
+			return $content;
+		}, 999 );
+	}
+}
+function _wpcom_vip_custom_cdn_replace( $url, $cdn_host ) {
+	return preg_replace( '|://[^/]+?/|', "://$cdn_host/", $url );
+}
+endif;
