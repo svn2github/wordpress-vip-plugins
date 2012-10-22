087cc02238df30cbd788cffe7dd00d34ddc8b00d
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-22 06:19:31 +0000

    Fix theme name for bgr
    git-svn-id: https://vip-svn.wordpress.com/plugins@81561 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -19,7 +19,7 @@ $coauthors_plus_guest_authors_whitelist = array(
 		'vip/pgoa-relish',
 		'vip/pgoa-spry',
 		'vip/vanguardadvisors',
-		'vip/boygeniusreport',
+		'vip/bgr',
 	);
 if ( !in_array( get_option( 'stylesheet' ), $coauthors_plus_guest_authors_whitelist ) )
 	add_filter( 'coauthors_guest_authors_enabled', '__return_false' );
