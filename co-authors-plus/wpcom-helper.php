4c40a55b6b0355e5b8c1ea033b60fe02253a7b26
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-24 17:36:26 +0000

    Co-Authors Plus: Enable guest authors for all
    git-svn-id: https://vip-svn.wordpress.com/plugins@81940 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -1,30 +1,5 @@
 <?php
 /**
- * Because the guest authors functionality is work in progress,
- * only enable it for specific VIP themes
- */
-$coauthors_plus_guest_authors_whitelist = array(
-		'vip/cartercenterorg',
-		'vip/dawn-urdu',
-		'vip/dawn-urdu-mobile',
-		'vip/newyorkobserver',
-		'vip/sc-beautyhigh',
-		'vip/sc-stylecaster',
-		'vip/sc-vivant',
-		'vip/thoughtcatalog',
-		'vip/lifestyle',
-		'vip/healthcommon',
-		'vip/pmc-movieline',
-		'vip/pgoa-ap',
-		'vip/pgoa-relish',
-		'vip/pgoa-spry',
-		'vip/vanguardadvisors',
-		'vip/bgr',
-	);
-if ( !in_array( get_option( 'stylesheet' ), $coauthors_plus_guest_authors_whitelist ) )
-	add_filter( 'coauthors_guest_authors_enabled', '__return_false' );
-
-/**
  * There's some ugly code in mu-plugins/gprofiles.php that redirects anything on profile.php that's not 
  * a whitelisted gprofile page. Let's put it under tools instead.
  */
