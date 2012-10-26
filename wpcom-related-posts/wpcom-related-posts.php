ef4c99b242e9c905129d730ee95f1fa9eb264363
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-26 02:35:02 +0000

    WordPress.com Related Posts: Sync from Github
    * Only show the ElasticSearch configuration notice if it's available on the machine
    https://github.com/Automattic/WPCOM-Related-Posts/commit/5f8a9945e56bc1350d10feda897cfea1041371d4
    
    * Fix a SQL syntax error and make sure we're requesting posts of this post type in the fallback
    https://github.com/Automattic/WPCOM-Related-Posts/commit/c7e0d4eef37f1b630d762781d6f431b9ec0a3bd1
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@82192 dd790b09-5c1c-0410-8744-875116bcd8a6

--- wpcom-related-posts/wpcom-related-posts.php
+++ wpcom-related-posts/wpcom-related-posts.php
@@ -27,7 +27,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 class WPCOM_Related_Posts {
 
-	public $is_elastic_search;
+	public $is_elastic_search = null;
 	public $index;
 
 	public $options_capability = 'manage_options';
@@ -84,14 +84,12 @@ class WPCOM_Related_Posts {
 				$this->is_elastic_search = true;
 			else
 				$this->is_elastic_search = false;
-		} else {
-			$this->is_elastic_search = false;
 		}
 
 		$this->stopwords = array( 'a', 'about', 'above', 'above', 'across', 'after', 'afterwards', 'again', 'against', 'all', 'almost', 'alone', 'along', 'already', 'also','although','always','am','among', 'amongst', 'amoungst', 'amount',  'an', 'and', 'another', 'any','anyhow','anyone','anything','anyway', 'anywhere', 'are', 'around', 'as',  'at', 'back','be','became', 'because','become','becomes', 'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'below', 'beside', 'besides', 'between', 'beyond', 'bill', 'both', 'bottom','but', 'by', 'call', 'can', 'cannot', 'cant', 'co', 'con', 'could', 'couldnt', 'cry', 'de', 'describe', 'detail', 'do', 'done', 'down', 'due', 'during', 'each', 'eg', 'eight', 'either', 'eleven','else', 'elsewhere', 'empty', 'enough', 'etc', 'even', 'ever', 'every', 'everyone', 'everything', 'everywhere', 'except', 'few', 'fifteen', 'fify', 'fill', 'find', 'fire', 'first', 'five', 'for', 'former', 'formerly', 'forty', 'found', 'four', 'from', 'front', 'full', 'further', 'get', 'give', 'go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'however', 'hundred', 'ie', 'if', 'in', 'inc', 'indeed', 'interest', 'into', 'is', 'it', 'its', 'itself', 'keep', 'last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made', 'many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine', 'more', 'moreover', 'most', 'mostly', 'move', 'much', 'must', 'my', 'myself', 'name', 'namely', 'neither', 'never', 'nevertheless', 'next', 'nine', 'no', 'nobody', 'none', 'noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of', 'off', 'often', 'on', 'once', 'one', 'only', 'onto', 'or', 'other', 'others', 'otherwise', 'our', 'ours', 'ourselves', 'out', 'over', 'own','part', 'per', 'perhaps', 'please', 'put', 'rather', 're', 'same', 'see', 'seem', 'seemed', 'seeming', 'seems', 'serious', 'several', 'she', 'should', 'show', 'side', 'since', 'sincere', 'six', 'sixty', 'so', 'some', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhere', 'still', 'such', 'system', 'take', 'ten', 'than', 'that', 'the', 'their', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'therefore', 'therein', 'thereupon', 'these', 'they', 'thickv', 'thin', 'third', 'this', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'top', 'toward', 'towards', 'twelve', 'twenty', 'two', 'un', 'under', 'until', 'up', 'upon', 'us', 'very', 'via', 'was', 'we', 'well', 'were', 'what', 'whatever', 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 'whither', 'who', 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with', 'within', 'without', 'would', 'yet', 'you', 'your', 'yours', 'yourself', 'yourselves', 'und', 'de', 'la', 'www', 'en' );
 		$this->stopwords = apply_filters( 'wrp_stopwords', $this->stopwords );
 
-		if ( ! $this->is_elastic_search )
+		if ( false === $this->is_elastic_search )
 			add_action( 'admin_notices', array( self::$instance, 'admin_notice_no_index' ) );
 
 	}
@@ -244,7 +242,8 @@ class WPCOM_Related_Posts {
 		} else {
 			$related_query_args = array(
 				'posts_per_page' => (int)$args['posts_per_page'],
-				'post__not_in'   => $post_id,
+				'post__not_in'   => array( $post_id ),
+				'post_type'      => $args['post_type'],
 			);
 			$categories = get_the_category( $post_id );
 			if ( ! empty( $categories ) )
