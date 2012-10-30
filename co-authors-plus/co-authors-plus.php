18c493ab29d3f10a9e638dc81feb67e5d1ffd872
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-10-30 15:54:23 +0000

    Co-Authors Plus: Sync from Github
    * Migrating terms on the fly has performance issues :( Instead, let's keep the old term if there is one. All new terms are prefixed, which should solve collisions in the creation process
    https://github.com/Automattic/Co-Authors-Plus/commit/804cb9265e90372327b3b632ebee195e44d9b1d4
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@82521 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/co-authors-plus.php
+++ co-authors-plus/co-authors-plus.php
@@ -1120,33 +1120,15 @@ class coauthors_plus {
 		}
 		$term_description = implode( ' ', $search_values );
 
-		$coauthor_slug = 'cap-' . $coauthor->user_nicename;
-		if ( $term = get_term_by( 'slug', $coauthor_slug, $this->coauthor_taxonomy ) ) {
+		if ( $term = $this->get_author_term( $coauthor ) ) {
 			wp_update_term( $term->term_id, $this->coauthor_taxonomy, array( 'description' => $term_description ) );
-			$new_term_id = $term->term_id;
 		} else {
+			$coauthor_slug = 'cap-' . $coauthor->user_nicename;
 			$args = array(
 				'slug'          => $coauthor_slug,
 				'description'   => $term_description,
 			);
 			$new_term = wp_insert_term( $coauthor->user_login, $this->coauthor_taxonomy, $args );
-			$new_term_id = $new_term['term_id'];
-		}
-		// Migrate the old term if there was one
-		if ( $old_term = get_term_by( 'slug', $coauthor->user_nicename, $this->coauthor_taxonomy ) ) {
-			if ( $old_term->count < 10 ) {
-				$args = array(
-						'default' => $new_term_id,
-						'force_default' => true,
-					);
-				wp_delete_term( $old_term->term_id, $this->coauthor_taxonomy, $args );
-			}
-			// else {
-			// 	if ( false === wp_cache_get( 'coauthors-plus-debug-' . $coauthor->user_nicename ) ) {
-			// 		wp_mail( 'daniel@a8c.com', '[' . site_url() . '] ' . $coauthor->user_nicename . ' has too many posts for the term to be deleted', var_export( $old_term, true ) );
-			// 		wp_cache_set( 'coauthors-plus-debug-' . $coauthor->user_nicename, 'apple' );
-			// 	}
-			// }
 		}
 		wp_cache_delete( 'author-term-' . $coauthor->user_nicename, 'co-authors-plus' );
 		return get_term_by( 'slug', $coauthor_slug, $this->coauthor_taxonomy );
