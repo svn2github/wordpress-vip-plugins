87753926093058231ff7f348d44422169e8fde8f
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-11-01 04:56:50 +0000

    lazy-load: disable for iPad as well; sonar does not work well.
    git-svn-id: https://vip-svn.wordpress.com/plugins@82721 dd790b09-5c1c-0410-8744-875116bcd8a6

--- lazy-load/wpcom-helper.php
+++ lazy-load/wpcom-helper.php
@@ -6,5 +6,8 @@ function wpcom_vip_disable_lazyload_on_mobile( $enabled ) {
 	if ( function_exists( 'jetpack_is_mobile' ) && jetpack_is_mobile() )
 		$enabled = false;
 
+	if ( class_exists( 'Jetpack_User_Agent_Info' ) && Jetpack_User_Agent_Info::is_ipad() )
+		$enabled = false;
+
 	return $enabled;
 } 
