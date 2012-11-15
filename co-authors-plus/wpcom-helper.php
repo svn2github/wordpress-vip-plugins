2990195025b0e9f8fb403ebaf8a98ec19581d91b
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-11-15 21:40:43 +0000

    Co-Authors Plus: Automatically apply template tags for Portfolio
    git-svn-id: https://vip-svn.wordpress.com/plugins@84258 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -4,3 +4,13 @@
  * a whitelisted gprofile page. Let's put it under tools instead.
  */
 add_filter( 'coauthors_guest_author_parent_page', function() { return 'tools.php'; } );
+
+/**
+ * Auto-apply Co-Authors Plus template tags on themes that are properly using the_author()
+ * and the_author_posts_link()
+ */
+$wpcom_coauthors_plus_auto_apply_themes = array(
+		'premium/portfolio',
+	);
+if ( in_array( get_option( 'template' ), $wpcom_coauthors_plus_auto_apply_themes ) )
+	add_filter( 'coauthors_auto_apply_template_tags', '__return_true' );
