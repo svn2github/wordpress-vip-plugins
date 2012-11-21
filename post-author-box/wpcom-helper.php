a12d4c260e7e5c1f329402a877812d14966020af
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-11-21 05:39:33 +0000

    Post Author Box: If Co-Authors Plus is active, override the value for %avatar% with the first co-author because it may be a guest author
    git-svn-id: https://vip-svn.wordpress.com/plugins@84844 dd790b09-5c1c-0410-8744-875116bcd8a6

--- post-author-box/wpcom-helper.php
+++ post-author-box/wpcom-helper.php
@@ -18,13 +18,19 @@ add_filter( 'pab_search_values', 'pabx_add_search_values' );
 * Set replacement values for specific tokens with Post Author Box
 */
 function pabx_add_replace_values( $tokens ) {
+	global $coauthors_plus;
 	
 	if ( ! function_exists( 'coauthors' ) )
 		return $tokens;
 
+	$coauthor = array_shift( get_coauthors() );
+
+	// Co-Authors Plus specific tokens
 	$tokens['%coauthors%'] = coauthors( null, null, null, null, false );
 	$tokens['%coauthors_posts_links%'] = coauthors_posts_links( null, null, null, null, false );
 	$tokens['%coauthors_firstnames%'] = coauthors_firstnames( null, null, null, null, false );
+	// Modify the %avatar% token to maybe use the coauthor
+	$tokens['%avatar%'] = get_avatar( $coauthor->user_email );
 	return $tokens;
 }
 add_filter( 'pab_replace_values', 'pabx_add_replace_values' );
