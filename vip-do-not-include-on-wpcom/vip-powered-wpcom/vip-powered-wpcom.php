6bf7b5b8889e87fac2b0db2b7170382b0bee9c68
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-12-10 05:31:47 +0000

    Powered By: Add classed to the powered by links for easy styling
    git-svn-id: https://vip-svn.wordpress.com/plugins@86696 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powered-wpcom.php
+++ vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powered-wpcom.php
@@ -88,7 +88,7 @@ function vip_powered_wpcom_img_html( $image ) {
 		);
 
 		if ( array_key_exists( $image, $vip_powered_wpcom_images ) )
-				return '<a href="' . esc_url( vip_powered_wpcom_url() ) . '" rel="generator nofollow"><img src="' . esc_url( plugins_url( 'images/' . $vip_powered_wpcom_images[$image][0], __FILE__ ) ) . '" width="' . esc_attr( $vip_powered_wpcom_images[$image][1] ) . '" height="' . esc_attr( $vip_powered_wpcom_images[$image][2] ) . '" /></a>';
+				return '<a href="' . esc_url( vip_powered_wpcom_url() ) . '" rel="generator nofollow" class="powered-by-wpcom"><img src="' . esc_url( plugins_url( 'images/' . $vip_powered_wpcom_images[$image][0], __FILE__ ) ) . '" width="' . esc_attr( $vip_powered_wpcom_images[$image][1] ) . '" height="' . esc_attr( $vip_powered_wpcom_images[$image][2] ) . '" /></a>';
 		else
 			return '';
 }
@@ -96,7 +96,7 @@ function vip_powered_wpcom_img_html( $image ) {
 function vip_powered_wpcom( $display = 'text', $before_text = 'Powered by ' ) {
 	switch ($display) {
 		case 'text':
-			$output = $before_text . '<a href="' . esc_url( vip_powered_wpcom_url() ) . '" rel="generator nofollow">WordPress.com VIP</a>';
+			$output = $before_text . '<a href="' . esc_url( vip_powered_wpcom_url() ) . '" rel="generator nofollow" class="powered-by-wpcom">WordPress.com VIP</a>';
 			break;
 		case 1:
 		case 2:
