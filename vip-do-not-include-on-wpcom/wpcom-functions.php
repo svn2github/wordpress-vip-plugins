e9372d646c1d44a5a2a3fed4b0eb263c0c667e83
viper007bond <viper007bond@dd790b09-5c1c-0410-8744-875116bcd8a6>
2012-12-17 21:15:33 +0000

    WP.com Local Development Functions: Add version of es_api_search_index() that will work for local development.
    git-svn-id: https://vip-svn.wordpress.com/plugins@87487 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/wpcom-functions.php
+++ vip-do-not-include-on-wpcom/wpcom-functions.php
@@ -98,4 +98,75 @@ if ( ! function_exists( 'wpcom_is_vip' ) ) : // Do not load these on WP.com
 			flush_rewrite_rules( false );
 		}
 	endif;
-endif;
+
+	/**
+	 * Executes an elasticsearch query via our REST API.
+	 * Requires setup on our end and a paid addon to your hosting account.
+	 * You probably shouldn't be using this function.
+	 *
+	 * Valid arguments:
+	 *
+	 * * size: Number of query results to return.
+	 *
+	 * * from: Offset, the starting result to return.
+	 *
+	 * * multi_match: Will do a match search against the default fields (this is almost certainly what you want).
+	 *                e.g. array( 'query' => 'this is a test', 'fields' => array( 'content' ) )
+	 *                See: http://www.elasticsearch.org/guide/reference/query-dsl/multi-match-query.html
+	 *
+	 * * query_string: Will do a query_string search, interprets the string as Lucene query syntax.
+	 *                 e.g. array( 'default_field' => 'content', 'query' => 'tag:(world OR nation) howdy' )
+	 *                 This can fail if the user doesn't close parenthesis, or specifies a field that is not valid.
+	 *                 See: http://www.elasticsearch.org/guide/reference/query-dsl/query-string-query.html
+	 *
+	 * * more_like_this: Will do a more_like_this search, which is best for related content.
+	 *                   e.g. array( 'fields' => array( 'title', 'content' ), 'like_text' => 'this is a test', 'min_term_freq' => 1, 'max_query_terms' => 12 )
+	 *                   See: http://www.elasticsearch.org/guide/reference/query-dsl/mlt-query.html
+	 *
+	 * * facets: Structured set of facets. DO NOT do a terms facet on the content of posts/comments. It will load ALL terms into memory,
+	 *           probably taking minutes to complete and slowing down the entire cluster. With great power... etc.
+	 *           See: http://www.elasticsearch.org/guide/reference/api/search/facets/index.html
+	 *
+	 * * filters: Structured set of filters (often FASTER, since cached from one query to the next).
+	 *            See: http://www.elasticsearch.org/guide/reference/query-dsl/filtered-query.html
+	 *
+	 * * highlight: Structure defining how to highlight the results.
+	 *              See: http://www.elasticsearch.org/guide/reference/api/search/highlighting.html
+	 *
+	 * * fields: Structure defining what fields to return with the results.
+	 *           See: http://www.elasticsearch.org/guide/reference/api/search/fields.html
+	 *
+	 * * sort: Structure defining how to sort the results.
+	 *         See: http://www.elasticsearch.org/guide/reference/api/search/sort.html
+	 *
+	 * Questions? Ask us.
+	 */
+	if ( ! function_exists( 'es_api_search_index' ) ) :
+		function es_api_search_index( $args ) {
+			$defaults = array(
+				'blog_id' => get_current_blog_id(),
+			);
+
+			$args = wp_parse_args( $args, $defaults );
+
+			$args['blog_id'] = absint( $args['blog_id'] );
+
+			$service_url = 'http://public-api.wordpress.com/rest/v1/sites/' . $args['blog_id'] . '/search';
+
+			unset( $args['blog_id'] );
+
+			$curl = curl_init( $service_url );
+			curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
+			curl_setopt( $curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json') ); 
+			curl_setopt( $curl, CURLOPT_POST, true );
+			$curl_args = json_encode( $args );
+			curl_setopt( $curl, CURLOPT_POSTFIELDS, $curl_args );
+			$curl_response = curl_exec( $curl );
+			$code = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
+			curl_close( $curl );
+
+			return json_decode( $curl_response );
+		}
+	endif; // function_exists( 'es_api_search_index' )
+
+endif; // function_exists( 'wpcom_is_vip' )
