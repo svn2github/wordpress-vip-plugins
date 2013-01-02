3928ca40c06fee8b924440f2509fd53bab5c259b
pmaiorana <pmaiorana@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-01-02 18:36:01 +0000

    Add Meta Tags: fixes broken link to settings from VIP plugin directory
    git-svn-id: https://vip-svn.wordpress.com/plugins@88286 dd790b09-5c1c-0410-8744-875116bcd8a6

--- add-meta-tags-mod/add-meta-tags.php
+++ add-meta-tags-mod/add-meta-tags.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Add Meta Tags Mod
-Description: Adds the <em>Description</em> and <em>Keywords</em> XHTML META tags to your blog's <em>front page</em> and to each one of the <em>posts</em>, <em>static pages</em> and <em>category archives</em>. This operation is automatic, but the generated META tags can be fully customized. Please read the tips and all other info provided at the <a href="options-general.php?page=add-meta-tags.php">configuration panel</a>.
+Description: Adds the <em>Description</em> and <em>Keywords</em> XHTML META tags to your blog's <em>front page</em> and to each one of the <em>posts</em>, <em>static pages</em> and <em>category archives</em>. This operation is automatic, but the generated META tags can be fully customized. Please read the tips and all other info provided at the <a href="options-general.php?page=amt_options">configuration panel</a>.
 Version: 1.7-WPCOM
 Author: George Notaras, Automattic
 */
