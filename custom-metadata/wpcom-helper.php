c3ca681869f9ec0679c38ee1bedb793a4f3b657e
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-01-25 01:02:03 +0000

    custom-metadata: add a wpcom-helper file with:
    * filter to force sanitize values if a sanitize callback is not set
    * filter to force user data to be saved as attributes instead of meta
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@90874 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ custom-metadata/wpcom-helper.php
@@ -0,0 +1,28 @@
+<?php
+
+// Force sanitize the value if a callback is not set
+add_filter( 'custom_metadata_manager_get_sanitize_callback', function( $callback, $field ) {
+	if ( empty( $callback ) )
+		$callback = '_wpcom_vip_custom_metadata_force_sanitize';
+	return $callback;
+}, 999, 3 );
+
+function _wpcom_vip_custom_metadata_force_sanitize( $field_slug, $field, $object_type, $object_id, $value ) {
+	return sanitize_text_field( $value );
+}
+
+// Force user data to be saved as user attributes instead of user meta
+add_filter( 'custom_metadata_manager_get_save_callback', function( $callback, $field, $object_type ) {
+	if ( 'user' == $object_type )
+		$callback = '_wpcom_vip_custom_metadata_save_user_data_as_attributes';
+	return $callback;
+}, 999, 3 );
+
+function _wpcom_vip_custom_metadata_save_user_data_as_attributes( $object_type, $object_id, $field_slug, $value ) {
+	if ( ! empty( $value ) ) {
+		update_user_attribute( $object_id, $field_slug, $value );
+	} else {
+		delete_user_attribute( $object_id, $field_slug );
+	}
+}
+
