ca89260de6cdbc78bb08b3150393387cb0459683
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-03-11 23:21:54 +0000

    Ad Code Manager: Sync from master
    * Google AdSense config: Wrap each ad output in a unique div for styling
    https://github.com/Automattic/Ad-Code-Manager/commit/881298ace3b23f0a5de9eb6713711f24d902b4b6
    
    * Some better default Google AdSense sizes
    https://github.com/Automattic/Ad-Code-Manager/commit/d7d8d786a8a257c85df424e069e72b338b13b833
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@97276 dd790b09-5c1c-0410-8744-875116bcd8a6

--- ad-code-manager/providers/google-adsense.php
+++ ad-code-manager/providers/google-adsense.php
@@ -12,32 +12,77 @@ class Google_AdSense_ACM_Provider extends ACM_Provider {
 	 */
 	public function __construct() {
 		// Default output HTML
-		$this->output_html = '<script type="text/javascript"><!-- 
+		$this->output_html = '<div id="acm-ad-tag-%tag%"><script type="text/javascript"><!-- 
 google_ad_client = "%publisher_id%"; 
 google_ad_slot = "%tag_id%"; 
 google_ad_width = %width%; 
 google_ad_height = %height%; 
 //--> 
 </script> 
-<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>';
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script></div>';
 
 		// Default Ad Tag Ids (you will pass this in your shortcode or template tag)
 		$this->ad_tag_ids = array(
 			array(
-					'tag'       => '250x250_a',
+					'tag'       => '728x90_leaderboard',
 					'url_vars'  => array(
-							'tag'       => '250x250_a',
+							'tag'       => '728x90_leaderboard',
+							'height'    => '90',
+							'width'     => '728',
+						),
+					'enable_ui_mapping' => true,
+				),
+			array(
+					'tag'       => '468x60_banner',
+					'url_vars'  => array(
+							'tag'       => '468x60_banner',
+							'height'    => '60',
+							'width'     => '468',
+						),
+					'enable_ui_mapping' => true,
+				),
+			array(
+					'tag'       => '120x600_skyscraper',
+					'url_vars'  => array(
+							'tag'       => '120x600_skyscraper',
+							'height'    => '600',
+							'width'     => '120',
+						),
+					'enable_ui_mapping' => true,
+				),
+			array(
+					'tag'       => '160x600_wideskyscraper',
+					'url_vars'  => array(
+							'tag'       => '160x600_wideskyscraper',
+							'height'    => '600',
+							'width'     => '160',
+						),
+					'enable_ui_mapping' => true,
+				),
+			array(
+					'tag'       => '300x600_largeskyscraper',
+					'url_vars'  => array(
+							'tag'       => '300x600_largeskyscraper',
+							'height'    => '600',
+							'width'     => '300',
+						),
+					'enable_ui_mapping' => true,
+				),
+			array(
+					'tag'       => '250x250_square',
+					'url_vars'  => array(
+							'tag'       => '250x250_square',
 							'height'    => '250',
 							'width'     => '250',
 						),
 					'enable_ui_mapping' => true,
 				),
 			array(
-					'tag'       => '250x250_b',
+					'tag'       => '200x200_smallsquare',
 					'url_vars'  => array(
-							'tag'       => '250x250_b',
-							'height'    => '250',
-							'width'     => '250',
+							'tag'       => '200x200_smallsquare',
+							'height'    => '200',
+							'width'     => '200',
 						),
 					'enable_ui_mapping' => true,
 				),
