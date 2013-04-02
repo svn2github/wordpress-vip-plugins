1284866190f6de91922cf238f462f6adde3c9ee0
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-04-02 21:59:41 +0000

    Co-Authors Plus: Allow the Enterprise notice to be short-circuited in mu-plugins
    git-svn-id: https://vip-svn.wordpress.com/plugins@100504 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -22,6 +22,11 @@ if ( in_array( get_option( 'template' ), $wpcom_coauthors_plus_auto_apply_themes
 if ( function_exists( 'Enterprise' ) ) {
 	if ( Enterprise()->is_enabled() && ! in_array( get_option( 'template' ), $wpcom_coauthors_plus_auto_apply_themes ) )
 		add_action( 'admin_notices', function() {
+
+			// Allow this to be short-circuted in mu-plugins
+			if ( ! apply_filters( 'wpcom_coauthors_show_enterprise_notice', true ) )
+				return;
+
 			echo '<div class="error"><p>' . __( "Co-Authors Plus isn't yet integrated with your theme. Please contact support to make it happen." ) . '</p></div>';
 		} );
 }
