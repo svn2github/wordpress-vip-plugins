22e241c90d9be0d75937a8516a07b7b17e93ad75
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-04-03 15:48:43 +0000

    syndication: track stats on how many posts are pushed/pulled
    git-svn-id: https://vip-svn.wordpress.com/plugins@100593 dd790b09-5c1c-0410-8744-875116bcd8a6

--- push-syndication/wpcom-helper.php
+++ push-syndication/wpcom-helper.php
@@ -10,3 +10,24 @@ add_filter( 'syn_transports', function( $transports ) {
 	return $transports;
 } );
 
+add_action( 'syn_post_pull_edit_post', function() {
+	wpcom_push_syndication_stats( 'vip-syndication-pull', 'edit' );
+} );
+add_action( 'syn_post_pull_new_post', function() {
+	wpcom_push_syndication_stats( 'vip-syndication-pull', 'new' );
+} );
+add_action( 'syn_post_push_delete_post', function() {
+	wpcom_push_syndication_stats( 'vip-syndication-push', 'delete' );
+} );
+add_action( 'syn_post_push_edit_post', function() {
+	wpcom_push_syndication_stats( 'vip-syndication-push', 'edit' );
+} );
+add_action( 'syn_post_push_new_post', function() {
+	wpcom_push_syndication_stats( 'vip-syndication-push', 'new' );
+} );
+
+function wpcom_push_syndication_stats( $stat, $action ) {
+	if ( function_exists( 'bump_stats_extras' ) ) {
+		bump_stats_extras( $stat, $action );
+	}	
+}
