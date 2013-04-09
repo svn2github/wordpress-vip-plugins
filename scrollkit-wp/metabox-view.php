918c6b9a9518287b0ed31ef1f2e3deb66db04fad
danielbachhuber <danielbachhuber@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-04-09 02:22:43 +0000

    Scrollkit: Initial commit. Reviewed by danielbachhuber
    http://vip-support.automattic.com/tickets/15202
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@101272 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ scrollkit-wp/metabox-view.php
@@ -0,0 +1,155 @@
+<?php
+
+
+$options = get_option( 'scroll_wp_options' );
+$scrollkit_id = get_post_meta( get_the_ID(), '_scroll_id', true );
+
+$nonce = wp_create_nonce( 'scrollkit-action' );
+
+$state = get_post_meta( get_the_ID(), '_scroll_state', true );
+
+$deactivate_link = add_query_arg(
+		array(
+			'nonce' => $nonce,
+			'scrollkit' => 'deactivate',
+			'scrollkit_cms_id' => get_the_ID()
+		), get_bloginfo('url') );
+
+$activate_link = add_query_arg(
+		array(
+			'nonce' => $nonce,
+			'scrollkit' => 'activate',
+			'scrollkit_cms_id' => get_the_ID()
+		), get_bloginfo('url') );
+
+$delete_link = add_query_arg(
+		array(
+			'nonce' => $nonce,
+			'scrollkit' => 'delete',
+			'scrollkit_cms_id' => get_the_ID()
+		), get_bloginfo('url') );
+
+$copy = array();
+
+switch($state){
+	case 'active':
+		$copy['heading'] = "This post is a scroll";
+		break;
+	case 'inactive':
+		$copy['heading'] = "This post has an inactive scroll";
+		$copy['activate'] = "Activate";
+		break;
+	default:
+		$copy['heading'] = "This post is not a scroll";
+		$copy['activate'] = "Convert";
+}
+
+?>
+
+<h4><?php echo $copy['heading'] ?></h4>
+
+<?php if (!empty($scrollkit_id)): ?>
+<a href="<?php echo ScrollKit::build_edit_url( $scrollkit_id ) ?>"
+		target="_blank"
+		class="button">
+	Edit
+</a>
+<?php endif; ?>
+
+<?php if( $state !== 'active' ): ?>
+<a href="<?php echo $activate_link  ?>"
+		class="button js-sk-disable-on-dirty">
+	<?php echo $copy['activate'] ?>
+</a>
+
+<a href="#TB_inline?height=155&width=300&inlineId=sk-load-scroll"
+	class="button thickbox js-sk-disable-on-dirty">
+	Copy Existing Scroll
+</a>
+<?php else: ?>
+
+<div class="updated">
+	<p>
+		This post is a scroll.
+		<a href="<?php echo ScrollKit::build_edit_url( $scrollkit_id ) ?>" target="_blank">
+			Edit this post with Scroll Kit
+		</a>
+	</p>
+</div>
+
+<?php endif ?>
+
+<?php if ( $state === 'active' ): ?>
+<a href="<?php echo $deactivate_link  ?>"
+		title="Turn this back into a normal wordpress post"
+		class="button js-sk-disable-on-dirty">
+	Dectivate
+</a>
+<?php endif ?>
+
+<?php if ( !empty( $state ) ): ?>
+<a href="<?php echo $delete_link ?>"
+		onclick="return confirm('This will permanently delete the scroll associated with this post, are you sure you want to delete it?');"
+		title="Permanently deletes the scroll associated with this post"
+		class="button js-sk-disable-on-dirty">
+	Delete
+</a>
+<?php endif ?>
+
+<div class="js-sk-enable-on-dirty" style="visibility: hidden; color: #a00">
+	<p>Save this post to activate Scroll Kit features</p>
+</div>
+
+<?php if (WP_DEBUG === true): ?>
+<pre>
+DEBUG
+_scroll_id: <?php echo get_post_meta( get_the_ID(), '_scroll_id', true ); ?>
+
+_scroll_state: <?php echo get_post_meta( get_the_ID(), '_scroll_state', true ); ?>
+</pre>
+<?php endif ?>
+
+<script>
+	(function(){
+		var postStatus = "<?php echo get_post_status() ?>";
+
+		isPostDirty = function(){
+			if (postStatus === 'auto-draft')
+				return true;
+
+			var mce = typeof(tinymce) != 'undefined' ? tinymce.activeEditor : false, title, content;
+
+			if ( mce && !mce.isHidden() ) {
+				return mce.isDirty();
+			} else {
+				if ( fullscreen && fullscreen.settings.visible ) {
+					title = jQuery('#wp-fullscreen-title').val() || '';
+					content = jQuery("#wp_mce_fullscreen").val() || '';
+				} else {
+					title = jQuery('#post #title').val() || '';
+					content = jQuery('#post #content').val() || '';
+				}
+
+				return ( ( title || content ) && title + content != autosaveLast );
+			}
+		}
+
+		var disableIfDirty = function() {
+			if ( isPostDirty() ) {
+				jQuery('.js-sk-disable-on-dirty').addClass('button-disabled');
+				jQuery('.js-sk-enable-on-dirty').css('visibility', 'visible');
+			}
+		}
+
+		jQuery('#title, #content').on('keydown', disableIfDirty);
+
+		// hook into the tiny mce iframe's iframe that lives within
+		// #content_ifr
+		jQuery(window).load(function() {
+			jQuery('#content_ifr').contents().on('keydown', disableIfDirty);
+		});
+
+		disableIfDirty();
+
+	})();
+</script>
---
+++ scrollkit-wp/settings-view.php
@@ -0,0 +1,107 @@
+<?php
+
+	$options = get_option( 'scroll_wp_options' );
+
+	$settings_updated = isset( $_GET['settings-updated'] ) && $_GET['settings-updated'];
+	$api_key_error = isset( $_GET['api-key-error'] ) && $_GET['api-key-error'];
+?>
+<div class="wrap">
+
+	<div class="icon32" id="icon-options-general"><br></div>
+	<h2>Scroll Kit</h2>
+
+	<?php if ( $api_key_error && !$settings_updated): ?>
+		<div class="error">
+			<p>
+				There was an error with your API key. <a href="<?php echo SCROLL_WP_SK_URL ?>/api/wp" target="_blank">Get yours here</a>
+			</p>
+		</div>
+	<?php endif ?>
+
+	<form method="post" action="options.php">
+		<?php settings_fields( 'scroll_wp_plugin_options' ) ?>
+
+		<?php //TODO make this more pretty, and with support links ?>
+
+
+		<table class="form-table">
+			<tr>
+				<th scope="row">Scroll Kit API Key</th>
+				<td>
+					<input type="text" size="57" name="scroll_wp_options[scrollkit_api_key]" value="<?php echo esc_attr($options['scrollkit_api_key']); ?>" autocomplete="off" />
+					<br>
+				  <a href="https://www.scrollkit.com/api/wp" target="_blank">Get an api key</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					HTML Header
+					<br>
+					<button id="scroll-header-default" type="button">Set to Default</button>
+				</td>
+				<td>
+					<textarea rows="10" cols="100"
+						id="header-input"
+						name="scroll_wp_options[template_header]"><?php
+						echo esc_textarea( stripslashes( $options['template_header'] ) );
+					?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					HTML Footer
+					<br>
+					<button id="scroll-footer-default" type="button">Set to Default</button>
+				</td>
+				<td>
+					<textarea rows="10" cols="100"
+						id="footer-input"
+						name="scroll_wp_options[template_footer]"><?php
+						echo esc_textarea( stripslashes( $options['template_footer'] ) )
+					?></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Template Style
+					<br>
+					<button id="scroll-style-default" type="button">Set to Default</button>
+				</td>
+				<td>
+					<textarea rows="10" cols="100"
+						id="style-input"
+						name="scroll_wp_options[template_style]"><?php
+						echo esc_textarea( stripslashes($options['template_style'] ) )
+					?></textarea>
+				</td>
+			</tr>
+		</table>
+		<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+	</form>
+</div>
+
+<script>
+	<?php $defaults = ScrollKit::option_defaults() ?>
+	(function(){
+		jQuery('#scroll-header-default').on('click', function(){
+			jQuery("#header-input").val(
+				<?php echo json_encode( $defaults['template_header'] ) ?>
+			);
+		});
+
+		jQuery('#scroll-footer-default').on('click', function(){
+			jQuery("#footer-input").val(
+				<?php echo json_encode( $defaults['template_footer'] ) ?>
+			);
+		});
+
+		jQuery('#scroll-style-default').on('click', function(){
+			jQuery("#style-input").val(
+				<?php echo json_encode( $defaults['template_style'] ) ?>
+			);
+		});
+
+	})();
+</script>
