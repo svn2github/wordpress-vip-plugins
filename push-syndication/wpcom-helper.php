dac497a4cdd8b7b9162dae87e910675edc391eb3
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-04-18 05:23:11 +0000

    syndication: enable xml pull on ewmobile.wordpress.com
    git-svn-id: https://vip-svn.wordpress.com/plugins@102691 dd790b09-5c1c-0410-8744-875116bcd8a6

--- push-syndication/wpcom-helper.php
+++ push-syndication/wpcom-helper.php
@@ -5,7 +5,7 @@ class WP_RSS_Client extends Syndication_WP_RSS_Client {}
 
 // XML Client only for a select few for now
 add_filter( 'syn_transports', function( $transports ) {
-	if ( ! in_array( parse_url( site_url(), PHP_URL_HOST ), array( 'instylemobile.wordpress.com' ) ) )
+	if ( ! in_array( parse_url( site_url(), PHP_URL_HOST ), array( 'instylemobile.wordpress.com', 'ewmobile.wordpress.com' ) ) )
 		unset( $transports['WP_XML'] );
 	return $transports;
 } );
