b65a98d36bb0ed73d7b20f2fd9ba465b53ccab69
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-04-30 19:09:12 +0000

    Load stats helpers everywhere
    git-svn-id: https://vip-svn.wordpress.com/plugins@104328 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -10,12 +10,10 @@ require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-permastructs.php' );
 
 // These are helper functions specific to WP.com-related functionality
 wpcom_vip_load_helper_wpcom(); // vip-helper-wpcom.php
-
+wpcom_vip_load_helper_stats(); // vip-helper-stats-wpcom.php
 
 // Load the WordPress.com dependent helper files, only on WordPress.com (for now)
-if ( WPCOM_IS_VIP_ENV ) {
-	wpcom_vip_load_helper_stats(); // vip-helper-stats-wpcom.php
-} else {
+if ( false === WPCOM_IS_VIP_ENV ) {
 	// Local helpers that add WP.com functionality
 	if ( ! function_exists( 'jetpack_is_mobile' ) )
 		require_once( __DIR__ . '/vip-do-not-include-on-wpcom/is-mobile.php' );
