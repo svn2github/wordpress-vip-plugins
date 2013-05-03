66cded41a3432c0e8a08a972cc142207542b4acf
johnjamesjacoby <johnjamesjacoby@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-05-03 02:53:26 +0000

    Redo the Jetpack Mobile Featured Images plugin:
    * Now works on WordPress.com.
    * Uses the settings API (for mobile-options page on wpcom.)
    * Trusts Jetpack functions are available.
    * Improves code through-out.
    * See #16585-z.
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@104731 dd790b09-5c1c-0410-8744-875116bcd8a6

--- jetpack-mobile-theme-featured-images/jetpack-mobile-theme-featured-images.php
+++ jetpack-mobile-theme-featured-images/jetpack-mobile-theme-featured-images.php
@@ -1,4 +1,64 @@
 <?php
 
-// Bootstrap file; let's our wpcom_vip_load_plugin() helper function work
-require( __DIR__ . '/jp-minileven-featured.php' );
+/**
+ * We have a special version of this plugin running for WordPress.com VIP
+ *
+ * - It uses the settings API to add itself to the mobile-options page
+ * - It trusts the jetpack_is_mobile() function implicitly
+ * - It knows that Jetpack functions will always be available
+ */
+
+/**
+ * Load up the settings fields
+ */
+function wpcom_jp_mini_featured_init() {
+	add_settings_field( 'jp_mini_featured_evwhere', __( 'Featured Images' ), 'wpcom_jp_mini_featured_evwhere', 'mobile-options', 'wp_mobile_settings_theme' );
+	register_setting( 'mobile-options', 'jp_mini_featured_evwhere', 'intval' );
+}
+add_action( 'admin_init', 'wpcom_jp_mini_featured_init' );
+
+/**
+ * Maybe conditionally add the filter to add the featured image to the title
+ * area of minileven.
+ */
+function wpcom_tweakjp_maybe_add_filter() {
+
+	// Bail if not mobile
+	if ( ! jetpack_is_mobile() )
+		return;
+
+	// Do we want to display the Featured images only on the home page?
+	if ( ! is_home() && get_option( 'jp_mini_featured_evwhere' ) )
+		return;
+
+	add_filter( 'the_title', 'wpcom_tweakjp_minileven_featuredimage' );
+}
+add_action( 'wp_head', 'tweakjp_maybe_add_filter' );
+
+/**
+ * Maybe conditionally add the featured image to the title area of minileven
+ *
+ * @param string $title
+ * @return string
+ */
+function wpcom_tweakjp_minileven_featuredimage( $title = '' ) {
+
+	// Maybe add the thumbnail after the title
+	if ( has_post_thumbnail() && in_the_loop() ) {
+		$title = $title . get_the_post_thumbnail();
+	}
+
+	return $title;
+}
+
+/**
+ * Admin bar for logged out users setting field
+ */
+function wpcom_jp_mini_featured_evwhere() {
+?>
+	
+	<input id="jp_mini_featured_evwhere" name="jp_mini_featured_evwhere" type="checkbox" value="1" <?php checked( true, (bool) get_option( 'jp_mini_featured_evwhere' ), true ); ?> />
+	<label for="jp_mini_featured_evwhere"><?php _e( 'Also show on single posts and archives', 'jetpack' ); ?></label>
+
+<?php
+}
