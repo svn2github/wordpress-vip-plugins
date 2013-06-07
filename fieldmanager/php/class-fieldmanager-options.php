eee30e0b001879f8f633aa55d597765adffe55d1
nickdaugherty <nickdaugherty@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-06-07 18:07:22 +0000

    Sync with latest upstream/master.
    Changes how forms are rendered - now all are abstracted into child calsses of Fieldmanager_Field
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@108406 dd790b09-5c1c-0410-8744-875116bcd8a6

--- fieldmanager/php/class-fieldmanager-options.php
+++ fieldmanager/php/class-fieldmanager-options.php
@@ -35,12 +35,6 @@ abstract class Fieldmanager_Options extends Fieldmanager_Field {
 	public $first_element = array();
 
 	/**
-	 * @var Fieldmanager_Datasource
-	 * Optionally generate field from datasource
-	 */
-	public $datasource = Null;
-
-	/**
 	 * @var boolean
 	 * Allow multiple selections?
 	 */
@@ -57,7 +51,7 @@ abstract class Fieldmanager_Options extends Fieldmanager_Field {
 	 * @param string $label
 	 * @param mixed $options
 	 */
-	public function __construct( $label= '', $options = array() ) {
+	public function __construct( $label = '', $options = array() ) {
 		parent::__construct( $label, $options );
 
 		if ( !empty( $this->options ) ) {
@@ -160,7 +154,15 @@ abstract class Fieldmanager_Options extends Fieldmanager_Field {
 	 * A single element for a single bit of data, e.g. '<option>'
 	 * @param mixed $value
 	 */
-	public abstract function form_data_element( $value );
+	public function form_data_element( $data_row, $value ) {
+		if ( !$this->options_template ) {
+			$tpl_slug = 'options-' . strtolower( str_replace( 'Fieldmanager_', '', get_class( $this ) ));
+			$this->options_template = fieldmanager_get_template( $tpl_slug );
+		}
+		ob_start();
+		include $this->options_template;
+		return ob_get_clean();
+	}
 
 	/**
 	 * Helper for output functions to toggle a selected options
@@ -236,7 +238,32 @@ abstract class Fieldmanager_Options extends Fieldmanager_Field {
 		
 		$this->data[] = $data;
 	}
-
+	
+	/**
+	 * Helper function to get the list of default meta boxes to remove.
+	 * If $remove_default_meta_boxes is true and the datasource is Fieldmanager_Datasource_Term, 
+	 * this will return a list of all default meta boxes for the specified taxonomies.
+	 * We only need to return id and context since the page will be handled by the list of post types provided to add_meta_box.
+	 * Otherwise, this will just return an empty array.
+	 * @param array current list of meta boxes to remove
+	 * @return array list of meta boxes to remove
+	 */
+	protected function add_meta_boxes_to_remove( &$meta_boxes_to_remove ) {
+		if ( $this->remove_default_meta_boxes && get_class( $this->datasource ) == 'Fieldmanager_Datasource_Term' ) {
+			// Iterate over the list and build the list of meta boxes
+			$meta_boxes = array();
+			foreach( $this->datasource->get_taxonomies() as $taxonomy ) {
+				$id = 'tagsdiv-' . $taxonomy;
+				$meta_boxes[$id] = array(
+					'id' => $id,
+					'context' => 'side'
+				);
+			}
+			
+			// Merge in the new meta boxes to remove
+			$meta_boxes_to_remove = array_merge( $meta_boxes_to_remove, $meta_boxes );
+		}
+	}
 }
 
 require_once( dirname( __FILE__ ) . '/class-fieldmanager-select.php' );
