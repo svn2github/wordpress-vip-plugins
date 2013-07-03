dc804c00163e4d23fb6a0610ccbb51db18e1d69c
betzster <betzster@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-07-03 14:14:34 +0000

    Reverting 110868
    git-svn-id: https://vip-svn.wordpress.com/plugins@110869 dd790b09-5c1c-0410-8744-875116bcd8a6

--- fieldmanager/php/context/class-fieldmanager-context.php
+++ fieldmanager/php/context/class-fieldmanager-context.php
@@ -9,16 +9,6 @@
  */
 abstract class Fieldmanager_Context {
 	
-	/**
-	 * @var Fieldmanager_Field
-	 * The base field associated with this context
-	 */
 	public $fm = Null;
-	
-	/**
-	 * @var string
-	 * Unique ID of the form. Used for forms that are not built into WordPress.
-	 */
-	public $uniqid;
 
 }
