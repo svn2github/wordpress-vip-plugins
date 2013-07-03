c1a796a71ef04e0c66c9d59f932c02a22f0f228e
betzster <betzster@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-07-03 14:09:42 +0000

    Merge fieldmanager changes from Github
    /z 18862
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@110868 dd790b09-5c1c-0410-8744-875116bcd8a6

--- fieldmanager/php/context/class-fieldmanager-context.php
+++ fieldmanager/php/context/class-fieldmanager-context.php
@@ -9,6 +9,16 @@
  */
 abstract class Fieldmanager_Context {
 	
+	/**
+	 * @var Fieldmanager_Field
+	 * The base field associated with this context
+	 */
 	public $fm = Null;
+	
+	/**
+	 * @var string
+	 * Unique ID of the form. Used for forms that are not built into WordPress.
+	 */
+	public $uniqid;
 
 }
