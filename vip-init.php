e696295d9c830b3f9b52e2f587756331cd21aed2
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-07-24 21:30:15 +0000

    vip-init: for local environments, load a copy of wpcom-sitemap.
    If a theme/site is already loading it, trigger an error.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@113257 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -18,6 +18,12 @@ if ( false === WPCOM_IS_VIP_ENV ) {
 	if ( ! function_exists( 'jetpack_is_mobile' ) )
 		require_once( __DIR__ . '/vip-do-not-include-on-wpcom/is-mobile.php' );
 	require_once( __DIR__ . '/vip-do-not-include-on-wpcom/wpcom-functions.php' );
+
+	if ( function_exists( 'wpcom_print_sitemap' ) ) {
+		trigger_error( 'You are loading a copy of the wpcom-sitemap plugin. This is no longer necessary and will be automattically loaded for you. Please remove your local copy to remove this error.', E_USER_WARNING );
+	} else {
+		require_once( __DIR__ . '/vip-do-not-include-on-wpcom/wpcom-plugins/wpcom-sitemap.php' );
+	}
 }
 
 // Load the "works everywhere" helper file
