64336700eef9a21406e323ba716e8a88cf202289
djpaulgibbs <djpaulgibbs@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-09-18 12:09:11 +0000

    Allow Contributors to Upload: fix problem caused by changes to a helper function in r119407
    vip_contrib_add_upload_cap() doesn't exist at plugins_loaded, so wrap the code in a function hooked to init.
    
    /z 21427
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@119661 dd790b09-5c1c-0410-8744-875116bcd8a6

--- wpcom-allow-contributors-to-upload/wpcom-allow-contributors-to-upload.php
+++ wpcom-allow-contributors-to-upload/wpcom-allow-contributors-to-upload.php
@@ -4,9 +4,12 @@ Plugin Name: WordPress.com Allow Contributors to Upload
 Plugin URI: http://automattic.com
 Description: Allow contributors to upload media. They still won't be able to publish.
 Author: Automattic
-Version: 1.0
+Version: 1.1
 Author URI: http://automattic.com
 */
 
-if ( function_exists( 'vip_contrib_add_upload_cap' ) )
-	vip_contrib_add_upload_cap();
+function wpcom_vip_allow_contributor_uploads() {
+	if ( function_exists( 'vip_contrib_add_upload_cap' ) )
+		vip_contrib_add_upload_cap();
+}
+add_action( 'init', 'wpcom_vip_allow_contributor_uploads', 8 );
