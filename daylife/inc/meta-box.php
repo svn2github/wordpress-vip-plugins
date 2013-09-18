svn2github/wordpress-vip-plugins

commit 9efe412f69441c986348dd1219bd9b6a9b5c472d

Make sure the heights and widths we're allowing here are non-negative integers, since we need to escape and negative integers don't make sense here.
See ticket 21702.



git-svn-id: https://vip-svn.wordpress.com/plugins@119615 dd790b09-5c1c-0410-8744-875116bcd8a6

daylife/inc/meta-box.php
@@ -146,9 +146,9 @@ class Daylife_Meta_Box {
 				$width = round( $height / $ratio );
 			}
 
-			$url = str_replace( '/45x45.jpg', '/' . $width . 'x' . $height . '.jpg', $image->thumb_url );
+			$url = str_replace( '/45x45.jpg', '/' . absint( $width ) . 'x' . absint( $height ) . '.jpg', $image->thumb_url );
 			echo '<div class="daylife-image-wrap">';
-			echo '<img src="' . esc_url( $url ) . '" alt="' . esc_attr( $image->caption ) . '" data-thumb_url="' . esc_attr( $image->thumb_url ) . '" data-url="' . esc_attr( $image->url ) . '" data-credit="' . esc_attr( $image->credit ) . '" data-caption="' . esc_attr( $image->caption ) . '" data-daylife_url="' . esc_attr( $image->daylife_url ) . '" data-image_title="' . esc_attr( $image->image_title ) . '" data-width="' . esc_attr( $width ) . '" data-height="' . esc_attr( $height ) . '" />';
+			echo '<img src="' . esc_url( $url ) . '" alt="' . esc_attr( $image->caption ) . '" data-thumb_url="' . esc_attr( $image->thumb_url ) . '" data-url="' . esc_attr( $image->url ) . '" data-credit="' . esc_attr( $image->credit ) . '" data-caption="' . esc_attr( $image->caption ) . '" data-daylife_url="' . esc_attr( $image->daylife_url ) . '" data-image_title="' . esc_attr( $image->image_title ) . '" data-width="' . absint( $width ) . '" data-height="' . absint( $height ) . '" />';
 			echo '<div class="daylife-overlay" title="' . esc_attr( $image->caption ) . "\r\nSource: " . esc_attr( $image->source->name ) . "\r\nDate: " . esc_attr( date_i18n( get_option( 'date_format' ), $image->timestamp_epoch ) ) . '"></div>';
 			echo '<button class="daylife-ste button">' . __( 'Insert into Post', 'daylife' ) . '</button>';
 			echo '</div>';

commit 59952da2daff22aaac17d66d59de4a4e37d3f2b5

daylife: fix portrait image issue introduced in r119615 /z 21742
git-svn-id: https://vip-svn.wordpress.com/plugins@119655 dd790b09-5c1c-0410-8744-875116bcd8a6

daylife/inc/meta-box.php
@@ -143,7 +143,7 @@ class Daylife_Meta_Box {
 				$height = round( $width / $ratio );
 			} else {
 				$height = apply_filters( 'daylife-image-default-thumb', 200 );
-				$width = round( $height / $ratio );
+				$width = round( $height * $ratio );
 			}
 
 			$url = str_replace( '/45x45.jpg', '/' . absint( $width ) . 'x' . absint( $height ) . '.jpg', $image->thumb_url );