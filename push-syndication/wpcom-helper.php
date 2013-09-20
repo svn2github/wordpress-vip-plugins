e1c50746e3fb56da2633ab2d996ccd4b113cd9cd
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-09-20 05:42:04 +0000

    syndication: Make xml-pull avaialable to all clients
    git-svn-id: https://vip-svn.wordpress.com/plugins@119888 dd790b09-5c1c-0410-8744-875116bcd8a6

--- push-syndication/wpcom-helper.php
+++ push-syndication/wpcom-helper.php
@@ -1,16 +1,5 @@
 <?php
 
-// XML Client only for a select few for now
-add_filter( 'syn_transports', function( $transports ) {
-	if ( false === WPCOM_IS_VIP_ENV )
-		return $transports;
-
-	if ( ! in_array( parse_url( site_url(), PHP_URL_HOST ), array( 'instylemobile.wordpress.com', 'ewmobile.wordpress.com' ) ) )
-		unset( $transports['WP_XML'] );
-
-	return $transports;
-} );
-
 add_action( 'syn_post_pull_edit_post', function() {
 	wpcom_push_syndication_stats( 'vip-syndication-pull', 'edit' );
 } );
