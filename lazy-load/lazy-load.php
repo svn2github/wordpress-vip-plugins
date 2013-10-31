abb45783e18700af72c2cad511af4c30b738a37b
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-10-31 04:12:17 +0000

    lazy-load: setup_filters should be a static function
    h/t suniel at ama
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@124236 dd790b09-5c1c-0410-8744-875116bcd8a6

--- lazy-load/lazy-load.php
+++ lazy-load/lazy-load.php
@@ -30,7 +30,7 @@ class LazyLoad_Images {
 		add_action( 'wp_head', array( __CLASS__, 'setup_filters' ), 9999 ); // we don't really want to modify anything in <head> since it's mostly all metadata, e.g. OG tags
 	}
 
-	function setup_filters() {
+	static function setup_filters() {
 		add_filter( 'the_content', array( __CLASS__, 'add_image_placeholders' ), 99 ); // run this later, so other content filters have run, including image_add_wh on WP.com
 		add_filter( 'post_thumbnail_html', array( __CLASS__, 'add_image_placeholders' ), 11 );
 		add_filter( 'get_avatar', array( __CLASS__, 'add_image_placeholders' ), 11 );
