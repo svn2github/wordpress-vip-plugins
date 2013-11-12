6c7b3e274908e92b9b1579d316cc5047688ab45c
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-11-12 17:20:38 +0000

    wpcom-geo: Introduce two modes to the plugin: simple and advanced
    * Simple mode is more purely country-based geolocation that relies on our nginx-based geolocation handling; this mode no longer uses cookies.
    * Advanced mode uses the original method of geolocation (using an AJAX request to determine location and set a cookie)
    
    Phase 1 of 2 for performance improvements to the plugin.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@125624 dd790b09-5c1c-0410-8744-875116bcd8a6

--- wpcom-geo-uniques/wpcom-geo-uniques.php
+++ wpcom-geo-uniques/wpcom-geo-uniques.php
@@ -36,6 +36,7 @@ class WPCOM_Geo_Uniques {
 	private static $expiry_time = 604800; // 1 week
 	private static $default_location = 'default';
 	private static $supported_locations = array();
+	private static $simple_mode = true;
 
 	static function after_setup_theme() {
 		if ( is_admin() )
@@ -48,20 +49,28 @@ class WPCOM_Geo_Uniques {
 			return;
 
 		// Add default to list of supported countries
-		static::add_location( static::$default_location );
+		self::add_location( self::get_default_location() );
 
 		// If the theme hasn't registered any locations, bail. 
 		$locations = self::get_registered_locations();
 		if ( count( $locations ) <= 1 )
 			return;
 
+		static::$simple_mode = (bool) apply_filters( 'wpcom_geo_simple_mode', static::$simple_mode );
+
+		// Note: For simple mode, we don't need to init anything simple location handling is on-demand
+		if ( false === static::$simple_mode ) {
+			self::init_advanced_geolocation();
+		}
+	}
+
+	static function init_advanced_geolocation() {
 		// Handle location detection on parse_request so we know the context of the request
-		// Do it as soon as possible!
 		add_action( 'parse_request', array( 'WPCOM_Geo_Uniques', 'action_parse_request' ) );
 	}
 
 	static function action_parse_request( $request ) {
-		// Don't do this on feed requests or robot.txt
+		// Don't do this on feed requests or robots.txt
 		if ( ! empty( $request->query_vars['feed'] ) || ! empty( $request->query_vars['robots'] ) )
 			return;
 
@@ -106,11 +115,11 @@ class WPCOM_Geo_Uniques {
 
 			if ( ! cookies_enabled ) {
 				document.cookie = '__testcookie=1';
-				if ( -1 !== document.cookie.indexOf( '__testcookie=1' ) ) {
+				if ( -1 !== document.cookie.indexOf( '__wpcomgeo_testcookie=1' ) ) {
 					cookies_enabled = true;
 				}
 				var expired_date = new Date( 2003, 5, 27 );
-				document.cookie = '__testcookie=1;expires=' + expired_date.toUTCString();
+				document.cookie = '__wpcomgeo_testcookie=1;expires=' + expired_date.toUTCString();
 			}
 
 			if ( cookies_enabled ) {
@@ -123,9 +132,18 @@ class WPCOM_Geo_Uniques {
 	<?php
 	}
 
+	static function is_valid_location( $location ) {
+		return in_array( $location, static::$supported_locations );
+	}
+
+	static function get_default_location() {
+		return static::$default_location;
+	}
+
 	static function set_default_location( $location ) {
-		if ( in_array( $location, static::$supported_locations ) )
+		if ( self::is_valid_location( $location ) ) {
 			static::$default_location = $location;
+		}
 	}
 
 	static function add_location( $location ) {
@@ -142,31 +160,40 @@ class WPCOM_Geo_Uniques {
 		if ( isset( $user_location ) )
 			return $user_location;
 
-		if ( ! self::user_has_location() )
-			return static::$default_location;
+		if ( static::$simple_mode ) {
+			$user_location = self::get_user_location_from_global( '$_SERVER[ "GEOIP_COUNTRY_CODE" ]' );
+		} else {
+			$user_location = self::get_user_location_from_global( sprintf( '$_COOKIE[ "%s" ]', static::COOKIE_NAME ) );
+		}
 
+		return $user_location;
+	}
+
+	static function get_user_location_from_global( $global_var ) {
 		$checks = array();
-		foreach ( static::$supported_locations as $location ) {
+		foreach ( self::get_registered_locations() as $location ) {
 			$checks[] = sprintf(
-				'( "%1$s" == $_COOKIE[ "%2$s" ] ) return "%1$s";',
+				'( "%1$s" == strtolower( %2$s ) ) { return "%1$s"; }',
 				$location,
-				static::COOKIE_NAME
+				$global_var
 			);
 		}
 
-		$test = sprintf( 'if %s', implode( ' elseif ', $checks ) );
-		$test .= sprintf( ' else return "%s";', static::$default_location );
+		$test  = sprintf( 'if ( empty( %s ) ) { return "%s"; }', $global_var, esc_js( self::get_default_location() ) );
+		$test .= sprintf( ' elseif %s', implode( ' elseif ', $checks ) );
+		$test .= sprintf( ' else { return "%s"; }', esc_js( self::get_default_location() ) );
 
 		$user_location = static::run_vary_cache_on_function( $test );
 		return $user_location;
 	}
 
 	static function user_has_location() {
+		// TODO: should currently only be used in advanced mode
 		return static::run_vary_cache_on_function( 'return isset( $_COOKIE[ "' . self::COOKIE_NAME . '" ] );' );
 	}
 
 	private static function ip2location( $location_type = 'country_short' ) {
-		$location_full = self::get_geolocation_data();
+		$location_full = self::get_geolocation_data_from_ip();
 
 		if ( $location_full && property_exists( $location_full, $location_type ) )
 			$location = $location_full->$location_type;
@@ -179,9 +206,9 @@ class WPCOM_Geo_Uniques {
 		return strtolower( $location );
 	}
 
-	private static function get_geolocation_data() {
+	private static function get_geolocation_data_from_ip() {
 
-		$location_full = apply_filters( 'wpcom_geo_pre_get_geolocation_data', false, $_SERVER['REMOTE_ADDR'] );
+		$location_full = apply_filters( 'wpcom_geo_pre_get_geolocation_data_from_ip', false, $_SERVER['REMOTE_ADDR'] );
 		if ( false !== $location_full )
 			return $location_full;
 
