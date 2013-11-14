svn2github/wordpress-vip-plugins

commit 2a82d3158ae9e730df00ac7a62759d2bfd3f6441

add-meta-tags: Add support for all term pages, not just categories.
git-svn-id: https://vip-svn.wordpress.com/plugins@120209 dd790b09-5c1c-0410-8744-875116bcd8a6

add-meta-tags-mod/add-meta-tags.php
@@ -646,22 +646,22 @@ class Add_Meta_Tags {
 			}
 
 
-		} elseif ( is_category() ) {
+		} elseif ( is_tax() ) {
 			/*
-			Writes a description META tag only if a description for the current category has been set.
+			Writes a description META tag only if a description for the current term has been set.
 			*/
 
-			$cur_cat_desc = category_description();
+			$cur_cat_desc = term_description();
 			if ( $cur_cat_desc ) {
-				$my_metatags .= "\n<meta name=\"description\" content=\"" . esc_attr( $this->amt_clean_desc($cur_cat_desc) ) . "\" />";
+				$my_metatags .= '<meta name="description" content="' . esc_attr( $this->amt_clean_desc($cur_cat_desc) ) . '" />';
 			}
 			
 			/*
-			Write a keyword metatag if there is a category name (always)
+			Write a keyword metatag if there is a term name (always)
 			*/
-			$cur_cat_name = single_cat_title($prefix = '', $display = false );
+			$cur_cat_name = single_term_title( '', false );
 			if ( $cur_cat_name ) {
-				$my_metatags .= "\n<meta name=\"keywords\" content=\"" . esc_attr( strtolower($cur_cat_name) ) . "\" />";
+				$my_metatags .= '<meta name="keywords" content="' . esc_attr( strtolower($cur_cat_name) ) . '" />';
 			}
 		}
 

commit a76b5766d42941385e59eac4e28e83b7e41e4705

add-meta-tags:
Fix bug introduced in r120209 categories and tags were no longer working because of how is_tax() behaves

/z 23626

props stephaneboisvert


git-svn-id: https://vip-svn.wordpress.com/plugins@125933 dd790b09-5c1c-0410-8744-875116bcd8a6

add-meta-tags-mod/add-meta-tags.php
@@ -653,7 +653,7 @@ class Add_Meta_Tags {
 			}
 
 
-		} elseif ( is_tax() ) {
+		} elseif ( is_tax() || is_tag() || is_category() ) {
 			/*
 			Writes a description META tag only if a description for the current term has been set.
 			*/