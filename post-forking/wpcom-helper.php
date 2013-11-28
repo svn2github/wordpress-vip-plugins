2bd9d865bd67999db8163ee64145c142a86e5d75
djpaulgibbs <djpaulgibbs@dd790b09-5c1c-0410-8744-875116bcd8a6>
2013-11-28 16:43:41 +0000

    Post Forking: update to version 0.3.0-alpha
    * Allows editors and authors to fork posts.
    * Permission checks now made against parent post, not the fork post itself.
    * Sync'd to https://github.com/automattic/post-forking at revision 0be767c
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@127645 dd790b09-5c1c-0410-8744-875116bcd8a6

--- post-forking/wpcom-helper.php
+++ post-forking/wpcom-helper.php
@@ -1,8 +1,9 @@
 <?php
-// Add capabilities to admin role for the Post Forking plugin
+// Add capabilities to admin + editor + author roles for the Post Forking plugin
 add_action( 'init', function() {
 	wpcom_vip_add_role_caps( 'administrator', array(
 		'edit_forks',
+		'edit_fork',
 		'edit_others_forks',
 		'edit_private_forks',
 		'edit_published_forks',
@@ -10,6 +11,37 @@ add_action( 'init', function() {
 		'read_private_forks',
 		'delete_forks',
 		'delete_others_forks',
+		'delete_private_forks',
+		'delete_published_forks',
+		'publish_forks',
+	) );
+
+	wpcom_vip_add_role_caps( 'editor', array(
+		'edit_forks',
+		'edit_fork',
+		'edit_others_forks',
+		'edit_private_forks',
+		'edit_published_forks',
+		'read_forks',
+		'read_private_forks',
+		'delete_forks',
+		'delete_others_forks',
+		'delete_private_forks',
+		'delete_published_forks',
+		'publish_forks',
+	) );
+	wpcom_vip_remove_role_caps( 'editor', array(
+		'edit_others_forks',
+		'delete_others_forks',
+	) );
+
+	wpcom_vip_add_role_caps( 'author', array(
+		'edit_forks',
+		'edit_private_forks',
+		'edit_published_forks',
+		'read_forks',
+		'read_private_forks',
+		'delete_forks',
 		'delete_private_forks',
 		'delete_published_forks',
 		'publish_forks',
