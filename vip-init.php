79a319169ad9261c85fc475c66cbec7ee10e9891
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-01-02 05:08:38 +0000

    vip-init: include missing require call from r130921
    git-svn-id: https://vip-svn.wordpress.com/plugins@130922 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-init.php
+++ vip-init.php
@@ -3,6 +3,7 @@
 define( 'WPCOM_IS_VIP_ENV', function_exists( 'wpcom_is_vip' ) );
 
 // Load our development and environment helpers
+require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-utils.php' );
 require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-local-development-helper/vip-local-development-helper.php' );
 require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powered-wpcom.php' );
 require_once( __DIR__ . '/vip-do-not-include-on-wpcom/vip-roles.php' );
