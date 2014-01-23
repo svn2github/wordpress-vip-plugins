4cf9ca7820127cfefb061ca6a07b37c9f776b270
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-01-23 01:09:49 +0000

    wpcom-legacy-redirector: allow redirecting to non-post URLs (like categories and terms).
    Allow the insert method to accept a non-post redirect_to value and store that in the post_excerpt field.
    
    Also updates docs to note these new features and updates cache group to flush caches since we now store the ID of the redirect post, not the post parent.
    
    h/t moguldomrich (for the initial idea and earlier code)
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@133769 dd790b09-5c1c-0410-8744-875116bcd8a6

--- wpcom-legacy-redirector/wpcom-legacy-redirector.php
+++ wpcom-legacy-redirector/wpcom-legacy-redirector.php
@@ -1,13 +1,26 @@
 <?php
 /**
  * Plugin Name: WPCOM Legacy Redirector
- * Description: Simple plugin for handling legacy redirects in a scalable manner. This is a no-frills plugin (no UI, for example).
+ * Description: Simple plugin for handling legacy redirects in a scalable manner.
+ *
+ * This is a no-frills plugin (no UI, for example). Data entry needs to be bulk-loaded via the wp-cli commands provided or custom scripts.
  * 
+ * Redirects are stored as a custom post type and use the following fields:
+ *
+ * - post_name for the md5 hash of the "from" path or URL.
+ *  - we use this column, since it's indexed and queries are super fast.
+ *  - we also use an md5 just to simplify the storage.
+ * - post_title to store the non-md5 version of the "from" path.
+ * - one of either:
+ *  - post_parent if we're redirect to a post; or
+ *  - post_excerpt if we're redirecting to an alternate URL.
+ *
  * Please contact us before using this plugin.
  */
 
 class WPCOM_Legacy_Redirector {
 	const POST_TYPE = 'vip-legacy-redirect';
+	const CACHE_GROUP = 'vip-legacy-redirect-2';
 
 	static function start() {
 		add_action( 'init', array( __CLASS__, 'init' ) );
@@ -38,37 +51,59 @@ class WPCOM_Legacy_Redirector {
 		}
 	}
 
-	static function insert_legacy_redirect( $url, $post_id ) {
-		// We need pre-slashed URLs
-		if ( 0 !== strpos( $url, '/' ) )
-			$url = '/' . $url;
+	/**
+ 	 *
+ 	 * @param string $from_url URL or path that should be redirected; should have leading slash if path.
+ 	 * @param int|string $redirect_to The post ID or URL to redirect to.
+ 	 * @return bool|WP_Error Error if invalid redirect URL specified; true otherwise. 
+ 	 */
+	static function insert_legacy_redirect( $from_url, $redirect_to ) {
 
-		$url_hash = self::get_url_hash( $url );
-		wp_insert_post( array(
-			'post_parent' => $post_id,
-			'post_name' => $url_hash,
-			'post_title' => $url,
+		$from_url = esc_url_raw( $from_url );
+		$from_url_hash = self::get_url_hash( $from_url );
+
+		$args = array(
+			'post_name' => $from_url_hash,
+			'post_title' => $from_url,
 			'post_type' => self::POST_TYPE,
-		) );
+		);
 
-		wp_cache_delete( $url_hash, self::POST_TYPE );
+		if ( is_numeric( $redirect_to ) ) {
+			$args['post_parent'] = $redirect_to;
+		} elseif ( false !== parse_url( $redirect_to ) ) {
+			$args['post_excerpt'] = esc_url_raw( $redirect_to );
+		} else {
+			return new WP_Error( 'invalid-redirect-url', 'Invalid redirect_to param; should be a post_id or a URL' );
+		}
+
+		wp_insert_post( $args );
+
+		wp_cache_delete( $from_url_hash, self::CACHE_GROUP );
+
+		return true;
 	}
 
 	static function get_redirect_uri( $url ) {
-		global $wpdb;
-		
 		$url = urldecode( $url );
 		$url_hash = self::get_url_hash( $url );
 
-		$redirect_post_id = wp_cache_get( $url_hash, self::POST_TYPE );
+		$redirect_post_id = wp_cache_get( $url_hash, self::CACHE_GROUP );
 
 		if ( false === $redirect_post_id ) {
 			$redirect_post_id = self::get_redirect_post_id( $url );
-			wp_cache_add( $url_hash, $redirect_post_id, self::POST_TYPE );
+			wp_cache_add( $url_hash, $redirect_post_id, self::CACHE_GROUP );
 		}
 
-		if ( $redirect_post_id )
-			return get_permalink( $redirect_post_id );
+		if ( $redirect_post_id ) {
+			$redirect_post = get_post( $redirect_post_id );
+
+			if ( 0 !== $redirect_post->post_parent ) {
+				return get_permalink( $redirect_post->post_parent );
+			} elseif ( ! empty( $redirect_post->post_excerpt ) ) {
+				return esc_url_raw( $redirect_post->post_excerpt );
+			}
+		}
+
 		return false;
 	}
 
@@ -77,7 +112,7 @@ class WPCOM_Legacy_Redirector {
 
 		$url_hash = self::get_url_hash( $url );
 
-		$redirect_post_id = $wpdb->get_var( $wpdb->prepare( "SELECT post_parent FROM $wpdb->posts WHERE post_type = %s AND post_name = %s LIMIT 1", self::POST_TYPE, $url_hash ) );
+		$redirect_post_id = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_type = %s AND post_name = %s LIMIT 1", self::POST_TYPE, $url_hash ) );
 
 		if ( ! $redirect_post_id )
 			$redirect_post_id = 0;
