6a086d7aea07b095d7d5667f631a1b8a8c951fcb
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-01-24 22:25:40 +0000

    newscred: passthrough nc_hourly_plugin_hook to jobs as well
    git-svn-id: https://vip-svn.wordpress.com/plugins@134087 dd790b09-5c1c-0410-8744-875116bcd8a6

--- newscred/wpcom-helper.php
+++ newscred/wpcom-helper.php
@@ -4,6 +4,8 @@
 // This allows certain functionality like the thumbnail download to work.
 add_filter( 'wpcom_vip_passthrough_cron_to_jobs', function( $events ) {
 		$events[] = 'nc_mins_plugin_hook';
-			return $events;
+		$events[] = 'nc_hourly_plugin_hook';
+
+		return $events;
 } );
 
