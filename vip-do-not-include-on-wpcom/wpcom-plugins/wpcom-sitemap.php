2db846bb51c473e7520186f14d8244678d5751e3
stephboisvert <stephboisvert@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-02-04 20:08:14 +0000

    Change Sitenamp Namespace from N to News in schema declaration
    git-svn-id: https://vip-svn.wordpress.com/plugins@135680 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/wpcom-plugins/wpcom-sitemap.php
+++ vip-do-not-include-on-wpcom/wpcom-plugins/wpcom-sitemap.php
@@ -330,7 +330,7 @@ function wpcom_print_news_sitemap($format) {
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
 	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-	xmlns:n="http://www.google.com/schemas/sitemap-news/0.9"
+	xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
 	xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
 	>
 <?php
