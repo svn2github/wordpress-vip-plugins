6ebb306739f590bbf8522ef24a857c170e16182c
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-02-05 21:47:52 +0000

    simple-page-ordering: update to 2.1.2 (better post type handling and support and other cleanup)
    git-svn-id: https://vip-svn.wordpress.com/plugins@135858 dd790b09-5c1c-0410-8744-875116bcd8a6

--- simple-page-ordering/simple-page-ordering.dev.js
+++ simple-page-ordering/simple-page-ordering.dev.js
@@ -1,78 +1,103 @@
 function update_simple_ordering_callback(response) {
-	if ( response == 'children' ) { window.location.reload(); return; }
+	if ( 'children' === response ) {
+		window.location.reload();
+		return;
+	}
 
-	var changes = jQuery.parseJSON(response);
+	var changes = jQuery.parseJSON( response );
 
-	if ( changes['id'] != null ) {
-		jQuery.post( ajaxurl, { action: 'simple_page_ordering', id: changes['id'], previd: changes['previd'], nextid: changes['nextid'], start: changes['start'] }, update_simple_ordering_callback );
-	} else {
-		jQuery.each(changes, function(key,value) {
-			if ( jQuery('#inline_'+key).length > 0 ) {
-				if ( value['menu_order'] != null ) {
-					jQuery('#inline_'+key+' .menu_order').html( value['menu_order'] );
-					jQuery('#inline_'+key+' .post_parent').html( value['post_parent'] );
-					var post_title = jQuery('#inline_'+key+' .post_title').html();
-					var dashes = 0;
-	  				while ( dashes < value['depth'] ) { post_title = '&mdash; ' + post_title; dashes++; }
-	  				jQuery('tr#post-'+key+' a.row-title').html( post_title );
-				} else {
-					jQuery('#inline_'+key+' .menu_order').html(value);
+	var new_pos = changes.new_pos;
+	for ( var key in new_pos ) {
+		if ( 'next' === key )
+			continue;
+
+		var inline_key = document.getElementById('inline_' + key);
+		if ( null !== inline_key && new_pos.hasOwnProperty(key) ) {
+			var dom_menu_order = inline_key.querySelector('.menu_order');
+
+			if ( undefined !== new_pos[key]['menu_order'] ) {
+				if ( null !== dom_menu_order )
+					dom_menu_order.innerHTML = new_pos[key]['menu_order'];
+
+				var dom_post_parent = inline_key.querySelector('.post_parent');
+				if ( null !== dom_post_parent )
+					dom_post_parent.innerHTML = new_pos[key]['post_parent'];
+
+				var post_title = null;
+				var dom_post_title = inline_key.querySelector('.post_title');
+				if ( null !== dom_post_title )
+					post_title = dom_post_title.innerHTML;
+
+				var dashes = 0;
+				while (dashes < new_pos[key]['depth']) {
+					post_title = '&mdash; ' + post_title;
+					dashes++;
 				}
+				var dom_row_title = inline_key.parentNode.querySelector('.row-title');
+				if ( null !== dom_row_title && null !== post_title )
+					dom_row_title.innerHTML = post_title;
+			} else if ( null !== dom_menu_order ) {
+				dom_menu_order.innerHTML = new_pos[key];
 			}
-		});
-		jQuery('.check-column .waiting').siblings('input').show().siblings('img').remove();
-		jQuery('tr.iedit').css('cursor','move');
-		jQuery(".wp-list-table tbody").sortable('enable');
+		}
+	}
+
+	if ( changes.next ) {
+		jQuery.post( ajaxurl, {
+			action: 'simple_page_ordering',
+			id: changes.next['id'],
+			previd: changes.next['previd'],
+			nextid: changes.next['nextid'],
+			start: changes.next['start'],
+			excluded: changes.next['excluded']
+		}, update_simple_ordering_callback );
+	} else {
+		jQuery(document.querySelector('.spo-updating-row')).removeClass('spo-updating-row');
+		sortable_post_table.removeClass('spo-updating').sortable('enable');
 	}
 }
 
-jQuery('tr.iedit').css('cursor','move');
-
-jQuery(".wp-list-table tbody").sortable({
-	items: 'tr:not(.inline-edit-row)',
+var sortable_post_table = jQuery( document.querySelector(".wp-list-table tbody") );
+sortable_post_table.sortable({
+	items: '> tr',
 	cursor: 'move',
 	axis: 'y',
 	containment: 'table.widefat',
 	scrollSensitivity: 40,
-	helper: function(e, ui) {
-		ui.children().each(function() { jQuery(this).width(jQuery(this).width()); });
-		return ui;
-	},
-	start: function(event, ui) {
-		if ( ! ui.item.hasClass('alternate') ) ui.item.css( 'background-color', '#ffffff' );
-		ui.item.children('td,th').css('border-bottom-width','0');
-		ui.item.css( 'outline', '1px solid #dfdfdf' );
-	},
-	stop: function(event, ui) {
-		ui.item.removeAttr('style');
-		ui.item.children('td,th').css('border-bottom-width','1px');
-	},
+	cancel:	'.inline-edit-row',
+	distance: 5,
+	opacity: .85,
+	forceHelperSize: true,
 	update: function(event, ui) {
-		jQuery('tr.iedit').css('cursor','default');
-		jQuery(".wp-list-table tbody").sortable('disable');
+		sortable_post_table.sortable('disable').addClass('spo-updating');
+		ui.item.addClass('spo-updating-row');
 
-		var postid = ui.item.attr('id').substr(5); // post id
+		var postid = ui.item[0].id.substr(5); // post id
 
 		var prevpostid = false;
 		var prevpost = ui.item.prev();
-		if ( prevpost.length > 0 )
+		if ( prevpost.length > 0 ) {
 			prevpostid = prevpost.attr('id').substr(5);
+		}
 
 		var nextpostid = false;
 		var nextpost = ui.item.next();
-		if ( nextpost.length > 0 )
+		if ( nextpost.length > 0 ) {
 			nextpostid = nextpost.attr('id').substr(5);
-
-		// show spinner
-		ui.item.find('.check-column input').hide().after('<img alt="processing" src="images/wpspin_light.gif" class="waiting" style="margin-left: 6px;" />');
+		}
 
 		// go do the sorting stuff via ajax
 		jQuery.post( ajaxurl, { action: 'simple_page_ordering', id: postid, previd: prevpostid, nextid: nextpostid }, update_simple_ordering_callback );
 
 		// fix cell colors
-		jQuery( 'tr.iedit' ).each(function(i){
-			if ( i%2 == 0 ) jQuery(this).addClass('alternate');
-			else jQuery(this).removeClass('alternate');
-		});
+		var table_rows = document.querySelectorAll('tr.iedit'),
+			table_row_count = table_rows.length;
+		while( table_row_count-- ) {
+			if ( table_row_count%2 == 0 ) {
+				jQuery( table_rows[table_row_count]).addClass('alternate');
+			} else {
+				jQuery( table_rows[table_row_count]).removeClass('alternate');
+			}
+		}
 	}
 });
--- simple-page-ordering/simple-page-ordering.php
+++ simple-page-ordering/simple-page-ordering.php
@@ -1,35 +1,15 @@
 <?php
 /**
- Plugin Name: Simple Page Ordering 2 - VIP
- Plugin URI: http://10up.com/plugins/simple-page-ordering-wordpress/
- Description: Order your pages and hierarchical post types using drag and drop on the built in page list. For further instructions, open the "Help" tab on the Pages screen.
- Version: 2.0-VIP
- Author: Jake Goldman (10up LLC)
- Author URI: http://10up.com
+Plugin Name: Simple Page Ordering
+Plugin URI: http://10up.com/plugins/simple-page-ordering-wordpress/
+Description: Order your pages and hierarchical post types using drag and drop on the built in page list. For further instructions, open the "Help" tab on the Pages screen.
+Version: 2.1.2
+Author: Jake Goldman, 10up
+Author URI: http://10up.com
+License: GPLv2 or later
+ */
 
-    Plugin: Copyright 2011 Jake Goldman (email : jake@10up.com)
-
-	 This plug-in is a derivative of Simple Page Ordering, created by Jake Goldman
-	 with copyright attributions to Oomph Inc. from inception to January 2011, with
-	 modifications and updates attributed to 10up LLC and Jake Goldman from February
-	 2011 to March 2012.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-class Simple_Page_Ordering_2 {
+class Simple_Page_Ordering {
 
 	public function __construct() {
 		add_action( 'load-edit.php', array( $this, 'load_edit_screen' ) );
@@ -38,8 +18,15 @@ class Simple_Page_Ordering_2 {
 
 	public function load_edit_screen() {
 		$screen = get_current_screen();
+		$post_type = $screen->post_type;
 
-		if ( !current_user_can('edit_others_pages') || ( !post_type_supports( $screen->post_type, 'page-attributes' ) && !is_post_type_hierarchical( $screen->post_type ) ) )		// check permission
+		// is post type sortable?
+		$sortable = ( post_type_supports( $post_type, 'page-attributes' ) || is_post_type_hierarchical( $post_type ) );		// check permission
+		if ( ! $sortable = apply_filters( 'simple_page_ordering_is_sortable', $sortable, $post_type ) )
+			return;
+
+		// does user have the right to manage these post objects?
+		if ( ! $this->check_edit_others_caps( $post_type ) )
 			return;
 
 		add_filter( 'views_' . $screen->id, array( $this, 'sort_by_order_link' )  );		// add view by menu order to views
@@ -48,9 +35,10 @@ class Simple_Page_Ordering_2 {
 	}
 
 	public function wp() {
-		if ( get_query_var('orderby') == 'menu_order title' ) {	// we can only sort if we're organized by menu order
+		if ( 0 === strpos( get_query_var('orderby'), 'menu_order' ) ) {	// we can only sort if we're organized by menu order
 			$script_name = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? 'simple-page-ordering.dev.js' : 'simple-page-ordering.js';
-			wp_enqueue_script( 'simple-page-ordering', plugins_url( 'simple-page-ordering/' . $script_name, dirname( __FILE__ ) ), array('jquery-ui-sortable'), '2.0', true );
+			wp_enqueue_script( 'simple-page-ordering', plugins_url( $script_name, __FILE__ ), array('jquery-ui-sortable'), '2.1', true );
+			wp_enqueue_style( 'simple-page-ordering', plugins_url( 'simple-page-ordering.css', __FILE__ ) );
 		}
 	}
 
@@ -58,116 +46,168 @@ class Simple_Page_Ordering_2 {
 		$screen = get_current_screen();
 		$screen->add_help_tab(array(
 			'id' => 'simple_page_ordering_help_tab',
-			'title' => __( 'Simple Page Ordering', 'simple_page_ordering' ),
-			'content' => '
-				<p><a href="http://www.get10up.com/plugins/simple-page-ordering-wordpress/" target="_blank">' . __( 'Simple Page Ordering', 'simple_page_ordering' ) . '</a> ' . __( 'is a plug-in by', 'simple-page-ordering' ) . ' <a href="http://www.get10up.com" target="_blank">Jake Goldman (10up)</a> ' . __( 'that  allows you to order pages and other hierarchical post types with drag and drop.', 'simple-page-ordering' ) . '</p>
-				<p>' . __( 'To reposition an item, simply drag and drop the row by "clicking and holding" it anywhere (outside of the links and form controls) and moving it to its new position.', 'simple-page-ordering' ) . ' ' . __( 'If you have a large number of pages, it may be helpful to adjust the items shown per page by opening the Screen Options tab.', 'simple-page-ordering' ) . '</p>
-			',
+			'title' => 'Simple Page Ordering',
+			'content' => '<p>' . __( 'To reposition an item, simply drag and drop the row by "clicking and holding" it anywhere (outside of the links and form controls) and moving it to its new position.', 'simple-page-ordering' ) . '</p>',
 		));
 	}
 
 	public function ajax_simple_page_ordering() {
-		// check permissions again and make sure we have what we need
-		if ( !current_user_can( 'edit_others_pages' ) || empty( $_POST['id'] ) || ( !isset( $_POST['previd'] ) && !isset( $_POST['nextid'] ) ) )
+		// check and make sure we have what we need
+		if ( empty( $_POST['id'] ) || ( !isset( $_POST['previd'] ) && !isset( $_POST['nextid'] ) ) )
 			die(-1);
 
 		// real post?
 		if ( ! $post = get_post( $_POST['id'] ) )
 			die(-1);
 
+		// does user have the right to manage these post objects?
+		if ( ! $this->check_edit_others_caps( $post->post_type ) )
+			die(-1);
+
+		// badly written plug-in hooks for save post can break things
+		if ( !defined( 'WP_DEBUG' ) || !WP_DEBUG )
+			error_reporting( 0 );
+
 		$previd = empty( $_POST['previd'] ) ? false : (int) $_POST['previd'];
 		$nextid = empty( $_POST['nextid'] ) ? false : (int) $_POST['nextid'];
-		$start = empty( $_POST['start'] ) ? 0 : (int) $_POST['start'];
+		$start = empty( $_POST['start'] ) ? 1 : (int) $_POST['start'];
+		$excluded = empty( $_POST['excluded'] ) ? array( $post->ID ) : array_filter( (array) $_POST['excluded'], 'intval' );
+
 		$new_pos = array(); // store new positions for ajax
+		$return_data = new stdClass;
+
+		do_action( 'simple_page_ordering_pre_order_posts', $post, $start );
 
 		// attempt to get the intended parent... if either sibling has a matching parent ID, use that
 		$parent_id = $post->post_parent;
-  		$next_post_parent = $nextid ? wp_get_post_parent_id( $nextid ) : false;
-  		if ( $previd == $next_post_parent )
-  			$parent_id = $next_post_parent;
-		elseif ( $next_post_parent !== $parent_id ) {
-  			$prev_post_parent = $previd ? wp_get_post_parent_id( $previd ) : false;
-			if ( $prev_post_parent !== $parent_id )
+		$next_post_parent = $nextid ? wp_get_post_parent_id( $nextid ) : false;
+		if ( $previd == $next_post_parent ) {	// if the preceding post is the parent of the next post, move it inside
+			$parent_id = $next_post_parent;
+		} elseif ( $next_post_parent !== $parent_id ) {  // otherwise, if the next post's parent isn't the same as our parent, we need to study
+			$prev_post_parent = $previd ? wp_get_post_parent_id( $previd ) : false;
+			if ( $prev_post_parent !== $parent_id ) {	// if the previous post is not our parent now, make it so!
 				$parent_id = ( $prev_post_parent !== false ) ? $prev_post_parent : $next_post_parent;
-  		}
+			}
+		}
+		// if the next post's parent isn't our parent, it might as well be false (irrelevant to our query)
+		if ( $next_post_parent !== $parent_id )
+			$nextid = false;
 
-		$siblings = get_posts(array(
-			'depth' => 1,
-			'numberposts' => -1,
-			'post_type' => $post->post_type,
-			'post_status' => array( 'publish' , 'pending', 'draft', 'future', 'private' ),
-			'post_parent' => $parent_id,
-			'orderby' => 'menu_order title',
-			'order' => 'ASC',
+		$max_sortable_posts = (int) apply_filters( 'simple_page_ordering_limit', 50 );	// should reliably be able to do about 50 at a time
+		if ( $max_sortable_posts < 5 )	// don't be ridiculous!
+			$max_sortable_posts = 50;
+
+		// we need to handle all post stati, except trash (in case of custom stati)
+		$post_stati = get_post_stati(array(
+			'show_in_admin_all_list' => true,
+		));
+
+		$siblings = new WP_Query(array(
+			'depth'						=> 1,
+			'posts_per_page'			=> $max_sortable_posts,
+			'post_type' 				=> $post->post_type,
+			'post_status' 				=> $post_stati,
+			'post_parent' 				=> $parent_id,
+			'orderby' 					=> 'menu_order title',
+			'order' 					=> 'ASC',
+			'post__not_in'				=> $excluded,
+			'update_post_term_cache'	=> false,
+			'update_post_meta_cache'	=> false,
+			'suppress_filters' 			=> true,
+			'ignore_sticky_posts'		=> true,
 		)); // fetch all the siblings (relative ordering)
-
-		$max_sortable_posts = (int) apply_filters( 'simple_page_ordering_limit', 30 );	// should reliably be able to do about 30 at a time
-		if ( $max_sortable_posts <= 0 )
-			$max_sortable_posts = 30;
-
-		$menu_order = 0;
 
 		// don't waste overhead of revisions on a menu order change (especially since they can't *all* be rolled back at once)
 		remove_action( 'pre_post_update', 'wp_save_post_revision' );
 
-		foreach( $siblings as $sibling ) :
-
-			// because their can be muliple requests for ordering to prevent time outs
-			if ( $start > $menu_order ) {
-				$new_pos[$sibling->ID] = $menu_order;
-				$menu_order++;
-				$max_sortable_posts++;
-				continue;
-			}
+		foreach( $siblings->posts as $sibling ) :
 
 			// don't handle the actual post
-			if ( $sibling->ID == $post->ID )
+			if ( $sibling->ID === $post->ID )
 				continue;
 
 			// if this is the post that comes after our repositioned post, set our repositioned post position and increment menu order
-			if ( $nextid == $sibling->ID ) {
-				wp_update_post(array( 'ID' => $post->ID, 'menu_order' => $menu_order, 'post_parent' => $parent_id ));
+			if ( $nextid === $sibling->ID ) {
+				wp_update_post(array(
+					'ID'			=> $post->ID,
+					'menu_order'	=> $start,
+					'post_parent'	=> $parent_id,
+				));
 				$ancestors = get_post_ancestors( $post->ID );
-				$new_pos[$post->ID] = array( 'menu_order' => $menu_order, 'post_parent' => $parent_id, 'depth' => count($ancestors) );
-				$menu_order++;
+				$new_pos[$post->ID] = array(
+					'menu_order'	=> $start,
+					'post_parent'	=> $parent_id,
+					'depth'			=> count( $ancestors ),
+				);
+				$start++;
 			}
 
 			// if repositioned post has been set, and new items are already in the right order, we can stop
-			if ( isset( $new_pos[$post->ID] ) && $sibling->menu_order >= $menu_order )
+			if ( isset( $new_pos[$post->ID] ) && $sibling->menu_order >= $start ) {
+				$return_data->next = false;
 				break;
-
-			// set the menu order of the current sibling and increment the menu order
-			wp_update_post(array( 'ID' => $sibling->ID, 'menu_order' => $menu_order ));
-			$new_pos[$sibling->ID] = $menu_order;
-			$menu_order++;
-
-			if ( !$nextid && $previd == $sibling->ID ) {
-				wp_update_post(array( 'ID' => $post->ID, 'menu_order' => $menu_order, 'post_parent' => $parent_id ));
-				$ancestors = get_post_ancestors( $post->ID );
-				$new_pos[$post->ID] = array( 'menu_order' => $menu_order, 'post_parent' => $parent_id, 'depth' => count($ancestors) );
-				$menu_order++;
 			}
 
-			// max per request
-			if ( $max_sortable_posts <= $menu_order ) {
-				$next_data = array( 'id' => $post->ID, 'previd' => $previd, 'nextid' => $nextid, 'start' => end($new_pos) );
-				die( json_encode($next_data) );
+			// set the menu order of the current sibling and increment the menu order
+			if ( $sibling->menu_order != $start ) {
+				wp_update_post(array(
+					'ID' 			=> $sibling->ID,
+					'menu_order'	=> $start,
+				));
+			}
+			$new_pos[$sibling->ID] = $start;
+			$start++;
+
+			if ( !$nextid && $previd == $sibling->ID ) {
+				wp_update_post(array(
+					'ID' 			=> $post->ID,
+					'menu_order' 	=> $start,
+					'post_parent' 	=> $parent_id
+				));
+				$ancestors = get_post_ancestors( $post->ID );
+				$new_pos[$post->ID] = array(
+					'menu_order'	=> $start,
+					'post_parent' 	=> $parent_id,
+					'depth' 		=> count($ancestors) );
+				$start++;
 			}
 
 		endforeach;
 
-		// if the moved post has children, we need to refresh the page
-		$children = get_posts(array(
-			'numberposts' => 1,
-			'post_type' => $post->post_type,
-			'post_status' => array('publish','pending','draft','future','private'),
-			'post_parent' => $post->ID
-		));
+		// max per request
+		if ( !isset( $return_data->next ) && $siblings->max_num_pages > 1 ) {
+			$return_data->next = array(
+				'id' 		=> $post->ID,
+				'previd' 	=> $previd,
+				'nextid' 	=> $nextid,
+				'start'		=> $start,
+				'excluded'	=> array_merge( array_keys( $new_pos ), $excluded ),
+			);
+		} else {
+			$return_data->next = false;
+		}
 
-		if ( ! empty( $children ) )
-			die('children');
+		do_action( 'simple_page_ordering_ordered_posts', $post, $new_pos );
 
-		die( json_encode($new_pos) );
+		if ( ! $return_data->next ) {
+			// if the moved post has children, we need to refresh the page (unless we're continuing)
+			$children = get_posts(array(
+				'numberposts'				=> 1,
+				'post_type' 				=> $post->post_type,
+				'post_status' 				=> $post_stati,
+				'post_parent' 				=> $post->ID,
+				'fields'					=> 'ids',
+				'update_post_term_cache'	=> false,
+				'update_post_meta_cache'	=> false,
+			));
+
+			if ( ! empty( $children ) )
+				die( 'children' );
+		}
+
+		$return_data->new_pos = $new_pos;
+
+		die( json_encode( $return_data ) );
 	}
 
 	public function sort_by_order_link( $views ) {
@@ -177,6 +217,18 @@ class Simple_Page_Ordering_2 {
 		$views['byorder'] = '<a href="'. $query_string . '" class="' . $class . '">Sort by Order</a>';
 		return $views;
 	}
+
+	/**
+	 * Checks to see if the current user has the capability to "edit others" for a post type
+	 *
+	 * @param (string) $post_type Post type name
+	 * @return (bool) True or false
+	 */
+	public function check_edit_others_caps( $post_type ) {
+		$post_type_object = get_post_type_object( $post_type );
+		$edit_others_cap = empty( $post_type_object ) ? 'edit_others_' . $post_type . 's' : $post_type_object->cap->edit_others_posts;
+		return apply_filters( 'simple_page_ordering_edit_rights', current_user_can( $edit_others_cap ), $post_type );
+	}
 }
 
-$simple_page_ordering_2 = new Simple_Page_Ordering_2;
+$simple_page_ordering = new Simple_Page_Ordering;
