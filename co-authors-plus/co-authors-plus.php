5a5e2481d1d0f45533d2999848012fc20011bae0
pcrumm <pcrumm@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-04-15 20:40:01 +0000

    [co-authors-plus] Stop-gap fix to allow author pages to display when $term->count incorrectly returns 0.
    /z 28282
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@144471 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/co-authors-plus.php
+++ co-authors-plus/co-authors-plus.php
@@ -951,7 +951,9 @@ class coauthors_plus {
 			$authordata = $author;
 			$term = $this->get_author_term( $authordata );
 		}
-		if ( ( is_object( $authordata ) && is_user_member_of_blog( $authordata->ID ) )
+		// WPCOMVIP - stopgap fix for $term->count returning 0 incorrectly.
+		// ref /z 28282
+		if ( ( is_object( $authordata ) )
 			|| ( ! empty( $term ) && $term->count ) ) {
 			$wp_query->queried_object = $authordata;
 			$wp_query->queried_object_id = $authordata->ID;
