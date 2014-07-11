d329b4a705ba127303e4922c231f1f488a4bfc28
westi <westi@dd790b09-5c1c-0410-8744-875116bcd8a6>
2014-07-11 10:14:39 +0000

    disqus: Avoid Fatal Errors if a theme outputs the comments template more than once in a single page by not redefining functions.
    Fixes: Fatal error in .../vip/plugins/disqus/comments.php:4 - Cannot redeclare dsq_render_single_comment() (previously declared in .../vip/plugins/disqus/comments.php:4)
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@155798 dd790b09-5c1c-0410-8744-875116bcd8a6

--- disqus/comments.php
+++ disqus/comments.php
@@ -1,26 +1,27 @@
 <?php
 global $dsq_response, $dsq_version;
 
-function dsq_render_single_comment( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment;
-	?>
-	<li id="dsq-comment-<?php echo comment_ID(); ?>">
-		<div id="dsq-comment-header-<?php echo comment_ID(); ?>" class="dsq-comment-header">
-			<cite id="dsq-cite-<?php echo comment_ID(); ?>">
-				<?php if(comment_author_url()) : ?>
-					<a id="dsq-author-user-<?php echo comment_ID(); ?>" href="<?php echo comment_author_url(); ?>" target="_blank" rel="nofollow"><?php echo comment_author(); ?></a>
-				<?php else : ?>
-					<span id="dsq-author-user-<?php echo comment_ID(); ?>"><?php echo comment_author(); ?></span>
-				<?php endif; ?>
-			</cite>
-		</div>
-		<div id="dsq-comment-body-<?php echo comment_ID(); ?>" class="dsq-comment-body">
-			<div id="dsq-comment-message-<?php echo comment_ID(); ?>" class="dsq-comment-message"><?php wp_filter_kses(comment_text()); ?></div>
-		</div>
-	</li>
-	<?php
+if ( ! function_exists( 'dsq_render_single_comment' ) ) {
+	function dsq_render_single_comment( $comment, $args, $depth ) {
+		$GLOBALS['comment'] = $comment;
+		?>
+		<li id="dsq-comment-<?php echo comment_ID(); ?>">
+			<div id="dsq-comment-header-<?php echo comment_ID(); ?>" class="dsq-comment-header">
+				<cite id="dsq-cite-<?php echo comment_ID(); ?>">
+					<?php if(comment_author_url()) : ?>
+						<a id="dsq-author-user-<?php echo comment_ID(); ?>" href="<?php echo comment_author_url(); ?>" target="_blank" rel="nofollow"><?php echo comment_author(); ?></a>
+					<?php else : ?>
+						<span id="dsq-author-user-<?php echo comment_ID(); ?>"><?php echo comment_author(); ?></span>
+					<?php endif; ?>
+				</cite>
+			</div>
+			<div id="dsq-comment-body-<?php echo comment_ID(); ?>" class="dsq-comment-body">
+				<div id="dsq-comment-message-<?php echo comment_ID(); ?>" class="dsq-comment-message"><?php wp_filter_kses(comment_text()); ?></div>
+			</div>
+		</li>
+		<?php
+	}
 }
-
 
 ?>
 <div id="disqus_thread">
