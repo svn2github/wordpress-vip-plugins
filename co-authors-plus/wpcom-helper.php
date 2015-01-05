e8dc8a433d8137ff551dc8cd76d58d99e1a0b266
betzster <betzster@dd790b09-5c1c-0410-8744-875116bcd8a6>
2015-01-05 16:58:57 +0000

    Co Authors Plus: Move Guest Authors menu back under Users.
    Previously, the guest authors menu was a submenu of Tools because
    of conflicts with WordPress.com profiles. Those issues have
    been resolved, so we're moving it back under Users.
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@179586 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -1,11 +1,5 @@
 <?php
 /**
- * There's some ugly code in mu-plugins/gprofiles.php that redirects anything on profile.php that's not 
- * a whitelisted gprofile page. Let's put it under tools instead.
- */
-add_filter( 'coauthors_guest_author_parent_page', function() { return 'tools.php'; } );
-
-/**
  * Auto-apply Co-Authors Plus template tags on themes that are properly using the_author()
  * and the_author_posts_link()
  */
