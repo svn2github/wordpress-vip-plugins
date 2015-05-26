dd5cb2ecbea55b154b3c0f5df5520ed57e6e2754
tarendai <tarendai@dd790b09-5c1c-0410-8744-875116bcd8a6>
2015-05-26 22:16:49 +0000

    apply changes by Weston Ruter fixing a bug in #40856-z
    git-svn-id: https://vip-svn.wordpress.com/plugins@199311 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-wp-cli.php
+++ vip-do-not-include-on-wpcom/vip-wp-cli.php
@@ -2,24 +2,28 @@
 
 class WPCOM_VIP_CLI_Command extends WP_CLI_Command {
 
-	/*
-	 *  Clear all of the caches for memory management
-	 */
+	/**
+	 * Clear all of the caches for memory management
+ 	 */
 	protected function stop_the_insanity() {
+		/**
+		 * @var \WP_Object_Cache $wp_object_cache
+		 * @var \wpdb $wpdb
+		 */
 		global $wpdb, $wp_object_cache;
 
 		$wpdb->queries = array(); // or define( 'WP_IMPORTING', true );
 
-		if ( !is_object( $wp_object_cache ) )
-			return;
+		if ( is_object( $wp_object_cache ) ) {
+			$wp_object_cache->group_ops = array();
+			$wp_object_cache->stats = array();
+			$wp_object_cache->memcache_debug = array();
+			$wp_object_cache->cache = array();
 
-		$wp_object_cache->group_ops = array();
-		$wp_object_cache->stats = array();
-		$wp_object_cache->memcache_debug = array();
-		$wp_object_cache->cache = array();
-
-		if ( is_callable( $wp_object_cache, '__remoteset' ) )
-			$wp_object_cache->__remoteset(); // important
+			if ( method_exists( $wp_object_cache, '__remoteset' ) ) {
+				$wp_object_cache->__remoteset(); // important
+			}
+		}
 	}
 
 }
