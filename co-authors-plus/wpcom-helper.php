6fa733f6ea5cf1800d37961e17c67f9cfc8f1f15
henriquemouta <henriquemouta@dd790b09-5c1c-0410-8744-875116bcd8a6>
2015-07-07 09:45:40 +0000

    Co-Author-Plus: Added filters to change author information in the subscription email.
    With this change, the co-authors will be included in the new post notification email.
    For further details on this, see https://wordpressvip.zendesk.com/agent/tickets/41544
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@205110 dd790b09-5c1c-0410-8744-875116bcd8a6

--- co-authors-plus/wpcom-helper.php
+++ co-authors-plus/wpcom-helper.php
@@ -53,3 +53,136 @@ function co_author_plus_es_support( $es_wp_query_args, $query ){
 }
 add_filter('wpcom_elasticsearch_wp_query_args', 'co_author_plus_es_support', 10, 2 );
 
+
+/**
+ * Change the post authors in the subscription email.
+ *
+ * Creates an array of authors, that will be used later.
+ *
+ * @param $author WP_User the original author
+ * @param $post_id
+ *
+ * @return array of coauthors
+ */
+add_filter( 'wpcom_subscriber_email_author', function( $author, $post_id ) {
+
+	$authors = get_coauthors( $post_id );
+	return $authors;
+
+}, 10, 2 );
+
+/**
+ * Change the author avatar url. If there are multiple authors, link the avatar to the post.
+ *
+ * @param $author_url
+ * @param $post_id
+ * @param $authors
+ *
+ * @return string with new author url.
+ */
+add_filter( 'wpcom_subscriber_email_author_url', function( $author_url, $post_id, $authors ) {
+	if( is_array( $authors ) ) {
+		if ( count( $authors ) > 1 ) {
+			return get_permalink( $post_id );
+		}
+
+		return get_author_posts_url( $authors[0]->ID, $authors[0]->user_nicename );
+	}
+
+	return get_author_posts_url( $authors->ID, $authors->user_nicename );
+}, 10, 3);
+
+/**
+ * Change the avatar to be the avatar of the first author
+ *
+ * @param $author_avatar
+ * @param $post_id
+ * @param $authors
+ *
+ * @return string with the html for the avatar
+ */
+add_filter( 'wpcom_subscriber_email_author_avatar', function( $author_avatar, $post_id, $authors ) {
+	if( is_array( $authors ) )
+		return coauthors_get_avatar( $authors[0], 50 );
+
+	return coauthors_get_avatar( $authors, 50 );
+}, 10, 3);
+
+/**
+ * Changes the author byline in the subscription email to include all the authors of the post
+ *
+ * @param $author_byline
+ * @param $post_id
+ * @param $authors
+ *
+ * @return string with the byline html
+ */
+add_filter( 'wpcom_subscriber_email_author_byline_html', function( $author_byline, $post_id, $authors ) {
+	// Check if $authors is a valid array
+	if( ! is_array( $authors ) ) {
+		$authors = array( $authors );
+	}
+
+	$byline = 'by ';
+	foreach( $authors as $author ) {
+		$byline .= '<a href="' . esc_url( get_author_posts_url( $author->ID, $author->user_nicename ) ) . '" style="color: #888 !important;">' . esc_html( $author->display_name ) . '</a>';
+		if ( $author != end( $authors ) ) {
+			$byline .= ', ';
+		}
+	}
+
+	return $byline;
+}, 10, 3);
+
+/**
+ * Change the meta information to include all the authors
+ *
+ * @param $meta
+ * @param $post_id
+ * @param $authors
+ *
+ * @return array with new meta information
+ */
+add_filter( 'wpcom_subscriber_email_meta', function( $meta, $post_id, $authors ) {
+	// Check if $authors is a valid array
+	if( ! is_array( $authors ) ) {
+		$authors = array( $authors );
+	}
+
+	$author_meta = '';
+	foreach( $authors as $author ) {
+		$author_meta .= '<strong><a href="' . esc_url( get_author_posts_url( $author->ID, $author->user_nicename ) ) . '">' . esc_html( $author->display_name ) . '</a></strong>';
+
+		if ( $author != end( $authors ) ) {
+			$author_meta .= ', ';
+		}
+	}
+
+	// Only the first entry of meta includes the author listing
+	$meta[0] = $author_meta;
+
+	return $meta;
+}, 10, 3);
+
+/**
+ * Change the author information in the text-only subscription email.
+ *
+ * @param $author
+ * @param $post_id
+ *
+ * @returns string with the authors
+ */
+add_filter( 'wpcom_subscriber_text_email_author', function( $author, $post_id ) {
+	// Check if $authors is a valid array
+	$authors = get_coauthors( $post_id );
+
+	$author_text = '';
+	foreach( $authors as $author ) {
+		$author_text .= esc_html( $author->display_name );
+		if ( $author != end( $authors ) ) {
+			$author_text .= ', ';
+		}
+	}
+
+	return $author_text;
+});
