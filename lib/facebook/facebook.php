17fa578956a5f35ad4c442de7651b6347a6c1312
spencermorin <spencermorin@dd790b09-5c1c-0410-8744-875116bcd8a6>
2015-08-12 15:10:46 +0000

    Facebook Lib: Don't load the facebook lib within the context of WP_CLI to prevent fatals from incompatibilities with the autoloader WP_CLI uses.
    git-svn-id: https://vip-svn.wordpress.com/plugins@209400 dd790b09-5c1c-0410-8744-875116bcd8a6

--- lib/facebook/facebook.php
+++ lib/facebook/facebook.php
@@ -1,5 +1,10 @@
 <?php
 
+// WPCOM: Don't load the facebook lib within the context of WP_CLI to prevent fatals
+// from incompatibilities with the autoloader WP_CLI uses.
+if( defined('WP_CLI') && WP_CLI )
+	return;
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/FacebookWPHttpClient.php';
 
