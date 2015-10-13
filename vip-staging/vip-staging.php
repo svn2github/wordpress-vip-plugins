f8d1d83a5debe9672e69c86fd63251abcd06b1bf
henriquemouta <henriquemouta@dd790b09-5c1c-0410-8744-875116bcd8a6>
2015-10-13 22:08:22 +0000

    VIP Staging: Code refactoring.
    It now uses an oposite logic, the production repository lies in 'themename-live', while the staging on is the 'themename'
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@217436 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-staging/vip-staging.php
+++ vip-staging/vip-staging.php
@@ -6,27 +6,46 @@ Version: 0.0.1
 Author: Team 1
 */
 
-function vip_staging_change_template( $template_dir, $template, $theme_root ) {
-	$staging_theme = wp_get_theme( $template . "-staging" );
-	// Check if there is a staging parent theme.
-	if ( $staging_theme->exists() ) {
-		return $theme_root . '/' . $template . "-staging";
+class VIP_Staging {
+
+	const SUFIX = '-live';
+
+	public function __construct() {
+
+		if( isset($_GET['staging']) ) {
+			add_filter( 'template_directory', array( $this, 'change_template_directory' ), 10, 3 );
+			add_filter( 'template', array( $this, 'change_template' ) );
+
+			add_filter( 'stylesheet_directory', array( $this, 'change_template_directory' ), 10, 3 );
+			add_filter( 'stylesheet', array( $this, 'change_template' ) );
+		}
+
 	}
 
-	return $template_dir;
-}
+	public function change_template_directory( $template_dir, $template, $theme_root ) {
+		$staging_name = str_replace( self::SUFIX, '', $template );
 
-add_filter( 'template_directory', 'vip_staging_change_template' , 10, 3);
-add_filter( 'stylesheet_directory', 'vip_staging_change_template' , 10, 3);
+		$staging_theme = wp_get_theme( $staging_name );
+		// Check if there is a staging parent theme.
+		if ( $staging_theme->exists() ) {
+			return $theme_root . '/' . $staging_name;
+		}
 
-function vip_staging_change_theme_name ( $theme_name ) {
-	$theme = wp_get_theme( $theme_name . '-staging' );
-
-	if( $theme->exists() ) {
-		return $theme_name . "-staging";
+		return $template_dir;
 	}
 
-	return $theme_name;
+	public function change_template( $template ) {
+
+		$staging_name = str_replace( self::SUFIX, '', $template );
+		$theme = wp_get_theme( $staging_name );
+
+		if( $theme->exists() ) {
+			return $staging_name;
+		}
+
+		return $template;
+	}
+
 }
-add_filter( 'template', 'vip_staging_change_theme_name' );
-add_filter( 'stylesheet', 'vip_staging_change_theme_name' );
+
+new VIP_Staging();
