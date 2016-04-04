8207709391955bb783ec93c8e95b3be0b64055db
henriquemouta <henriquemouta@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-04-04 16:05:39 +0000

    WPCOM Caching: Return the count of comments instead of false, when it's not a global comment count.
    See https://wordpressvip.zendesk.com/agent/tickets/51518 
    Props drebbits
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@239195 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/wpcom-caching.php
+++ vip-do-not-include-on-wpcom/wpcom-caching.php
@@ -569,7 +569,7 @@ function wpcom_vip_attachment_url_to_postid( $url ){
 function wpcom_vip_cache_full_comment_counts( $counts = false , $post_id = 0 ){
 	//We are only caching the global comment counts for now since those are often in the millions while the per page one is usually more reasonable.
 	if ( $post_id !== 0 ){
-		return false;
+		return $counts;
 	}
 	$cache_key = "vip-comments-{$post_id}";
 	$stats_object = wp_cache_get( $cache_key );
