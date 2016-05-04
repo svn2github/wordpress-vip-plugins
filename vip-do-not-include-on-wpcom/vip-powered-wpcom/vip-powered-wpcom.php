57038cac6659e8aceecef777a7d51b377f389ca0
pmaiorana <pmaiorana@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-05-04 21:13:23 +0000

    VIP: adds tracking
    git-svn-id: https://vip-svn.wordpress.com/plugins@243682 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powered-wpcom.php
+++ vip-do-not-include-on-wpcom/vip-powered-wpcom/vip-powered-wpcom.php
@@ -164,5 +164,5 @@ function vip_powered_wpcom( $display = 'text', $before_text = 'Powered by ' ) {
  * @return string
  */
 function vip_powered_wpcom_url() {
-	return 'https://vip.wordpress.com/';
+	return 'https://vip.wordpress.com/?utm_source=vip_powered_wpcom&utm_medium=web&utm_campaign=VIP%20Footer%20Credit';
 }
