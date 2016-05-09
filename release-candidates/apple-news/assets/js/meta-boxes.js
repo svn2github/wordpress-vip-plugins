697196c1666b050ab4f90bbfe67aba30d9958854
davidbinovec <davidbinovec@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-05-09 13:15:29 +0000

    Apple News RC: deprecating the RC version since it has been in sync with production version for a long time
    git-svn-id: https://vip-svn.wordpress.com/plugins@244212 dd790b09-5c1c-0410-8744-875116bcd8a6

--- release-candidates/apple-news/assets/js/meta-boxes.js
+++
@@ -1,9 +0,0 @@
-(function ( $, window, undefined ) {
-	'use strict';
-
-	$( '#apple-news-publish-submit' ).click(function ( e ) {
-		$( '#apple-news-publish-action' ).val( apple_news_meta_boxes.publish_action );
-		$( '#post' ).submit();
-	});
-
-})( jQuery, window );
