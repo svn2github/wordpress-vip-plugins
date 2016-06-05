fb22d2e4d6dd61a8953b2fca6e2b665d286ad08e
philpeter <philpeter@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-06-05 16:09:10 +0000

    VIP: Reverting Ooyala v2.2 removal because errors
    git-svn-id: https://vip-svn.wordpress.com/plugins@247737 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ ooyala-2.2.0/pulse-params.js
@@ -0,0 +1,44 @@
+{
+	/**
+	 * Pulse settings
+	 */
+	//pulse_host: "http://pulse-host.videoplaza.tv",
+	//pulse_device_container: "deviceContainer",
+	//pulse_persistend_id: "persistendId",
+
+	/**
+	 * Values mapped to the request settings
+	 */
+	//pulse_height: 500,
+	//pulse_width: 400,
+	//pulse_max_bitrate: 400,
+	//pulse_linear_cuepoints: "5,23,34",
+	//pulse_non_linear_cuepoints: "12,14,50",
+	//pulse_insertion_point_filter: "onBeforeContent,playbackPosition",
+	//pulse_referrer_url: "http://www.ooyala.com",
+	//pulse_linear_slot_size: 3,
+
+	/**
+	 * Values mapped to the content metadata
+	 */
+	//pulse_category: "myCategory",
+	//pulse_content_form : "longForm", //Can be 'longForm' or 'shortForm'
+	//pulse_content_id: "myCustomId",
+	//pulse_content_partner: "myContentPartner",
+	//pulse_duration: 52,
+
+	/**
+	 * The flags and tags will be merged with the ones coming from
+	 * the Backlot Ad Set, if any. If you want to override
+	 * them completely, set pulse_override_metadata to true.
+	 */
+	//pulse_flags: "",
+	//pulse_tags: "",
+	//pulse_custom_parameters: {
+	//	key1: value1,
+	//	key2: value2
+	//},
+
+	// If set, all the Backlot metadata (ad set or custom)
+	// will be ignored. Only the client side metadata will be used
+}
