32ad3e9d7a32b37062247699f7b72cd007abca1e
stephboisvert <stephboisvert@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-06-07 18:40:38 +0000

    VIP: add randomness to caching on wpcom_vip_wp_old_slug_redirect() to hopefuly stop cache stampedes (see Zendesk 51500)
    git-svn-id: https://vip-svn.wordpress.com/plugins@248062 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/wpcom-caching.php
+++ vip-do-not-include-on-wpcom/wpcom-caching.php
@@ -635,7 +635,7 @@ function wpcom_vip_wp_old_slug_redirect(){
 		if ( false === $redirect ){
 			add_filter('old_slug_redirect_url', 'wpcom_vip_set_old_slug_redirect_cache');
 			//If an old slug is not found the function returns early and does not apply the old_slug_redirect_url filter. so we will set the cache for not found and if it is found it will be overwritten later in wpcom_vip_set_old_slug_redirect_cache()
-			wp_cache_set( 'old_slug'. $wp_query->query_vars['name'], 'not_found', 'default', 4 * HOUR_IN_SECONDS );
+			wp_cache_set( 'old_slug'. $wp_query->query_vars['name'], 'not_found', 'default', 12 * HOUR_IN_SECONDS + mt_rand(0, 12 * HOUR_IN_SECONDS ) );
 		} elseif ( 'not_found' === $redirect ){
 			//wpcom_vip_set_old_slug_redirect_cache() will cache 'not_found' when a url is not found so we don't keep hammering the database
 			remove_action( 'template_redirect', 'wp_old_slug_redirect' );
