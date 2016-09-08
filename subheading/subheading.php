472f71edd4d5c118af5eeb123a15d50bdf6aa1e6
kulash <kulash@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-09-08 19:26:32 +0000

    VIP: subheading: Replace PHP4-style constructors for PHP7 compatibility
    git-svn-id: https://vip-svn.wordpress.com/plugins@260317 dd790b09-5c1c-0410-8744-875116bcd8a6

--- subheading/subheading.php
+++ subheading/subheading.php
@@ -38,7 +38,7 @@ if ( ! class_exists( 'SubHeading' ) ) {
 		 * Initiate the plugin by setting the default values and assigning any
 		 * required actions and filters.
 		 */
-		function SubHeading()
+		function __construct()
 		{
 			$this->options = apply_filters('subheading_options', $this->get_options() );
 			$this->meta_key = '_' . $this->tag;
@@ -61,6 +61,10 @@ if ( ! class_exists( 'SubHeading' ) ) {
 					// searching. This can only be re-enabled if ES is enabled on a site.
 				}
 			}
+		}
+
+		function SubHeading() {
+			self::__construct();
 		}
 		
 		/**
