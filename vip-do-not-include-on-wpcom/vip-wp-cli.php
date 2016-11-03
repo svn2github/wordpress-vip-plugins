a7e8832320f1ebf216e3d546b8560549d21b9164
stephboisvert <stephboisvert@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-11-03 19:17:16 +0000

    VIP: In quickstart (and other local setups) ES_WP_Indexing_Trigger might not be defined. Let's make sure it exists before calling it
    git-svn-id: https://vip-svn.wordpress.com/plugins@268613 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-wp-cli.php
+++ vip-do-not-include-on-wpcom/vip-wp-cli.php
@@ -36,7 +36,10 @@ class WPCOM_VIP_CLI_Command extends WP_CLI_Command {
 		add_filter( 'wpcom_pushpress_should_send_ping', '__return false' );
 		// Disable term count updates for speed
 		wp_defer_term_counting( true );
-		ES_WP_Indexing_Trigger::get_instance()->disable(); //disconnects the wp action hooks that trigger indexing jobs
+		if ( class_exists( 'ES_WP_Indexing_Trigger' ) ){
+			ES_WP_Indexing_Trigger::get_instance()->disable(); //disconnects the wp action hooks that trigger indexing jobs
+		}
+
 	}
 
 	/**
@@ -47,7 +50,9 @@ class WPCOM_VIP_CLI_Command extends WP_CLI_Command {
 	protected function end_bulk_operation(){
 		remove_filter( 'wpcom_pushpress_should_send_ping', '__return false' ); //This shouldn't be required but it's nice to clean up all the settings we changed so they are back to their defaults.
 		wp_defer_term_counting( false ); // This will also trigger a term count.
-		ES_WP_Indexing_Trigger::get_instance()->enable(); //reenable the hooks
-		ES_WP_Indexing_Trigger::get_instance()->trigger_bulk_index( get_current_blog_id(), 'wp_cli' ); //queues async indexing job to be sent on wp shutdown hook, this will re-index the site inside Elasticsearch
+		if ( class_exists( 'ES_WP_Indexing_Trigger' ) ){
+			ES_WP_Indexing_Trigger::get_instance()->enable(); //reenable the hooks
+			ES_WP_Indexing_Trigger::get_instance()->trigger_bulk_index( get_current_blog_id(), 'wp_cli' ); //queues async indexing job to be sent on wp shutdown hook, this will re-index the site inside Elasticsearch
+		}
 	}
 }
