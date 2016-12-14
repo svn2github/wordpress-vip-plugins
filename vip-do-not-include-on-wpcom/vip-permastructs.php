c1e0bb2ae6ff1adfd4ad99e56c95dc3b8d23bd58
davidbinovec <davidbinovec@dd790b09-5c1c-0410-8744-875116bcd8a6>
2016-12-14 10:56:25 +0000

    vip-permastruct: whitespace changes
    git-svn-id: https://vip-svn.wordpress.com/plugins@274152 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/vip-permastructs.php
+++ vip-do-not-include-on-wpcom/vip-permastructs.php
@@ -124,7 +124,7 @@ if ( ! function_exists( 'wpcom_vip_load_custom_cdn' ) ):
  * 		string|array cdn_host_static => Optional. Hostname of the CDN for static assets.
  * 		bool include_admin => Optional. Whether the custom CDN host should be used in the admin context as well.
  * 		bool disable_ssl => Optional. Whether SSL should be disabled for the custom CDN.
- *		bool exclude_preview => Optional. Whether the custom CDN host should be used in the preview context as well.
+ *  	bool exclude_preview => Optional. Whether the custom CDN host should be used in the preview context as well.
  */
 function wpcom_vip_load_custom_cdn( $args ) {
 	if ( defined( 'WPCOM_SANDBOXED' ) && WPCOM_SANDBOXED )
