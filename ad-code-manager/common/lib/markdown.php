5699ecfc93e8d2a04f4c41f21190581ea5cd49ca
nickdaugherty <nickdaugherty@dd790b09-5c1c-0410-8744-875116bcd8a6>
2017-05-02 17:29:03 +0000

    Plugins: Ad Code Manager: Call parent constructor of markdown class in PHP 7  compatible way
    Use ::__construct() instead of ::MarkdownParser()
    
    Syncs https://github.com/Automattic/Ad-Code-Manager/pull/106
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@292514 dd790b09-5c1c-0410-8744-875116bcd8a6

--- ad-code-manager/common/lib/markdown.php
+++ ad-code-manager/common/lib/markdown.php
@@ -1574,7 +1574,7 @@ class MarkdownExtra_Parser extends Markdown_Parser {
 			"doAbbreviations"    => 70,
 		);
 
-		parent::Markdown_Parser();
+		parent::__construct();
 	}
 
 	function MarkdownExtra_Parser() {
