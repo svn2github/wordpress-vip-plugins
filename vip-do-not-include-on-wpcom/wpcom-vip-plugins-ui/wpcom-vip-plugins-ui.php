3d6c67e64c0cba657fc8cba1f51e38a47cb5e951
stephboisvert <stephboisvert@dd790b09-5c1c-0410-8744-875116bcd8a6>
2017-07-07 20:46:01 +0000

    VIP: Remove adding of option if not present.
    When connection problems happen with memcache, some get_option calls might fail. This leads to WordPress's add_option to kick in and overwrite the option in the database even if it's already present. 
    Instead we'll rely on the default value being used (L558) of a blank array which while it would not load the plugins for that specific request will atleast not overwrite it with blank data.  
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@301908 dd790b09-5c1c-0410-8744-875116bcd8a6

--- vip-do-not-include-on-wpcom/wpcom-vip-plugins-ui/wpcom-vip-plugins-ui.php
+++ vip-do-not-include-on-wpcom/wpcom-vip-plugins-ui/wpcom-vip-plugins-ui.php
@@ -364,7 +364,6 @@ class WPcom_VIP_Plugins_UI {
 	 * @uses add_action() To add various actions
 	 */
 	private function setup_actions() {
-		add_option( self::OPTION_ACTIVE_PLUGINS, array() );
 
 		// Loaded at priority 5 because all plugins are typically loaded before 'plugins_loaded'
 		add_action( 'plugins_loaded', array( $this, 'include_active_plugins' ), 5 );
