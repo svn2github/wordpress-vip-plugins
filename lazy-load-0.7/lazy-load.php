c92bab51bacd692e3954f031d7e741a0518f036a
batmoo <batmoo@dd790b09-5c1c-0410-8744-875116bcd8a6>
2017-08-03 21:59:05 +0000

    lazy-load: Adding 0.7
    https://github.com/Automattic/lazy-load/releases/tag/0.7
    
    
    git-svn-id: https://vip-svn.wordpress.com/plugins@305656 dd790b09-5c1c-0410-8744-875116bcd8a6

---
+++ lazy-load-0.7/lazy-load.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Plugin Name: Lazy Load
+ * Description: Lazy load images to improve page load times. Uses jQuery.sonar to only load an image when it's visible in the viewport.
+ * Version: 0.7
+ * Text Domain: lazy-load
+ *
+ * Code by the WordPress.com VIP team, TechCrunch 2011 Redesign team, and Jake Goldman (10up LLC).
+ * Uses jQuery.sonar by Dave Artz (AOL): http://www.artzstudio.com/files/jquery-boston-2010/jquery.sonar/
+ *
+ * License: GPL2
+ */
+
+if ( ! class_exists( 'LazyLoad_Images' ) ) :
+
+class LazyLoad_Images {
+
+	const version = '0.7';
+	protected static $enabled = true;
+
+	static function init() {
+		if ( is_admin() )
+			return;
+
+		if ( ! apply_filters( 'lazyload_is_enabled', true ) ) {
+			self::$enabled = false;
+			return;
+		}
+
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'add_scripts' ) );
+		add_action( 'wp_head', array( __CLASS__, 'setup_filters' ), 9999 ); // we don't really want to modify anything in <head> since it's mostly all metadata, e.g. OG tags
+	}
+
+	static function setup_filters() {
+		add_filter( 'the_content', array( __CLASS__, 'add_image_placeholders' ), 99 ); // run this later, so other content filters have run, including image_add_wh on WP.com
+		add_filter( 'post_thumbnail_html', array( __CLASS__, 'add_image_placeholders' ), 11 );
+		add_filter( 'get_avatar', array( __CLASS__, 'add_image_placeholders' ), 11 );
+	}
+
+	static function add_scripts() {
+		wp_enqueue_script( 'wpcom-lazy-load-images',  self::get_url( 'js/lazy-load.js' ), array( 'jquery', 'jquery-sonar' ), self::version, true );
+		wp_enqueue_script( 'jquery-sonar', self::get_url( 'js/jquery.sonar.min.js' ), array( 'jquery' ), self::version, true );
+	}
+
+	static function add_image_placeholders( $content ) {
+		if ( ! self::is_enabled() )
+			return $content;
+
+		// Don't lazyload for feeds, previews, mobile
+		if( is_feed() || is_preview() )
+			return $content;
+
+		// Don't lazyload for amp-wp content
+		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
+			return $content;
+		}
+
+		// Don't lazy-load if the content has already been run through previously
+		if ( false !== strpos( $content, 'data-lazy-src' ) )
+			return $content;
+
+		// This is a pretty simple regex, but it works
+		$content = preg_replace_callback( '#<(img)([^>]+?)(>(.*?)</\\1>|[\/]?>)#si', array( __CLASS__, 'process_image' ), $content );
+
+		return $content;
+	}
+
+	static function process_image( $matches ) {
+		// In case you want to change the placeholder image
+		$placeholder_image = apply_filters( 'lazyload_images_placeholder_image', self::get_url( 'images/1x1.trans.gif' ) );
+
+		$old_attributes_str = $matches[2];
+		$old_attributes_kses_hair = wp_kses_hair( $old_attributes_str, wp_allowed_protocols() );
+
+		if ( empty( $old_attributes_kses_hair['src'] ) ) {
+			return $matches[0];
+		}
+
+		$old_attributes = self::flatten_kses_hair_data( $old_attributes_kses_hair );
+		$new_attributes = $old_attributes;
+
+		// Set placeholder and lazy-src
+		$new_attributes['src'] = self::get_placeholder_image();
+		$new_attributes['data-lazy-src'] = $old_attributes['src'];
+
+		// Handle `srcset`
+		if ( ! empty( $new_attributes['srcset'] ) ) {
+			$new_attributes['data-lazy-srcset'] = $old_attributes['srcset'];
+			unset( $new_attributes['srcset'] );
+		}
+
+		// Handle `sizes`
+		if ( ! empty( $new_attributes['sizes'] ) ) {
+			$new_attributes['data-lazy-sizes'] = $old_attributes['sizes'];
+			unset( $new_attributes['sizes'] );
+		}
+
+		$new_attributes_str = self::build_attributes_string( $new_attributes );
+
+		return sprintf( '<img %1$s><noscript>%2$s</noscript>', $new_attributes_str, $matches[0] );
+	}
+
+	private static function get_placeholder_image() {
+		return apply_filters( 'lazyload_images_placeholder_image', self::get_url( 'images/1x1.trans.gif' ) );
+	}
+
+	private static function flatten_kses_hair_data( $attributes ) {
+		$flattened_attributes = array();
+		foreach ( $attributes as $name => $attribute ) {
+			$flattened_attributes[ $name ] = $attribute['value'];
+		}
+		return $flattened_attributes;
+	}
+
+	private static function build_attributes_string( $attributes ) {
+		$string = array();
+		foreach ( $attributes as $name => $value ) {
+			if ( '' === $value ) {
+				$string[] = sprintf( '%s', $name );
+			} else {
+				$string[] = sprintf( '%s="%s"', $name, esc_attr( $value ) );
+			}
+		}
+		return implode( ' ', $string );
+	}
+
+	static function is_enabled() {
+		return self::$enabled;
+	}
+
+	static function get_url( $path = '' ) {
+		return plugins_url( ltrim( $path, '/' ), __FILE__ );
+	}
+}
+
+function lazyload_images_add_placeholders( $content ) {
+	return LazyLoad_Images::add_image_placeholders( $content );
+}
+
+add_action( 'init', array( 'LazyLoad_Images', 'init' ) );
+
+endif;
